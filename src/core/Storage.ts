6941d1279bc6de3c64d8b1b14ec3f4e16f440a72
komed3 <pk97@outlook.de>
2025-12-02 19:50:44 +0100

    load storage config

--- src/core/Storage.ts
+++ src/core/Storage.ts
@@ -1,12 +1,25 @@
 import { existsSync } from 'node:fs';
 import { readFile, writeFile } from 'node:fs/promises';
-import {  } from 'node:path';
+import { join } from 'node:path';
+import { load } from 'js-yaml';
+
+export interface StorageConfig {
+    baseDir: string;
+    minify: boolean;
+};
 
 export class Storage {
 
     private static instance: Storage;
+    private static readonly cwd = process.cwd();
 
-    private constructor () {}
+    private readonly config: StorageConfig;
+
+    private constructor () {
+
+        this.config = load( join( Storage.cwd, 'config/storage.yml' ) ) as StorageConfig;
+
+    }
 
     public static getInstance () : Storage {
 
@@ -15,10 +28,16 @@ export class Storage {
 
     }
 
+    private pathBuilder ( filePath: string ) : string {
+
+        return join( Storage.cwd, this.config.baseDir, filePath );
+
+    }
+
     private async saveJson< T = any > ( filePath: string, data: T ) : Promise< void > {
 
         const jsonData = JSON.stringify( data, null, 2 );
-        await writeFile( filePath, jsonData, 'utf8' );
+        await writeFile( this.pathBuilder( filePath ), jsonData, 'utf8' );
 
     }
 
@@ -26,7 +45,7 @@ export class Storage {
 
         if ( ! existsSync( filePath ) ) return false;
 
-        const data = await readFile( filePath, 'utf8' );
+        const data = await readFile( this.pathBuilder( filePath ), 'utf8' );
         return JSON.parse( data ) as T;
 
     }
