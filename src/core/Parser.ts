3c353fca05cea05960eb3ad44428f5c3a0e5c377
komed3 <pk97@outlook.de>
2025-12-03 23:47:02 +0100

    parse lists, counties and industries

--- src/core/Parser.ts
+++ src/core/Parser.ts
@@ -1,5 +1,11 @@
-import { ISOCountryCode } from '@rtbnext/schema/src/abstract/const';
+import * as Const from '@rtbnext/schema/src/abstract/const';
 import { getAlpha2Code } from 'i18n-iso-countries';
+
+export const Industry = ( [
+    'automotive', 'casinos', 'diversified', 'energy', 'engineering', 'finance',
+    'foodstuff', 'healthcare', 'logistics', 'manufacturing', 'media', 'mining',
+    'property', 'retail', 'service', 'sports', 'technology', 'telecom'
+] as any ) as Const.Industry;
 
 export class Parser {
 
@@ -7,10 +13,28 @@ export class Parser {
     public static number ( value: any, d = 0 ) : number { return Number( Number( value ).toFixed( d ) ) }
     public static string ( value: any ) : string { return String( value ).trim() }
 
-    public static country2ISOCode ( country: string ) : ISOCountryCode | undefined {
+    public static list ( value: any ) : string[] {
 
-        const code = getAlpha2Code( country, 'en' );
-        if ( code ) return code.toLowerCase() as ISOCountryCode;
+        return this.string( value ).split( ',' ).map(
+            s => this.string( s )
+        );
+
+    }
+
+    public static country ( value: any ) : Const.ISOCountryCode | undefined {
+
+        const code = getAlpha2Code( Parser.string( value ), 'en' );
+        if ( code ) return code.toLowerCase() as Const.ISOCountryCode;
+
+    }
+
+    public static state () {}
+
+    public static industry ( value: any ) : Const.Industry {
+
+        const i = this.string( value ).toLowerCase();
+        if ( Industry.includes( i ) ) return i as Const.Industry;
+        return 'diversified';
 
     }
 
