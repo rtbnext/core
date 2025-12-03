e29c71d6dbf2bf0e6931cc9829d394e7e4ab1352
komed3 <pk97@outlook.de>
2025-12-03 23:23:58 +0100

    Update Parser.ts

--- src/core/Parser.ts
+++ src/core/Parser.ts
@@ -1,9 +1,15 @@
 export class Parser {
 
     public static number ( value: any, digits = 0 ) : number {
+        return Number( Number( value ).toFixed( digits ) );
+    }
 
-        return Number( ( Number( value ) ).toFixed( digits ) );
+    public static string ( value: any ) : string {
+        return String( value ).trim();
+    }
 
+    public static boolean ( value: any ) : boolean {
+        return Boolean( value );
     }
 
 }
