bc122793ca9406a9df64b32e071a61a557278c08
komed3 <pk97@outlook.de>
2025-12-03 16:00:08 +0100

    request daily RTB list

--- src/maintenance/fetchRTB.ts
+++ src/maintenance/fetchRTB.ts
@@ -1,1 +1,17 @@
-export class FetchRTB {}
+import { Config } from '../core/Config';
+import { Fetch } from '../core/Fetch';
+
+export async function fetchRTB () {
+
+    const config = Config.getInstance().getAPIConfig();
+    const fetch = Fetch.getInstance();
+
+    const res = await fetch.request( config.endpoints.list.replace( '{URI}', 'rtb' ) );
+    if ( ! res.success ) return;
+
+    // proceed data ...
+
+}
+
+// Runner
+( async () => { if ( process.argv.includes( '--run' ) ) await fetchRTB() } )();
