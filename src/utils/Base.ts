c5c92b178310c86069a7638798538cfb40be908d
komed3 <pk97@outlook.de>
2025-12-11 00:20:22 +0100

    sanitize value

--- src/utils/Base.ts
+++ src/utils/Base.ts
@@ -1,1 +1,8 @@
-export class Base {}
+export class Base {
+
+    public static sanitize ( value: any, delimiter: string = '-' ) : string {
+        return String( value ).toLowerCase().trim().replace( /[^a-z0-9]/g, delimiter )
+            .replace( new RegExp( `[${delimiter}]{2,}`, 'g' ), delimiter );
+    }
+
+}
