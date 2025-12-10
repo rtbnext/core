e82b594b26d0840a6aacb93c47c105a4049e786a
komed3 <pk97@outlook.de>
2025-12-11 00:18:01 +0100

    create utils index file

---
+++ src/utils/index.ts
@@ -0,0 +1,14 @@
+import { Base } from './Base';
+import * as Const from './Const';
+import { Logger } from './Logger';
+import { Parser } from './Parser';
+
+const Utils = {
+    const: Const,
+    base: Base,
+    logger: Logger,
+    parser: Parser
+};
+
+export default Utils;
+export { Const, Base, Logger, Parser };
