980fc33dbb5e0bdbaa3468e6235f8cac455825cd
komed3 <pk97@outlook.de>
2025-12-11 23:49:53 +0100

    init DB (ensure directories exists)

--- src/core/Storage.ts
+++ src/core/Storage.ts
@@ -1,16 +1,28 @@
 import { StorageConfig } from '@/types/config';
 import { Logger } from '@/utils/Logger';
 import { ConfigLoader } from './ConfigLoader';
+import { mkdirSync } from 'node:fs';
+import { join } from 'node:path';
+import { cwd } from 'node:process';
 
 export class Storage {
 
     private static instance: Storage;
     private readonly logger: Logger;
     private readonly config: StorageConfig;
+    private readonly path: string;
 
     private constructor () {
         this.logger = Logger.getInstance();
         this.config = ConfigLoader.getInstance().storage;
+        this.path = join( cwd(), this.config.baseDir );
+        this.initDB();
+    }
+
+    public initDB () : void {
+        [ 'profile', 'list', 'filter', 'mover', 'stats' ].forEach(
+            dir => mkdirSync( join( this.path, dir ), { recursive: true } )
+        );
     }
 
     public static getInstance () : Storage {
