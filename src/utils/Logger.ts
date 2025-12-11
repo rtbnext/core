7fd48b0df020768beaf15328f766c10fd1738b43
komed3 <pk97@outlook.de>
2025-12-11 02:09:18 +0100

    implement logger methods

--- src/utils/Logger.ts
+++ src/utils/Logger.ts
@@ -1,1 +1,25 @@
-export class Logger {}
+export class Logger {
+
+    private static instance: Logger;
+
+    private constructor () {}
+
+    private format () {}
+
+    private log () {}
+
+    public error () {}
+
+    public exit () {}
+
+    public warn () {}
+
+    public info () {}
+
+    public debug () {}
+
+    public static getInstance () : Logger {
+        return Logger.instance ||= new Logger();
+    }
+
+}
