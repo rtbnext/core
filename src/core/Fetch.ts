96b47b0e61ca79f19de75257c267cbaee0215c5f
komed3 <pk97@outlook.de>
2025-12-11 10:23:34 +0100

    setup axios

--- src/core/Fetch.ts
+++ src/core/Fetch.ts
@@ -1,8 +1,22 @@
+import { ConfigLoader } from '@/core/ConfigLoader';
+import { FetchConfig } from '@/types/config';
+import axios, { AxiosInstance } from 'axios';
+
 export class Fetch {
 
     private static instance: Fetch;
+    private readonly config: FetchConfig;
+    private httpClient: AxiosInstance;
 
-    private constructor () {}
+    private constructor () {
+        this.config = ConfigLoader.getInstance().fetch;
+        this.httpClient = this.setupHttpClient();
+    }
+
+    private setupHttpClient () : AxiosInstance {
+        const { baseUrl: baseURL, headers, rateLimit: { timeout } } = this.config;
+        return axios.create( { baseURL, headers, timeout } );
+    }
 
     public static getInstance () {
         return Fetch.instance ||= new Fetch();
