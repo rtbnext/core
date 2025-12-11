ff668ec071a6224689fa6255541a6b3f86e568dc
komed3 <pk97@outlook.de>
2025-12-11 01:50:34 +0100

    Update Base.ts

--- src/utils/Base.ts
+++ src/utils/Base.ts
@@ -7,16 +7,19 @@ export class Base {
             .replace( new RegExp( `[${delimiter}]{2,}`, 'g' ), delimiter );
     }
 
-    public static sort< L extends ListLike >(
-        value: L, compare?: ( a: any, b: any ) => -1 | 0 | 1
+    public static sort< L extends ListLike > (
+        value: L, compare?: ( a: any, b: any ) => -1 | 0 | 1,
+        objCompare?: ( a: any, b: any ) => -1 | 0 | 1
     ) : L {
         compare ||= ( a, b ) => ( a > b ? 1 : a < b ? -1 : 0 );
-        const keys = ( a: any, b: any ) => compare( a[ 0 ], b[ 0 ] );
+        objCompare ||= ( a, b ) => compare( a[ 0 ], b[ 0 ] );
 
         return ( Array.isArray( value ) ? [ ...value ].sort( compare )
             : value instanceof Set ? new Set( [ ...value ].sort( compare ) )
-            : value instanceof Map ? new Map( [ ...value.entries() ].sort( keys ) )
-            : typeof value === 'object' ? Object.fromEntries( Object.entries( value ).sort( keys ) )
+            : value instanceof Map ? new Map( [ ...value.entries() ].sort( objCompare ) )
+            : typeof value === 'object' ? Object.fromEntries(
+                Object.entries( value ).sort( objCompare )
+            )
             : [ ...value as Iterable< any > ].sort( compare )
         ) as L;
     }
