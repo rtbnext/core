24f4ef06215127df82b6681267f6ba80ce3d6b3b
komed3 <pk97@outlook.de>
2025-12-16 23:28:19 +0100

    exporting collections

--- src/collection/index.ts
+++ src/collection/index.ts
@@ -3,4 +3,10 @@ import { ListIndex } from './ListIndex';
 import { Profile } from './Profile';
 import { ProfileIndex } from './ProfileIndex';
 
-export {};
+const collection = {
+    profileIndex: ProfileIndex.getInstance,
+    listIndex: ListIndex.getInstance
+};
+
+export { List, ListIndex, Profile, ProfileIndex };
+export default collection;
