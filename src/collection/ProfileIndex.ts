d5ecfeb9d452f2a908843426c8cb124a40ee171c
komed3 <pk97@outlook.de>
2025-12-17 01:02:35 +0100

    implement methods to find and move profile index items

--- src/collection/ProfileIndex.ts
+++ src/collection/ProfileIndex.ts
@@ -1,5 +1,6 @@
 import { Index } from '@/abstract/Index';
 import { TProfileIndex, TProfileIndexItem } from '@/types/profile';
+import { Utils } from '@/utils';
 
 export class ProfileIndex extends Index< TProfileIndexItem, TProfileIndex > {
 
@@ -7,6 +8,29 @@ export class ProfileIndex extends Index< TProfileIndexItem, TProfileIndex > {
 
     private constructor () {
         super( 'profile/index.json' );
+    }
+
+    public find ( uriLike: string ) : TProfileIndex {
+        const uri = Utils.sanitize( uriLike );
+        return new Map( [ ...this.index ].filter( ( [ key, { aliases } ] ) =>
+            key === uri || aliases.includes( uri )
+        ) );
+    }
+
+    public move ( from: string, to: string, makeAlias: boolean = true ) : TProfileIndexItem | false {
+        const sanitizedFrom = Utils.sanitize( from );
+        const sanitizedTo = Utils.sanitize( to );
+
+        const data = this.index.get( sanitizedFrom );
+        if ( ! data || this.find( sanitizedTo ).size ) return false;
+
+        const item = { ...data, uri: sanitizedTo };
+        if ( makeAlias ) item.aliases.push( sanitizedFrom );
+        this.index.delete( sanitizedFrom );
+        this.index.set( sanitizedTo, item );
+        this.saveIndex();
+
+        return item;
     }
 
     public static getInstance () {
