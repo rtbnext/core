112bf43687ca5694a039b56231fcf655cecd6d86
komed3 <pk97@outlook.de>
2025-12-17 11:49:22 +0100

    Update Profile.ts

--- src/collection/Profile.ts
+++ src/collection/Profile.ts
@@ -1,10 +1,15 @@
 import { TProfileData } from '@/types/profile';
 import { TProfileResponse } from '@/types/response';
+import { Parser } from '@/utils/Parser';
+import { Utils } from '@/utils/Utils';
 
 export class Profile {
 
-    public static parser ( raw: TProfileResponse ) : Partial< TProfileData > {
-        return {};
+    public static parser ( raw: TProfileResponse[ 'person' ] ) : Partial< TProfileData > {
+        return {
+            ...Parser.container< TProfileData >( {} ),
+            uri: Utils.sanitize( raw.uri )
+        };
     }
 
 }
