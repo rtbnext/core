ea3b50496651fb4d9c260171e7718320cee5f917
komed3 <pk97@outlook.de>
2025-12-18 17:33:03 +0100

    fetch batched profiles

--- src/job/fetchProfile.ts
+++ src/job/fetchProfile.ts
@@ -2,7 +2,10 @@ import { Job, jobRunner } from '@/abstract/Job';
 
 export class FetchProfile extends Job {
 
-    public async run () : Promise< void > {}
+    public async run () : Promise< void > {
+        const batch = this.queue.next( 'profile', this.config.fetch.rateLimit.maxBatchSize );
+        const res = this.fetch.profile( ...batch.map( i => i.uri ) );
+    }
 
 }
 
