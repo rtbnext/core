90a9def4c6cdd9687d1492b5805778c5a679a945
komed3 <pk97@outlook.de>
2025-12-18 09:49:05 +0100

    add abstract Job cls constructor

--- src/abstract/Job.ts
+++ src/abstract/Job.ts
@@ -1,6 +1,25 @@
+import { ConfigLoader } from '@/core/ConfigLoader';
+import { Fetch } from '@/core/Fetch';
+import { TConfigObject } from '@/types/config';
 import helper from '@/utils';
 
-export abstract class Job {}
+export abstract class Job {
+
+    protected readonly silent: boolean;
+    protected readonly safeMode: boolean;
+    protected config: TConfigObject;
+    protected fetch: Fetch;
+
+    constructor ( silent: boolean, safeMode: boolean ) {
+        this.silent = silent;
+        this.safeMode = safeMode;
+        this.config = ConfigLoader.getInstance();
+        this.fetch = Fetch.getInstance();
+    }
+
+    public abstract run () : void | Promise< void >;
+
+}
 
 export function jobRunner< T extends typeof Job > (
     cls: T, method: keyof InstanceType< T > = 'run', ci: string = '--run',
