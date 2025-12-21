0696d50c51f520337a6754ba92d59f40c962c77c
komed3 <pk97@outlook.de>
2025-12-21 23:13:19 +0100

    Update Wiki.ts

--- src/utils/Wiki.ts
+++ src/utils/Wiki.ts
@@ -23,7 +23,9 @@ export class Wiki {
         else if ( birthDate && item.birthdate?.value ) score -= 0.1;
 
         // Gender matching
-        if ( item.gender?.value.endsWith( gender === 'm' ? 'Q6581097' : gender === 'f' ? 'Q6581072' : '-' ) ) score += 0.1;
+        if ( gender && item.gender?.value.endsWith(
+            gender === 'm' ? 'Q6581097' : gender === 'f' ? 'Q6581072' : '-'
+        ) ) score += 0.1;
 
         // Citizenship matching
         if ( citizenship && item.iso2?.value === citizenship.toUpperCase() ) score += 0.2;
@@ -33,7 +35,9 @@ export class Wiki {
         if ( item.image ) score += 0.05;
 
         // Occupation matching
-        if ( [ 'Q131524', 'Q557880', 'Q911554', 'Q2462658' ].some( e => item.occupation?.value.endsWith( e ) ) ) score += 0.2;
+        if ( [ 'Q131524', 'Q557880', 'Q911554', 'Q2462658' ].some(
+            e => item.occupation?.value.endsWith( e )
+        ) ) score += 0.2;
         else if ( item.occupation ) score += 0.05;
 
         // Economic matching
