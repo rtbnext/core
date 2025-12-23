da6d26c2dc76612de633edf496b24a7e71ffb978
komed3 <pk97@outlook.de>
2025-12-24 00:24:30 +0100

    Update ManageQueue.ts

--- src/job/ManageQueue.ts
+++ src/job/ManageQueue.ts
@@ -1,3 +1,16 @@
+/**
+ * ManageQueue Job
+ * 
+ * node ./dist/job/ManageQueue.ts [silent?] [safeMode?] [--queue=type] [--clear] [--add=taskUri] [--args=jsonArgs] [--prio=number]
+ * @arg silent Whether to suppress log output
+ * @arg safeMode Whether to enable safe mode
+ * @arg queue Type of queue to manage (default: 'profile')
+ * @arg clear Whether to clear the specified queue
+ * @arg add URI of the task to add to the queue
+ * @arg args JSON-encoded arguments for the task being added
+ * @arg prio Priority of the task being added
+ */
+
 import { Job, jobRunner } from '@/abstract/Job';
 import { TArgs } from '@/types/generic';
 import { Parser } from '@/utils/Parser';
