2b9e67ef089a379f3ee3a1769895c9372fed8222
komed3 <pk97@outlook.de>
2025-12-23 16:52:30 +0100

    revert last change

--- src/core/Queue.ts
+++ src/core/Queue.ts
@@ -57,7 +57,7 @@ export class Queue {
         this.queue[ type ].set( uri, { uri, prio, ts: new Date().toISOString() } );
         this.saveQueue();
 
-        log.info( `Added to queue [${type}]: ${uri} (prio: ${prio ?? this.config.defaultPrio})` );
+        log.debug( `Added to queue [${type}]: ${uri} (prio: ${prio ?? this.config.defaultPrio})` );
         return true;
     }
 
