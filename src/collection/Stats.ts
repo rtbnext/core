a8fbc2c1e47227bd850c16cacd0ef66dff00ddcd
komed3 <pk97@outlook.de>
2025-12-24 10:19:13 +0100

    Update Stats.ts

--- src/collection/Stats.ts
+++ src/collection/Stats.ts
@@ -1,8 +1,24 @@
+import { Storage } from '@/core/Storage';
+import { TGlobalStats } from '@/types/stats';
+
 export class Stats {
 
     private static instance: Stats;
+    private readonly storage: Storage;
+    private globalStats: TGlobalStats;
 
-    private constructor () {}
+    private constructor () {
+        this.storage = Storage.getInstance();
+        this.globalStats = this.loadGlobalStats();
+    }
+
+    private loadGlobalStats () : TGlobalStats {
+        return this.storage.readJSON< TGlobalStats >( 'stats/global.json' ) || {} as TGlobalStats;
+    }
+
+    public global () : TGlobalStats {
+        return this.globalStats;
+    }
 
     public static getInstance () : Stats {
         return Stats.instance ||= new Stats();
--- src/collection/index.ts
+++ src/collection/index.ts
@@ -2,3 +2,4 @@ export { List } from '@/collection/List';
 export { ListIndex } from '@/collection/ListIndex';
 export { Profile } from '@/collection/Profile';
 export { ProfileIndex } from '@/collection/ProfileIndex';
+export { Stats } from '@/collection/Stats';
