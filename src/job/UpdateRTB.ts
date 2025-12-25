435d1616912f908c5df77afbcc99f67d130f596c
komed3 <pk97@outlook.de>
2025-12-25 10:36:32 +0100

    parse partial profile data from list entry

--- src/job/UpdateRTB.ts
+++ src/job/UpdateRTB.ts
@@ -1,8 +1,7 @@
 import { Job, jobRunner } from '@/abstract/Job';
 import { Stats } from '@/collection/Stats';
-import { TArgs } from '@/types/generic';
-import { TRTBSnapshot } from '@/types/list';
-import { TRTBResponse } from '@/types/response';
+import { TRTBItem } from '@/types/list';
+import { TListResponse } from '@/types/response';
 import { ListParser } from '@/utils/ListParser';
 import { Parser } from '@/utils/Parser';
 
@@ -14,21 +13,22 @@ export class UpdateRTB extends Job {
         super( silent, safeMode, 'UpdateRTB' );
     }
 
-    public async run ( args: TArgs ) : Promise< void > {
+    public async run () : Promise< void > {
         await this.protect( async () => {
             const rtStats = this.stats.getRealtime();
-            const res = await this.fetch.list< TRTBResponse >( 'rtb', '0' );
+            const res = await this.fetch.list< TListResponse >( 'rtb', '0' );
             if ( ! res?.success || ! res.data ) throw new Error( 'Request failed' );
 
             const raw = res.data.personList.personsLists;
             const listDate = Parser.date( raw[ 0 ].date || raw[ 0 ].timestamp, 'ymd' )!;
             if ( rtStats.date === listDate ) throw new Error( 'RTB list is already up to date' );
 
-            const items: TRTBSnapshot[ 'items' ] = [];
+            const items: TRTBItem[] = [];
             const count = 0, total = 0, woman = 0;
 
             for ( const row of raw ) {
                 const parser = new ListParser( row );
+                const profileData = {};
             }
 
             rtStats.date = listDate;
--- src/utils/ListParser.ts
+++ src/utils/ListParser.ts
@@ -1,7 +1,33 @@
+import { TProfileData } from '@/types/profile';
 import { TListResponse } from '@/types/response';
+import { Parser } from '@/utils/Parser';
+import { Utils } from '@/utils/Utils';
 
-export class ListParser< T extends TListResponse[ 'personList' ][ 'personsLists' ][ number ] > {
+export class ListParser {
 
-    constructor ( private readonly res: T ) {}
+    constructor ( private readonly raw: TListResponse[ 'personList' ][ 'personsLists' ][ number ] ) {}
+
+    public uri () : string {
+        return Utils.sanitize( this.raw.uri );
+    }
+
+    public id () : string {
+        return Utils.hash( this.raw.naturalId );
+    }
+
+    public name () : ReturnType< typeof Parser.name > {
+        return Parser.name( this.raw.person?.name ?? this.raw.personName, this.raw.lastName );
+    }
+
+    public info () : Partial< TProfileData[ 'info' ] > {
+        return Parser.container< Partial< TProfileData[ 'info' ] > >( {
+            dropOff: { value: this.raw.finalWorth ? this.raw.finalWorth < 1e3 : undefined, method: 'boolean' },
+            gender: { value: this.raw.gender, method: 'gender' },
+            birthDate: { value: this.raw.birthDate, method: 'date' },
+            citizenship: { value: this.raw.countryOfCitizenship, method: 'country' },
+            industry: { value: this.raw.industries?.[ 0 ], method: 'industry' },
+            source: { value: this.raw.source, method: 'list' }
+        } );
+    }
 
 }
