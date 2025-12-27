78b20a650df78b7d4fb8c9a4eeab9a8e9b015c16
komed3 <pk97@outlook.de>
2025-12-27 15:03:06 +0100

    add storage instance for movers

--- src/collection/Mover.ts
+++ src/collection/Mover.ts
@@ -1,8 +1,13 @@
+import { Storage } from '@/core/Storage';
+
 export class Mover {
 
     private static instance: Mover;
+    private readonly storage: Storage;
 
-    private constructor () {}
+    private constructor () {
+        this.storage = Storage.getInstance();
+    }
 
     public static getInstance () : Mover {
         return this.instance ||= new Mover();
