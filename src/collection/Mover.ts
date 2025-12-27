349f9e34f4f15be7b6c81ce1265ef120b0d59ff7
komed3 <pk97@outlook.de>
2025-12-27 15:04:44 +0100

    scan movers directory

--- src/collection/Mover.ts
+++ src/collection/Mover.ts
@@ -1,12 +1,16 @@
 import { Storage } from '@/core/Storage';
+import { Utils } from '@/utils/Utils';
 
 export class Mover {
 
     private static instance: Mover;
     private readonly storage: Storage;
+    private dates: string[];
 
     private constructor () {
         this.storage = Storage.getInstance();
+        this.storage.ensurePath( 'movers', true );
+        this.dates = Utils.sort( this.storage.scanDir( 'movers' ) );
     }
 
     public static getInstance () : Mover {
