6601f4a426f2353d7c7c2f3c42462442e2b288e3
komed3 <pk97@outlook.de>
2025-12-28 18:41:34 +0100

    rename job

--- src/job/UpdateStats.ts
+++ src/job/UpdateStats.ts
@@ -3,10 +3,10 @@ import { Profile } from '@/collection/Profile';
 import { ProfileIndex } from '@/collection/ProfileIndex';
 import { TArgs } from '@/types/generic';
 
-export class UpdateFiler extends Job {
+export class UpdateStats extends Job {
 
     constructor ( silent: boolean, safeMode: boolean ) {
-        super( silent, safeMode, 'UpdateFilter' );
+        super( silent, safeMode, 'UpdateStats' );
     }
 
     public async run ( args: TArgs ) : Promise< void > {
@@ -14,6 +14,8 @@ export class UpdateFiler extends Job {
             for ( const item of ProfileIndex.getInstance().getIndex().values() ) {
                 const profile = Profile.getByItem( item );
                 if ( ! profile ) continue;
+
+                const { info, realtime } = profile.getData();
                 // ...
             }
         } );
@@ -21,4 +23,4 @@ export class UpdateFiler extends Job {
 
 }
 
-jobRunner( UpdateFiler );
+jobRunner( UpdateStats );
