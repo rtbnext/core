117c256d22cbf0ef3d8e019c53969905003e24e7
komed3 <pk97@outlook.de>
2025-12-29 17:26:52 +0100

    rework stats types

--- src/types/stats.d.ts
+++ src/types/stats.d.ts
@@ -1,22 +1,46 @@
+import { TMetaData } from '@/types/generic';
 import { TListStats } from '@/types/list';
-import { Gender, MaritalStatus } from '@/utils/Const';
-
-export type TGlobalStats = TListStats & {
-    agePyramid: Record< Gender, Record< number, number > >;
-    maritalStatus: { [ K in MaritalStatus ]?: number };
-    children: Record< number, number >;
-};
+import { Gender, Industry, MaritalStatus } from '@/utils/Const';
 
 export type TStatsHistory = TStatsHistoryItem[];
 
 export type TStatsHistoryItem = [ string, number, number, number, number, number, number ];
 
-export type TReturnHistory = TReturnHistoryItem[];
-
-export type TReturnHistoryItem = [ string, number, number, number, number, number, number ];
-
-export interface TReturnCollection {
-    industry: Record< string, TReturnHistory >;
-    citizenship: Record< string, TReturnHistory >;
-    country: Record< string, TReturnHistory >;
+export interface TStats< T extends string > {
+    index: TMetaData & { [ K in T ]: TListStats & { first: {
+        readonly uri: string;
+        name: string;
+        rank: number;
+        networth: number;
+    } } };
+    history: { [ K in T ]: TStatsHistory };
 }
+
+export type TIndustryStats = TStats< Industry >;
+
+export type TCitizenshipStats = TStats< string >;
+
+export type TStatsList< T extends string > = { [ K in T ]?: number };
+
+export type TAgePyramid = { [ K in Gender ]?: TStatsList< `${number}` > };
+
+export interface TGlobalStats {
+    agePyramid: TAgePyramid;
+    maritalStatus: TStatsList< MaritalStatus >;
+    children: {
+        full: StatsList< `${number}` >;
+        short: StatsList< 'none' | 'one' | 'two' | 'three' | 'four' | '5-to-10' | 'over-10' >;
+    };
+    selfMade: TStatsList< `${number}` >;
+    philanthropyScore: TStatsList< `${number}`  >;
+}
+
+export interface TScatterItem {
+    readonly uri: string;
+    name: string;
+    gender: Gender;
+    age: number;
+    networth: number;
+}
+
+export type TScatter = TScatterItem[];
