e050700bc16284a7de1c2629d7f2a17de80d1bdb
komed3 <pk97@outlook.de>
2025-12-29 11:41:05 +0100

    implement Filter.saveFilter() method

--- src/collection/Filter.ts
+++ src/collection/Filter.ts
@@ -1,14 +1,31 @@
 import { Storage } from '@/core/Storage';
-import { TFilterCollection } from '@/types/filter';
+import { TFilter, TFilterCollection, TFilterList } from '@/types/filter';
+import { Utils } from '@/utils';
 
 export class Filter {
 
     private static instance: Filter;
     private static readonly storage = Storage.getInstance();
 
-    private data?: TFilterCollection;
+    private data: Partial< TFilterCollection > = {};
+    private readonly filterGroups = [
+        'industry', 'citizenship', 'country', 'state', 'gender',
+        'age', 'maritalStatus', 'special'
+    ];
 
-    private constructor () {}
+    private constructor () {
+        for ( const g of this.filterGroups ) Filter.storage.ensurePath( `filter/${g}`, true );
+    }
+
+    private saveFilter ( path: string, list: TFilter[] ) : void {
+        const items = [ ...new Map( list.map( i => [ i.uri, i ] ) ).values() ].sort(
+            ( a, b ) => a.uri.localeCompare( b.uri )
+        );
+
+        Filter.storage.writeJSON< TFilterList >( path, {
+            ...Utils.metaData(), items, count: items.length
+        } );
+    }
 
     public static getInstance () : Filter {
         return Filter.instance ||= new Filter();
