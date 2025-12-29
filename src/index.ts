d7595cc8ddaa9aa39e72b4f99514ef203cdc1c7c
komed3 <pk97@outlook.de>
2025-12-29 11:26:10 +0100

    min fix

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,12 @@ import { MergeProfile } from '@/job/MergeProfile';
 import { MoveProfile } from '@/job/MoveProfile';
 import { UpdateProfile } from '@/job/UpdateProfile';
 import { UpdateRTB } from '@/job/UpdateRTB';
+import { UpdateStats } from '@/job/UpdateStats';
 import { UpdateWiki } from '@/job/UpdateWiki';
 
 export const jobs = {
-    ManageQueue, MergeProfile, MoveProfile,
-    UpdateProfile, UpdateRTB, UpdateWiki
+    ManageQueue, MergeProfile, MoveProfile, UpdateProfile,
+    UpdateRTB, UpdateStats, UpdateWiki
 } as const;
 
 export * from '@/collection';
