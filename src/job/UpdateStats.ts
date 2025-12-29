e55d1dca3689b659cfb140d7980112f7aa891f6f
komed3 <pk97@outlook.de>
2025-12-29 09:41:53 +0100

    generate filtered lists

--- src/job/UpdateStats.ts
+++ src/job/UpdateStats.ts
@@ -1,7 +1,9 @@
 import { Job, jobRunner } from '@/abstract/Job';
 import { Profile } from '@/collection/Profile';
 import { ProfileIndex } from '@/collection/ProfileIndex';
+import { TFilter, TFilterCollection } from '@/types/filter';
 import { TArgs } from '@/types/generic';
+import { Parser } from '@/utils/Parser';
 
 export class UpdateStats extends Job {
 
@@ -11,12 +13,29 @@ export class UpdateStats extends Job {
 
     public async run ( args: TArgs ) : Promise< void > {
         await this.protect( async () => {
+            const filter: TFilterCollection = {
+                industry: {}, citizenship: {}, country: {}, state: {}, gender: {}, age: {}, maritalStatus: {},
+                special: { deceased: [], dropOff: [], family: [], selfMade: [] }
+            };
+
             for ( const item of ProfileIndex.getInstance().getIndex().values() ) {
                 const profile = Profile.getByItem( item );
                 if ( ! profile ) continue;
 
-                const { info, realtime } = profile.getData();
-                // ...
+                const { uri, info, realtime } = profile.getData();
+                const fItem: TFilter = { uri, name: info.shortName ?? info.name };
+                
+                if ( info.industry ) ( filter.industry[ info.industry ] ??= [] ).push( fItem );
+                if ( info.citizenship ) ( filter.citizenship[ info.citizenship ] ??= [] ).push( fItem );
+                if ( info.residence?.country ) ( filter.country[ info.residence.country ] ??= [] ).push( fItem );
+                if ( info.residence?.state ) ( filter.state[ info.residence.state ] ??= [] ).push( fItem );
+                if ( info.gender ) ( filter.gender[ info.gender ] ??= [] ).push( fItem );
+                if ( info.birthDate ) ( filter.age[ Parser.ageDecade( info.birthDate )! ] ??= [] ).push( fItem );
+                if ( info.maritalStatus ) ( filter.maritalStatus[ info.maritalStatus ] ??= [] ).push( fItem );
+                if ( info.deceased ) filter.special.deceased.push( fItem );
+                if ( info.dropOff ) filter.special.dropOff.push( fItem );
+                if ( info.family ) filter.special.family.push( fItem );
+                if ( info.selfMade?.is ) filter.special.selfMade.push( fItem );
             }
         } );
     }
