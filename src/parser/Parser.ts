b9d4baa46536f13aec00529f485b1ca65c1e3457
komed3 <pk97@outlook.de>
2026-01-02 23:24:49 +0100

    implement parser helper method "strict"

--- src/parser/Parser.ts
+++ src/parser/Parser.ts
@@ -1,1 +1,12 @@
-export class Parser {}
+export class Parser {
+
+    // Helper
+
+    public static strict< T = any > (
+        value: any, method: keyof typeof Parser, ...args: any
+    ) : T | undefined {
+        return value === null || value === undefined ? undefined
+            : ( Parser as any )[ method ]( value, ...args ) as T;
+    }
+
+}
