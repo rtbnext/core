4102118b84dd01571d5eaa2859d7a13c54cb1abf
komed3 <pk97@outlook.de>
2026-01-02 15:23:46 +0100

    add TMeasuredResult type

--- src/types/generic.d.ts
+++ src/types/generic.d.ts
@@ -3,3 +3,8 @@ export type TArgs = Record< string, string | boolean >;
 export type TAggregator =
     | 'all' | 'first' | 'last' | 'sum' | 'min' | 'max' | 'mean'
     | ( ( values: readonly T[ K ][] ) => R );
+
+export interface TMeasuredResult< R > {
+    result: R;
+    ms: number;
+}
