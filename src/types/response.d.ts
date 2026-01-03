bd9c2e80a724e1ae7fd7a8b34a284518e1e50fd1
komed3 <pk97@outlook.de>
2026-01-03 16:02:04 +0100

    describe (Forbes) profile & list responses

--- src/types/response.d.ts
+++ src/types/response.d.ts
@@ -17,3 +17,131 @@ export interface TWaybackResponse {
         };
     };
 }
+
+export interface TProfileResponse {
+    person: {
+        naturalId: string;
+        name: string;
+        listImages?: {
+            image: string;
+            uri: string;
+            caption: string;
+            description: string;
+            title: string;
+            credit: string;
+        }[];
+        geoLocation?: {
+            longitude: number;
+            latitude: number;
+        };
+        relatedEntities?: {
+            name: string;
+            type: string;
+            uri?: string;
+            relationshipType?: string;
+        }[];
+        uri: string;
+        dropOff: boolean;
+        uris: string[];
+        industries: string[];
+        embargo?: boolean;
+        firstName?: string;
+        lastName?: string;
+        birthDate?: number;
+        finalWorth: number;
+        finalWorthDate: number;
+        stateProvince?: string;
+        city?: string;
+        numberOfChildren?: number;
+        maritalStatus?: 'M' | 'F';
+        source: string;
+        title?: string;
+        organization?: string;
+        asianFormat?: 'N' | 'Y';
+        personLists: {
+            listUri: string;
+            name: string;
+            listDescription?: string;
+            rank?: number;
+            finalWorth?: number;
+            timestamp: number | string;
+            financialAssets?: {
+                exchange: string;
+                ticker: string;
+                companyName: string;
+                numberOfShares?: number;
+                sharePrice: number;
+                exchangeRate?: number;
+                currentPrice?: number;
+            }[];
+            date?: number | string;
+            estWorthPrev?: number;
+            privateAssetsWorth?: number;
+            familyList?: boolean;
+            archivedWorth?: number;
+            bios?: string[];
+            abouts?: string[];
+            philanthropyScore?: number;
+        }[];
+        educations?: {
+            school: string;
+            degree: string;
+        }[];
+        selfMade?: boolean;
+        gender: string;
+        countryOfCitizenship: string;
+        countryOfResidence?: string;
+        birthCity?: string;
+        birthState?: string;
+        birthCountry?: string;
+        deceased: boolean;
+        selfMadeType?: string;
+        selfMadeRank?: number;
+        quote?: string;
+    };
+}
+
+export interface TListResponse {
+    personList: {
+        personsLists: {
+            naturalId: string;
+            name: string;
+            year: number;
+            listUri: string;
+            uri: string;
+            rank?: number;
+            finalWorth?: number;
+            person?: {
+                name?: string;
+                uri?: string;
+            },
+            personName: string;
+            state?: string;
+            city?: string;
+            source?: string;
+            industries?: string[];
+            countryOfCitizenship?: string;
+            timestamp: number;
+            gender?: 'M' | 'F';
+            birthDate?: number;
+            lastName?: string;
+            financialAssets?: {
+                exchange: string;
+                ticker: string;
+                companyName: string;
+                numberOfShares?: number;
+                sharePrice?: number;
+                currencyCode?: string;
+                exchangeRate?: number;
+                currentPrice?: number;
+            }[];
+            date?: number;
+            estWorthPrev?: number;
+            privateAssetsWorth?: number;
+            archivedWorth?: number;
+            csfDisplayFields: string[];
+            bios?: string[];
+            abouts?: string[];
+        }[];
+    };
+}
