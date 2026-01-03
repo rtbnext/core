6387b92d16ac6bad17f25feab7fedae3e52b6e0e
komed3 <pk97@outlook.de>
2026-01-03 15:49:13 +0100

    add fetch class helper methods

--- src/core/Fetch.ts
+++ src/core/Fetch.ts
@@ -18,6 +18,18 @@ export class Fetch {
         return axios.create( { headers, timeout } );
     }
 
+    private getRandomUserAgent () : string {
+        return this.config.agentPool[ Math.floor(
+            Math.random() * this.config.agentPool.length
+        ) ];
+    }
+
+    private getRandomDelay () : Promise< void > {
+        const { max, min } = this.config.rateLimit.requestDelay;
+        const delay = Math.round( Math.random() * ( max - min ) + min );
+        return new Promise( resolve => setTimeout( resolve, delay ) );
+    }
+
     public static getInstance () {
         return Fetch.instance ||= new Fetch();
     }
