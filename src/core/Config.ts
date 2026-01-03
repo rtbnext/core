9558a92625d2bb0474166026dc6d47cad345cf80
komed3 <pk97@outlook.de>
2026-01-03 15:32:13 +0100

    add config getters

--- src/core/Config.ts
+++ src/core/Config.ts
@@ -12,13 +12,13 @@ export class Config {
     private readonly cwd: string;
     private readonly path: string;
     private readonly env: string;
-    private readonly config: TConfigObject;
+    private readonly cfg: TConfigObject;
 
     private constructor () {
         this.cwd = cwd();
         this.path = join( this.cwd, 'config' );
         this.env = process.env.NODE_ENV || 'production';
-        this.config = this.loadConfig();
+        this.cfg = this.loadConfig();
     }
 
     private loadConfigFile ( path: string ) : Partial< TConfigObject > {
@@ -33,6 +33,10 @@ export class Config {
             { arrayMerge: ( t, s ) => Utils.mergeArray( t, s, 'replace' ) }
         );
     }
+
+    public get root () : string { return this.cwd }
+    public get environment () : string { return this.env }
+    public get config () : TConfigObject { return this.cfg }
 
     public static getInstance () : Config {
         return Config.instance ||= new Config();
