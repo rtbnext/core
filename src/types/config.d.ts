0a56165b900ecf2b80a1c5a4b8113ac280c85ff6
komed3 <pk97@outlook.de>
2026-01-03 15:43:23 +0100

    describe fetch config

--- src/types/config.d.ts
+++ src/types/config.d.ts
@@ -2,7 +2,27 @@ export interface TLoggingConfig {}
 
 export interface TStorageConfig {}
 
-export interface TFetchConfig {}
+export interface TFetchConfig {
+    endpoints: {
+        profile: string;
+        list: string;
+        wikipedia: string;
+        commons: string;
+        wikidata: string;
+        wayback: string;
+    };
+    headers: Record< string, string >;
+    agentPool: string[];
+    rateLimit: {
+        batchSize: number;
+        timeout: number;
+        retries: number;
+        requestDelay: {
+            max: number;
+            min: number;
+        };
+    };
+}
 
 export interface TQueueConfig {}
 
