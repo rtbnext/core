f49188483e97fa0a3aa9a6f6082d8c9e5ecf4d32
komed3 <pk97@outlook.de>
2026-01-03 22:33:29 +0100

    instantiate storage class

--- src/core/Storage.ts
+++ src/core/Storage.ts
@@ -1,1 +1,20 @@
-export class Storage {}
+import { Config } from '@/core/Config';
+import { TStorageConfig } from '@/types/config';
+import { join } from 'node:path';
+
+export class Storage {
+
+    private static instance: Storage;
+    private static readonly config: TStorageConfig = Config.getInstance().storage;
+
+    private readonly path: string;
+
+    private constructor () {
+        this.path = join( Config.getInstance().root, Storage.config.baseDir );
+    }
+
+    public static getInstance () : Storage {
+        return Storage.instance ||= new Storage();
+    }
+
+}
