fee2effce2ce32256b37def421f64e791dcd6252
komed3 <pk97@outlook.de>
2026-01-04 15:22:13 +0100

    Create interfaces/queue.d.ts

---
+++ src/interfaces/queue.d.ts
@@ -0,0 +1,13 @@
+export interface Queue {
+    getQueue () : TQueueItem[];
+    size () : number;
+    getByKey ( key: string ) : TQueueItem | undefined;
+    hasKey ( key: string ) : boolean;
+    getByUri ( uriLike: string ) : TQueueItem[];
+    hasUri ( uriLike: string ) : boolean;
+    clear () : void;
+    add ( opt: TQueueOptions, save: boolean = true ) : boolean;
+    addMany ( items: TQueueOptions[] ) : number;
+    next ( n: number = 1 ) : TQueueItem[];
+    nextUri ( n: number = 1 ) : string[];
+}
