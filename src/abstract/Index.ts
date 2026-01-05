708b1d57c6e29c8b587dcc61cdb2ed8574f32c95
komed3 <pk97@outlook.de>
2026-01-05 09:33:39 +0100

    load / save index

--- src/abstract/Index.ts
+++ src/abstract/Index.ts
@@ -1,3 +1,29 @@
+import { Storage } from '@/core/Storage';
 import { IIndex } from '@/interfaces/index';
+import { TIndex } from '@rtbnext/schema/src/abstract/generic';
 
-export abstract class Index implements IIndex {}
+export abstract class Index<
+    I extends TIndex, T extends Map< string, I >
+> implements IIndex< I, T > {
+
+    protected static readonly storage = Storage.getInstance();
+    protected readonly path: string;
+    protected index: T;
+
+    protected constructor ( path: string ) {
+        this.path = path;
+        Index.storage.ensurePath( this.path );
+        this.index = this.loadIndex();
+    }
+
+    protected loadIndex () : T {
+        const raw = Index.storage.readJSON< Record< string, I > > ( this.path ) ?? {};
+        return new Map( Object.entries( raw ) ) as T;
+    }
+
+    protected saveIndex () : void {
+        const content = Object.fromEntries( this.index );
+        Index.storage.writeJSON< Record< string, I > >( this.path, content );
+    }
+
+}
