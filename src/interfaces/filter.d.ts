d48a34dd055785c515949beec31d022ec86e0420
komed3 <pk97@outlook.de>
2026-01-06 09:23:21 +0100

    update filter types

--- src/interfaces/filter.d.ts
+++ src/interfaces/filter.d.ts
@@ -1,12 +1,12 @@
 import { TFilterGroup, TFilterSpecial } from '@rtbnext/schema/src/abstract/const';
-import { TFilter } from '@rtbnext/schema/src/model/filter';
+import { TFilterItem, TFilterList } from '@rtbnext/schema/src/model/filter';
 
 export interface IFilter {
     resolvePath ( path: string ) : string | false;
-    getFilter ( group: TFilterGroup, key: string ) : TFilter[] | false;
-    getFilterByPath ( path: string ) : TFilter[] | false;
-    getGroup ( group: TFilterGroup ) : Record< string, TFilter[] >;
-    getSpecial ( special: TFilterSpecial ) : TFilter[];
+    getFilter ( group: TFilterGroup, key: string ) : TFilterItem[] | false;
+    getFilterByPath ( path: string ) : TFilterItem[] | false;
+    getGroup ( group: TFilterGroup ) : Record< string, TFilterItem[] >;
+    getSpecial ( special: TFilterSpecial ) : TFilterItem[];
     has ( path: string, uriLike: string ) : boolean;
-    save ( collection: Partial< TFilterCollection > ) : void;
+    save ( collection: Partial< TFilterList > ) : void;
 }
