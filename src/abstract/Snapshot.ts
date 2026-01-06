301039a0e370e560344eba39f1197bf490badd3d
komed3 <pk97@outlook.de>
2026-01-06 16:16:30 +0100

    get / save snapshot data

--- src/abstract/Snapshot.ts
+++ src/abstract/Snapshot.ts
@@ -1,8 +1,10 @@
+import { log } from '@/core/Logger';
 import { Storage } from '@/core/Storage';
 import { Utils } from '@/core/Utils';
 import { ISnapshot } from '@/interfaces/snapshot';
 import { Parser } from '@/parser/Parser';
 import { TSnapshot } from '@rtbnext/schema/src/abstract/generic';
+import { join } from 'node:path';
 
 export abstract class Snapshot< T extends TSnapshot > implements ISnapshot< T > {
 
@@ -21,6 +23,10 @@ export abstract class Snapshot< T extends TSnapshot > implements ISnapshot< T > 
 
     protected scanDates () : string[] {
         return Utils.sort( Snapshot.storage.scanDir( this.path ) );
+    }
+
+    protected datedPath ( date: string ) : string {
+        return join( this.path, `${date}.${this.ext}` );
     }
 
     // Basic snapshot getters
@@ -61,6 +67,37 @@ export abstract class Snapshot< T extends TSnapshot > implements ISnapshot< T > 
     public latestInYear ( year: string | number ) : string | undefined {
         const target = Parser.string( year );
         return this.dates.filter( date => date.substring( 0, 4 ) === target ).at( -1 );
+    }
+
+    // Get snapshot data
+
+    public getSnapshot ( dateLike: string, exactMatch: boolean = true ) : T | false {
+        const target = Parser.date( dateLike )!;
+        const date = this.hasDate( target ) ? target : exactMatch
+            ? undefined : this.nearestDate( target );
+
+        return date ? Snapshot.storage.readJSON< T >( this.datedPath( date ) ) : false;
+    }
+
+    public getLatest () : T | false {
+        return this.dates.length ? this.getSnapshot( this.latestDate()! ) : false;
+    }
+
+    // Save snapshot data
+
+    public saveSnapshot ( snapshot: T, force: boolean = false ) : boolean {
+        log.debug( `Saving snapshot for date ${snapshot.date}` );
+        return log.catch( () => {
+            if ( ! force && this.hasDate( snapshot.date ) ) {
+                throw new Error( `Snapshot for date ${snapshot.date} already exists` );
+            }
+            const path = this.datedPath( snapshot.date );
+            if ( ! Snapshot.storage.writeJSON< T >( path, snapshot ) ) {
+                throw new Error( `Failed to write snapshot to ${path}` );
+            }
+            this.dates = this.scanDates();
+            return true;
+        }, `Failed to save snapshot for date ${snapshot.date}` ) ?? false;
     }
 
 }
