90c7cea722088c67b0e2970fb7fe84fbe506c8c1
komed3 <pk97@outlook.de>
2026-01-06 16:19:20 +0100

    Update snapshot.d.ts

--- src/interfaces/snapshot.d.ts
+++ src/interfaces/snapshot.d.ts
@@ -1,3 +1,15 @@
 import { TSnapshot } from '@rtbnext/schema/src/abstract/generic';
 
-export interface ISnapshot< T extends TSnapshot > {}
+export interface ISnapshot< T extends TSnapshot > {
+    getDates () : string[];
+    hasDate ( dateLike: string ) : boolean;
+    firstDate () : string | undefined;
+    latestDate () : string | undefined;
+    nearestDate ( dateLike: string ) : string | undefined;
+    datesInRange ( from: string, to: string ) : string[];
+    firstInYear ( year: string | number ) : string | undefined;
+    latestInYear ( year: string | number ) : string | undefined;
+    getSnapshot ( dateLike: string, exactMatch: boolean = true ) : T | false;
+    getLatest () : T | false;
+    saveSnapshot ( snapshot: T, force: boolean = false ) : boolean;
+}
