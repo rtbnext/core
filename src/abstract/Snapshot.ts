c112eb4d9bde48dff2608bdbaddaa1a2bb4864c1
komed3 <pk97@outlook.de>
2026-01-06 16:02:14 +0100

    implement special snapshot getters

--- src/abstract/Snapshot.ts
+++ src/abstract/Snapshot.ts
@@ -41,4 +41,26 @@ export abstract class Snapshot< T extends TSnapshot > implements ISnapshot< T > 
         return this.dates.at( -1 );
     }
 
+    // Special snapshot getters
+
+    public nearestDate ( dateLike: string ) : string | undefined {
+        const target = Parser.date( dateLike )!;
+        return this.dates.slice().reduce( ( nearest, date ) => date > target ? nearest : date );
+    }
+
+    public datesInRange ( from: string, to: string ) : string[] {
+        const fromDate = Parser.date( from )!, toDate = Parser.date( to )!;
+        return this.dates.filter( date => date >= fromDate && date <= toDate );
+    }
+
+    public firstInYear ( year: string | number ) : string | undefined {
+        const target = Parser.string( year );
+        return this.dates.find( date => date.substring( 0, 4 ) === target );
+    }
+
+    public latestInYear ( year: string | number ) : string | undefined {
+        const target = Parser.string( year );
+        return this.dates.filter( date => date.substring( 0, 4 ) === target ).at( -1 );
+    }
+
 }
