5eed4557943071c81f47268e14b81cba689526f3
komed3 <pk97@outlook.de>
2026-01-07 13:55:53 +0100

    instantiate stats class

--- src/model/Stats.ts
+++ src/model/Stats.ts
@@ -1,3 +1,15 @@
+import { Storage } from '@/core/Storage';
 import { IStats } from '@/interfaces/stats';
 
-export class Stats implements IStats {}
+export class Stats implements IStats {
+
+    private static readonly storage = Storage.getInstance();
+    private static instance: Stats;
+
+    private constructor () {}
+
+    public static getInstance () : Stats {
+        return Stats.instance ||= new Stats();
+    }
+
+}
