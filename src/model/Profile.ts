58a3184e3d960b53fcd09dc950e7e2fa51d770f7
komed3 <pk97@outlook.de>
2026-01-07 09:46:36 +0100

    add statics

--- src/model/Profile.ts
+++ src/model/Profile.ts
@@ -1,3 +1,10 @@
+import { Storage } from '@/core/Storage';
 import { IProfile } from '@/interfaces/profile';
+import { ProfileIndex } from '@/model/ProfileIndex';
 
-export class Profile implements IProfile {}
+export class Profile implements IProfile {
+
+    private static readonly storage = Storage.getInstance();
+    private static readonly index = ProfileIndex.getInstance();
+
+}
