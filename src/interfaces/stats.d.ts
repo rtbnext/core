962d8d7032e75a97c65d28d7d2b025e5752be442
komed3 <pk97@outlook.de>
2026-01-08 09:35:23 +0100

    describe stats getter

--- src/interfaces/stats.d.ts
+++ src/interfaces/stats.d.ts
@@ -1,1 +1,10 @@
-export interface IStats {}
+import { TDBStats, TGlobalStats, THistory, TProfileStats, TScatter, TWealthStats } from '@rtbnext/schema/src/model/stats';
+
+export interface IStats {
+    getGlobalStats () : TGlobalStats;
+    getDBStats () : TDBStats;
+    getHistory () : THistory;
+    getProfileStats () : TProfileStats;
+    getWealthStats () : TWealthStats;
+    getScatter () : TScatter;
+}
