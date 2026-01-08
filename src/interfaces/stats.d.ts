a16cc05a898bb7cb1d0cf4f137d46caa7031354c
komed3 <pk97@outlook.de>
2026-01-08 14:36:44 +0100

    Update stats.d.ts

--- src/interfaces/stats.d.ts
+++ src/interfaces/stats.d.ts
@@ -2,12 +2,15 @@ import * as S from '@rtbnext/schema/src/model/stats';
 
 export interface IStats {
     getGlobalStats () : S.TGlobalStats;
-    getHistory () : S.THistory;
     getProfileStats () : S.TProfileStats;
     getWealthStats () : S.TWealthStats;
     getScatter () : S.TScatter;
+    getHistory () : S.THistory;
     getDBStats () : S.TDBStats;
     setGlobalStats ( data: Partial< S.TGlobalStats > ) : boolean;
+    setProfileStats ( data: Partial< S.TProfileStats > ) : boolean;
+    setWealthStats ( data: Partial< S.TWealthStats > ) : boolean;
+    setScatter ( data: Partial< S.TScatter > ) : boolean;
     updateHistory ( data: Partial< S.TGlobalStats > ) : boolean;
     generateDBStats () : boolean;
 }
