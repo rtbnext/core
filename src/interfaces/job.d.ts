93e9f75a744235f66b9b1fb00f33f5c75b78f846
komed3 <pk97@outlook.de>
2026-01-10 10:20:28 +0100

    Update job.d.ts

--- src/interfaces/job.d.ts
+++ src/interfaces/job.d.ts
@@ -1,1 +1,9 @@
-export interface IJob {}
+import { TArgs } from '@/types/generic';
+
+export interface IJob {
+    getJobName () : string;
+    getArgs () : TArgs;
+    isSilent () : boolean;
+    isSafeMode () : boolean;
+    run ( ...args: any[] ) : void | Promise< void >;
+}
