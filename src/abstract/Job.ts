9faf11b29200eb388341883d5459d122bb8baa75
komed3 <pk97@outlook.de>
2026-01-10 10:08:40 +0100

    instantiate job

--- src/abstract/Job.ts
+++ src/abstract/Job.ts
@@ -1,3 +1,25 @@
+import { Config } from '@/core/Config';
+import { log } from '@/core/Logger';
+import { ListQueue, ProfileQueue } from '@/core/Queue';
+import { Storage } from '@/core/Storage';
+import { Utils } from '@/core/Utils';
 import { IJob } from '@/interfaces/job';
+import { TArgs } from '@/types/generic';
 
-export abstract class Job implements IJob {}
+export abstract class Job implements IJob {
+
+    protected static readonly config = Config.getInstance();
+    protected static readonly storage = Storage.getInstance();
+    protected static readonly profileQueue = ProfileQueue.getInstance();
+    protected static readonly listQueue = ListQueue.getInstance();
+
+    protected readonly job: string;
+    protected readonly args: TArgs = {};
+
+    constructor ( job: string ) {
+        this.job = job;
+        this.args = Utils.parseArgs( process.argv.slice( 2 ) );
+        log.info( `Run job: ${job}`, this.args );
+    }
+
+}
