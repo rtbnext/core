c5b27ffd7dfc7991dff3ce18f93280b974e762a7
komed3 <pk97@outlook.de>
2026-01-11 10:10:30 +0100

    implement queue job

--- src/job/Queue.ts
+++ src/job/Queue.ts
@@ -1,5 +1,8 @@
 import { Job, jobRunner } from '@/abstract/Job';
+import { ListQueue, ProfileQueue } from '@/core/Queue';
 import { IJob } from '@/interfaces/job';
+import { IQueue } from '@/interfaces/queue';
+import { Parser } from '@/parser/Parser';
 
 export class QueueJob extends Job implements IJob {
 
@@ -9,7 +12,20 @@ export class QueueJob extends Job implements IJob {
 
     public async run () : Promise< void > {
         await this.protect( async () => {
-            //
+            const { type, clear, add, prio, args } = this.args;
+
+            let queue: IQueue;
+            if ( type === 'profile' ) queue = ProfileQueue.getInstance();
+            else if ( type === 'list' ) queue = ListQueue.getInstance();
+            else throw new Error( `Unknown queue type: ${type}` );
+
+            if ( Parser.boolean( clear ) ) queue.clear();
+            else if ( add ) queue.addMany( Parser.string( add ).split( ',' ).filter( Boolean ).map(
+                uriLike => ( {
+                    uriLike, prio: Parser.strict( prio, 'number' ),
+                    args: typeof args === 'string' ? JSON.parse( args ) : undefined
+                } )
+            ) );
         } );
     }
 
