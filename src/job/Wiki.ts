f067501470bbaaf721d9e6fc08c8f793159916c4
komed3 <pk97@outlook.de>
2026-01-11 17:47:16 +0100

    Update Wiki.ts

--- src/job/Wiki.ts
+++ src/job/Wiki.ts
@@ -1,6 +1,14 @@
 import { Job, jobRunner } from '@/abstract/Job';
 import { IJob } from '@/interfaces/job';
 
-export class WikiJob extends Job implements IJob {}
+export class WikiJob extends Job implements IJob {
+
+    constructor ( args: string[] ) {
+        super( args, 'Wiki' );
+    }
+
+    public async run () : Promise< void > {}
+
+}
 
 jobRunner( WikiJob );
