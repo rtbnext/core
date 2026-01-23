c794296a5a220a856356fcc49f3e46b3483ca87d
komed3 <pk97@outlook.de>
2026-01-23 10:57:24 +0100

    caching parsed data

--- src/parser/ProfileParser.ts
+++ src/parser/ProfileParser.ts
@@ -1,3 +1,4 @@
+import { Utils } from '@/core/Utils';
 import { IProfileParser } from '@/interfaces/parser';
 import { TProfileResponse } from '@/types/response';
 
@@ -5,12 +6,26 @@ export class ProfileParser implements IProfileParser {
 
     private readonly raw: TProfileResponse[ 'person' ];
     private readonly lists: TProfileResponse[ 'person' ][ 'personLists' ];
+    private cachedData: Map< string, any > = new Map();
 
     constructor ( res: TProfileResponse ) {
         this.raw = res.person;
         this.lists = res.person.personLists.sort(
             ( a, b ) => Number( b.date ?? 0 ) - Number( a.date ?? 0 )
         );
+    }
+
+    private cache< T = any > ( key: string, fn: () => T ) : T {
+        if ( ! this.cachedData.has( key ) ) this.cachedData.set( key, fn() );
+        return this.cachedData.get( key );
+    }
+
+    public uri () : string {
+        return this.cache( 'uri', () => Utils.sanitize( this.raw.uri ) );
+    }
+
+    public id () : string {
+        return this.cache( 'id', () => Utils.hash( this.raw.naturalId ) );
     }
 
 }
