58476ae64c660159b6c95f1796175b8138848b08
komed3 <pk97@outlook.de>
2026-01-23 11:06:16 +0100

    update pp interface

--- src/interfaces/parser.d.ts
+++ src/interfaces/parser.d.ts
@@ -1,4 +1,8 @@
+import { ProfileParser } from '@/parser/ProfileParser';
+
 export interface IProfileParser {
     uri () : string;
     id () : string;
+    aliases () : string[];
+    name () : ReturnType< typeof ProfileParser.name >;
 }
