7efdce27e8bd99a8d3819a213a0f9a2c4080aaf3
komed3 <pk97@outlook.de>
2026-01-25 11:26:39 +0100

    Update utils.d.ts

--- src/types/utils.d.ts
+++ src/types/utils.d.ts
@@ -1,11 +1,6 @@
 import { Profile } from '@/model/Profile';
 
-export interface TProfileOperation {
-    profile: Profile | null;
-    isExisting: boolean;
-    isSimilar: boolean;
-    action: 'create' | 'update' | 'merge';
-}
+export type TProfileOperation = 'create' | 'update' | 'merge';
 
 export interface TProfileLookupResult {
     profile: Profile | null;
