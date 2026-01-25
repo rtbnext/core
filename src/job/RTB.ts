99ea04a79d4d2d0b4d9b00cbd3b110e49b9400ea
komed3 <pk97@outlook.de>
2026-01-25 14:28:14 +0100

    import services

--- src/job/RTB.ts
+++ src/job/RTB.ts
@@ -1,7 +1,14 @@
 import { Job, jobRunner } from '@/abstract/Job';
+import { Fetch } from '@/core/Fetch';
+import { ProfileQueue } from '@/core/Queue';
 import { IJob } from '@/interfaces/job';
+import { Stats } from '@/model/Stats';
 
 export class RTBJob extends Job implements IJob {
+
+    private static readonly fetch = Fetch.getInstance();
+    private static readonly queue = ProfileQueue.getInstance();
+    private static readonly stats = Stats.getInstance();
 
     constructor ( args: string[] ) {
         super( args, 'RTB' );
