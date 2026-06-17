7af746ec6c5e07e01fc66c43df58a767742ecbcf
komed3 <pk97@outlook.de>
2026-06-17 19:28:09 +0200

    get meta data

--- src/core/Utils.ts
+++ src/core/Utils.ts
@@ -1,6 +1,12 @@
+import type { TMetaData } from '@rtbnext/schema/src/base/generic';
+
 export class Utils {
   public static date ( format: 'iso' | 'ymd' | 'ym' | 'y' = 'ymd' ) : string {
     const iso = new Date().toISOString();
     return format === 'iso' ? iso : iso.split( '-' ).slice( 0, format.length ).join( '-' );
+  }
+
+  public static metaData < T extends Record< string, any > > ( obj?: T ) : TMetaData {
+    return { $metadata: { schemaVersion: 2, lastModified: Utils.date( 'iso' ), ...obj } };
   }
 }
