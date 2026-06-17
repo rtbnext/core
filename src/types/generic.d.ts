de51ff39c60450b316a8c85880a1fa9911c3e236
komed3 <pk97@outlook.de>
2026-06-17 22:40:55 +0200

    declare aggregator type

--- src/types/generic.d.ts
+++ src/types/generic.d.ts
@@ -1,3 +1,7 @@
+export type TAggregator =
+  | 'all' | 'first' | 'last' | 'sum' | 'min' | 'max' | 'mean'
+  | ( ( values: readonly T[ K ][] ) => R );
+
 export type TMeasuredResult< R > = {
   result: R;
   ms: number;
