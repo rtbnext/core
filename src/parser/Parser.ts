ce0cb5cd095915f866acf4b88279d90223806d25
komed3 <pk97@outlook.de>
2026-06-17 19:39:49 +0200

    parse number types

--- src/parser/Parser.ts
+++ src/parser/Parser.ts
@@ -1,19 +1,31 @@
 import { REGEX_SPACES } from '@/lib/regex';
 
 export class Parser {
-  public static string ( value: any ) : string {
+  public static string ( value: unknown ) : string {
     return String( value ).trim().replace( REGEX_SPACES, ' ' );
   }
 
-  public static safeStr ( value: any, allowedTags?: string[] ) : string {
+  public static safeStr ( value: unknown, allowedTags?: string[] ) : string {
     return Parser.string( value ).replace( new RegExp( allowedTags?.length
       ? `<\\/?(?!(${ allowedTags.join( '|' ) })\\b)(\\w+)([^>]*)>` : '<[^>]*>', 'gi'
     ), '' ).replace( REGEX_SPACES, ' ' ).trim();
   }
 
-  public static boolean ( value: any ) : boolean {
+  public static boolean ( value: unknown ) : boolean {
     return value !== null && value !== undefined && ( typeof value === 'boolean' ? value :
       [ '1', 'true', 'yes', 'y' ].includes( Parser.string( value ).toLowerCase() )
     );
+  }
+
+  public static number ( value: unknown, digits: number = 0 ) : number {
+    return Number( Number( value ).toFixed( digits ) );
+  }
+
+  public static money ( value: unknown ) : number {
+    return Parser.number( value, 3 );
+  }
+
+  public static pct ( value: unknown, digits: number = 2 ) : number {
+    return Parser.number( value, digits );
   }
 }
