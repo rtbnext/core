a7daef140c654335c8031dd25b666d1d999edf98
komed3 <pk97@outlook.de>
2026-06-17 23:00:53 +0200

    declare basic constants

--- src/lib/const.ts
+++ src/lib/const.ts
@@ -1,16 +1,33 @@
-import type { TGender, TIndustry, TMaritalStatus } from '@rtbnext/schema/src/base/const';
+import type { TAssetType, TChangeFlag, TGender, TIndustry, TMaritalStatus, TRelationType, TSelfMadeRank } from '@rtbnext/schema/src/base/const';
+
+// --- basics ---
+
+export const Industry: TIndustry[] = [
+  'automotive', 'diversified', 'energy', 'engineering', 'finance', 'foodstuff', 'gambling',
+  'healthcare', 'logistics', 'manufacturing', 'media', 'mining', 'property', 'retail', 'service',
+  'sports', 'technology', 'telecom'
+] as const;
 
 export const Gender: TGender[] = [
   'm', 'f', 'd'
 ] as const;
 
 export const MaritalStatus: TMaritalStatus[] = [
-    'single', 'relationship', 'married', 'remarried', 'engaged',
-    'separated', 'divorced', 'widowed'
+  'single', 'relationship', 'married', 'remarried', 'engaged', 'separated', 'divorced', 'widowed'
 ] as const;
 
-export const Industry: TIndustry[] = [
-    'automotive', 'diversified', 'energy', 'engineering', 'finance', 'foodstuff',
-    'gambling', 'healthcare', 'logistics', 'manufacturing', 'media', 'mining',
-    'property', 'retail', 'service', 'sports', 'technology', 'telecom'
+export const SelfMadeRank: TSelfMadeRank[] = [
+  '1', '2', '3', '4', '5', '6', '7', '8', '9', '10'
+] as const;
+
+export const RelationType: TRelationType[] = [
+  'person', 'organization', 'place', 'unknown'
+] as const;
+
+export const AssetType: TAssetType[] = [
+  'public', 'private', 'misc'
+] as const;
+
+export const ChangeFlag: TChangeFlag[] = [
+  'up', 'down', 'unchanged'
 ] as const;
