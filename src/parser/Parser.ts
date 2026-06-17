feb00b37f6a049dcb3c5fc8dc3dd9a0939a558cf
komed3 <pk97@outlook.de>
2026-06-17 19:37:14 +0200

    implement "safeStr" parser method to remove tags from strings

--- src/parser/Parser.ts
+++ src/parser/Parser.ts
@@ -4,4 +4,10 @@ export class Parser {
   public static string ( value: any ) : string {
     return String( value ).trim().replace( REGEX_SPACES, ' ' );
   }
+
+  public static safeStr ( value: any, allowedTags?: string[] ) : string {
+    return Parser.string( value ).replace( new RegExp( allowedTags?.length
+      ? `<\\/?(?!(${ allowedTags.join( '|' ) })\\b)(\\w+)([^>]*)>` : '<[^>]*>', 'gi'
+    ), '' ).replace( REGEX_SPACES, ' ' ).trim();
+  }
 }
