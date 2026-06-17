b6af852eacca41cee46f298eb552cd7d9fca617d
komed3 <pk97@outlook.de>
2026-06-17 23:34:17 +0200

    declare config class vars

--- src/core/Config.ts
+++ src/core/Config.ts
@@ -1,6 +1,14 @@
 import type { IConfig } from '@/interface/config';
+import type { TConfigObject } from '@/type/config';
 
 
 export class Config implements IConfig {
+  private static instance: Config;
+
+  private readonly cwd: string;
+  private readonly path: string;
+  private readonly env: string;
+  private readonly cfg: TConfigObject;
+
   private constructor () {}
 }
