1f4ced99ab97dd7015ccf87f664a2698690fa805
komed3 <pk97@outlook.de>
2026-06-17 19:55:01 +0200

    add parser primitive helper

--- src/parser/Parser.ts
+++ src/parser/Parser.ts
@@ -1,8 +1,12 @@
+import type { Primitive } from 'devtypes/types/primitive';
+
 import { REGEX_SPACES } from '@/lib/regex';
 import type { TParserDateType } from '@/type/parser';
 
 
 export class Parser {
+  // --- primitive ---
+
   public static string ( value: unknown ) : string {
     return String( value ).trim().replace( REGEX_SPACES, ' ' );
   }
@@ -34,5 +38,14 @@ export class Parser {
   public static date ( value?: any, format: TParserDateType = 'ymd' ) : string | undefined {
     try { value = new Date( value ).toISOString() } catch { return undefined }
     return format === 'iso' ? value : value.split( '-' ).slice( 0, format.length ).join( '-' );
+  }
+
+  // --- helper ---
+
+  public static primitive ( value: unknown, safe: boolean = true ) : Primitive {
+    return value === null || value === undefined ? value
+      : typeof value === 'boolean' ? value
+      : ! isNaN( Number( value ) ) && value !== '' ? Parser.number( value )
+      : safe ? Parser.safeStr( value ) : Parser.string( value );
   }
 }
