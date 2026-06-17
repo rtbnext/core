cd7354728a81d9e72890eef98d9b824b2c8cda61
komed3 <pk97@outlook.de>
2026-06-17 19:36:16 +0200

    parse strings

--- src/parser/Parser.ts
+++ src/parser/Parser.ts
@@ -1,1 +1,7 @@
-export class Parser {}
+import { REGEX_SPACES } from '@/lib/regex';
+
+export class Parser {
+  public static string ( value: any ) : string {
+    return String( value ).trim().replace( REGEX_SPACES, ' ' );
+  }
+}
