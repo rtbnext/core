49c51da5596a35c3f57f48b8a2d7248f4e910001
komed3 <pk97@outlook.de>
2026-06-18 14:20:19 +0200

    implement fetch helper

--- src/core/Fetch.ts
+++ src/core/Fetch.ts
@@ -1,15 +1,19 @@
-import axios, { type AxiosInstance } from 'axios';
+import type { AxiosInstance, AxiosResponse } from 'axios';
+import axios from 'axios';
 
+import { Config } from '@/core/Config';
+import { log } from '@/core/Logger';
+import { Utils } from '@/core/Utils';
 import type { IFetch } from '@/interface/fetch';
 import type { TFetchConfig } from '@/type/config';
-import { Config } from './Config';
+import type { TResponse } from '@/type/response';
 
 
 export class Fetch implements IFetch {
   private static instance: Fetch;
 
-  private readonly wikiQuery = { format: 'json', formatversion: 2 };
   private readonly config: TFetchConfig;
+  private readonly wikiQuery = { format: 'json', formatversion: 2 };
   private httpClient: AxiosInstance;
 
   private constructor () {
@@ -35,6 +39,37 @@ export class Fetch implements IFetch {
     const delay = Math.round( Math.random() * ( max - min ) + min );
 
     return new Promise( resolve => setTimeout( resolve, delay ) );
+  }
+
+  // --- fetch ---
+
+  private async fetch < T > ( url: string, method: 'get' | 'post' = 'get' ) : Promise< TResponse< T > > {
+    log.debug( `Fetching URL: ${ url } via ${ method.toUpperCase() }` );
+
+    const { result: res, ms } = await Utils.measure( async () => {
+      let res: AxiosResponse< T, any, {} >;
+      let retries = 0;
+
+      do {
+        const headers = { ...this.config.headers, 'User-Agent': this.getRandomUserAgent() };
+        res = await this.httpClient[ method ]< T >( url, { headers } );
+        if ( res.status === 200 && res.data ) break;
+
+        log.warn( `Request failed with status: ${ res.status }. Retrying ...` );
+        await this.getRandomDelay();
+      } while ( ++retries < this.config.rateLimit.retries );
+
+      return { ...res, retries };
+    } );
+
+    log.debug( `Fetched URL: ${ url } in ${ ms } ms` );
+
+    return Object.assign( { duration: ms, retries: res.retries },
+      res.status === 200 && res.data ? { success: true, data: res.data } : {
+        success: false, error: `Invalid response status: ${ res.status }`,
+        statusCode: res.status
+      }
+    );
   }
 
   // --- instantiate ---
