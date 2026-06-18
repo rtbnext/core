6ab6678296a151a7a69e3d9cbe6bd0cbe7ffdd09
komed3 <pk97@outlook.de>
2026-06-18 02:29:49 +0200

    Update logger.d.ts

--- src/interfaces/logger.d.ts
+++ src/interfaces/logger.d.ts
@@ -5,4 +5,12 @@ export interface ILogger {
   warn ( label: string, msg: string, meta?: unknown ) : void;
   info ( label: string, msg: string, meta?: unknown ) : void;
   debug ( label: string, msg: string, meta?: unknown ) : void;
+  catch < F extends ( ...args: any[] ) => any, R = ReturnType< F > > (
+    fn: F, label: string, msg: string, level: TLoggingLevel = 'error'
+  ) : R | undefined;
+  catchAsync < F extends ( ...args: any[] ) => Promise< any >, R = Awaited< ReturnType< F > > > (
+    fn: F, label: string, msg: string, level: TLoggingLevel = 'error'
+  ) : Promise< R | undefined >;
+  getLogFile ( date: string ) : string | undefined;
+  getCurrentLogFile () : string | undefined;
 }
