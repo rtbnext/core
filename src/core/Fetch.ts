703e07c292182a3def3e7bb16925fcac15f2b45c
komed3 <pk97@outlook.de>
2026-06-18 14:12:08 +0200

    prep. fetch class

---
+++ src/core/Fetch.ts
@@ -0,0 +1,18 @@
+import { type AxiosInstance } from 'axios';
+
+import type { IFetch } from '@/interface/fetch';
+import type { TFetchConfig } from '@/type/config';
+import { Config } from './Config';
+
+
+export class Fetch implements IFetch {
+  private static instance: Fetch;
+
+  private readonly wikiQuery = { format: 'json', formatversion: 2 };
+  private readonly config: TFetchConfig;
+  private httpClient: AxiosInstance;
+
+  private constructor () {
+    this.config = Config.getInstance().fetch;
+  }
+}
