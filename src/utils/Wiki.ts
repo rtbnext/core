ace7ce78f1e81e6f94143df0995ae3c51360b093
komed3 <pk97@outlook.de>
2026-06-19 01:49:46 +0200

    query wiki page

--- src/utils/Wiki.ts
+++ src/utils/Wiki.ts
@@ -1,10 +1,10 @@
-import type { TImage } from '@rtbnext/schema/src/base/generic';
+import type { TImage, TWiki } from '@rtbnext/schema/src/base/generic';
 import type { TProfileData } from '@rtbnext/schema/src/model/profile';
 
 import { Fetch } from '@/core/Fetch';
 import { log } from '@/core/Logger';
 import { Parser } from '@/parser/Parser';
-import type { TCommonsResponse, TWikidataResponse, TWikidataResponseItem } from '@/type/response';
+import type { TCommonsResponse, TWikidataResponse, TWikidataResponseItem, TWikipediaResponse } from '@/type/response';
 import type { TWikidata } from '@/type/wiki';
 
 
@@ -134,5 +134,43 @@ export class Wiki {
         credits: { value: credits, type: 'safeStr' }
       } );
     }, `Failed to query Wikimedia Commons image: ${ title }` );
+  }
+
+  public static async queryWikiPage (
+    title: string, qid?: string, image?: TImage, confidence: number = 1
+  ) : Promise< TWiki | undefined > {
+    log.debug( `Querying Wikipedia page: ${ title }` );
+
+    return await log.catchAsync( async () => {
+      const res = await Wiki.fetch.wikipedia< TWikipediaResponse >( {
+        action: 'query', prop: 'extracts|info|pageprops|pageimages',
+        titles: title, redirects: 1, exintro: 1, explaintext: 1,
+        exsectionformat: 'plain', piprop: 'name', pilimit: 1
+      } );
+
+      if ( ! res?.success || ! res.data || ! res.data.query.pages.length )
+        throw new Error( `No Wikipedia page found for: ${ title }` );
+
+      log.debug( `Wikipedia page info received for: ${ title }` );
+      const raw = res.data.query.pages[ 0 ];
+
+      if ( ! image && raw.pageimage ) {
+        log.debug( `Querying page image from Wikimedia Commons: ${ raw.pageimage }` );
+        image = await this.queryCommonsImage( raw.pageimage );
+      }
+
+      return { image, ...Parser.container< TWiki >( {
+        uri: { value: title, type: 'string' },
+        pageId: { value: raw.pageid, type: 'number' },
+        refId: { value: raw.lastrevid, type: 'number' },
+        confidence: { value: confidence, type: 'number', args: [ 3 ] },
+        name: { value: raw.title, type: 'string' },
+        lastModified: { value: raw.touched, type: 'date', args: [ 'iso' ] },
+        summary: { value: raw.extract ?? '', type: 'list', args: [ 'safeStr', '\n' ], strict: false },
+        sortKey: { value: raw.pageprops?.[ 'defaultsort' ], type: 'string' },
+        wikidata: { value: qid ?? raw.pageprops?.[ 'wikibase_item' ], type: 'string' },
+        desc: { value: raw.pageprops?.[ 'wikibase-shortdesc' ], type: 'safeStr' }
+      } ) };
+    }, `Failed to query Wikipedia page: ${ title }` );
   }
 }
