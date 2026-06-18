94888a475f77ff7246364ccd96459c60b3cde36e
komed3 <pk97@outlook.de>
2026-06-18 02:06:09 +0200

    declare log level

--- src/core/Logger.ts
+++ src/core/Logger.ts
@@ -3,11 +3,15 @@ import { join } from 'node:path';
 
 import { Config } from '@/core/Config';
 import type { ILogger } from '@/interface/logger';
-import type { TLoggingConfig } from '@/type/config';
+import type { TLoggingConfig, TLoggingLevel } from '@/type/config';
 
 
 export class Logger implements ILogger {
   private static instance: Logger;
+
+  private static readonly LEVEL: Record< TLoggingLevel, number > = {
+    error: 0, warn: 1, info: 2, debug: 3
+  };
 
   private readonly config: TLoggingConfig;
   private readonly path: string;
