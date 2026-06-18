b82ed07ded4a622050949a739743c8f03fd77382
komed3 <pk97@outlook.de>
2026-06-18 11:05:10 +0200

    Update storage.d.ts

--- src/interfaces/storage.d.ts
+++ src/interfaces/storage.d.ts
@@ -7,10 +7,10 @@ export interface IStorage {
   scanDir ( path: string, ext: string[] = [ 'json', 'csv' ] ) : string[];
   readJSON < T extends object > ( path: string ) : T | false;
   writeJSON < T extends object > ( path: string, content: T ) : boolean;
-  readCSV< T extends any[] > ( path: string ) : T | false;
-  writeCSV< T extends any[] > ( path: string, content: T ) : boolean;
-  appendCSV< T extends any[] > ( path: string, content: T, nl: boolean = true ) : boolean;
-  datedCSV< T extends any[] > ( path: string, content: T, force: boolean = false ) : boolean;
+  readCSV < T extends any[] > ( path: string ) : T | false;
+  writeCSV < T extends any[] > ( path: string, content: T ) : boolean;
+  appendCSV < T extends any[] > ( path: string, content: T, nl: boolean = true ) : boolean;
+  datedCSV < T extends any[] > ( path: string, content: T, force: boolean = false ) : boolean;
   remove ( path: string, force: boolean = true ) : boolean;
   move ( from: string, to: string, force: boolean = false ) : boolean;
 }
