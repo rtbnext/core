005f9ecf9720d63e6efc108134d4eae120a54e64
komed3 <pk97@outlook.de>
2026-06-18 02:05:24 +0200

    instantiate logger

--- src/core/Logger.ts
+++ src/core/Logger.ts
@@ -1,8 +1,23 @@
+import { mkdirSync } from 'node:fs';
+import { join } from 'node:path';
+
+import { Config } from '@/core/Config';
 import type { ILogger } from '@/interface/logger';
+import type { TLoggingConfig } from '@/type/config';
 
 
 export class Logger implements ILogger {
   private static instance: Logger;
+
+  private readonly config: TLoggingConfig;
+  private readonly path: string;
+
+  private constructor () {
+    const { root, logging } = Config.getInstance();
+    this.config = logging;
+    this.path = join( root, 'logs' );
+    mkdirSync( this.path, { recursive: true } );
+  }
 
   // --- instantiate ---
 
