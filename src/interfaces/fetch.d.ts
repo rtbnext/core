3b7100b0da9b3c3e3e25d3987206aed7c656f40e
komed3 <pk97@outlook.de>
2026-06-18 19:30:00 +0200

    implement IFetch interface

--- src/interfaces/fetch.d.ts
+++ src/interfaces/fetch.d.ts
@@ -1,1 +1,14 @@
-export interface IFetch {}
+import type { TFetchMethod } from '@/type/fetch';
+import type { TListResponse, TProfileResponse, TResponse } from '@/type/response';
+
+
+export interface IFetch {
+  single < T > ( url: string, method: TFetchMethod = 'get' ) : Promise< TResponse< T > >;
+  batch < T > ( urls: string[], method: TFetchMethod = 'get' ) : Promise< TResponse< T >[] >;
+  wayback < T > ( url: string, ts: unknown ) : Promise< TResponse< T > >;
+  list < T extends object > ( uriLike: string, year: string ) : Promise< TResponse< TListResponse< T > > >;
+  profile ( ...uriLike: string[] ) : Promise< TResponse< TProfileResponse >[] >;
+  wikidata < T > ( sparql: string ) : Promise< TResponse< T > >;
+  wikipedia < T > ( query: Record< string, unknown >, lang: string = 'en' ) : Promise< TResponse< T > >;
+  commons < T > ( query: Record< string, unknown > ) : Promise< TResponse< T > >;
+}
