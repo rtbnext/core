aa54c8474795b82f206bd31c526718dcb41f0752
komed3 <pk97@outlook.de>
2026-06-18 15:06:49 +0200

    declare the forbes profile response type

--- src/types/response.d.ts
+++ src/types/response.d.ts
@@ -17,3 +17,86 @@ export type TWaybackResponse = {
     };
   };
 };
+
+export type TProfileResponse = {
+  person: {
+    naturalId: string;
+    name: string;
+    listImages?: Array< {
+      image: string;
+      uri: string;
+      caption: string;
+      description: string;
+      title: string;
+      credit: string;
+    } >;
+    geoLocation?: {
+      longitude: number;
+      latitude: number;
+    };
+    relatedEntities?: Array< {
+      name: string;
+      type: string;
+      uri?: string;
+      relationshipType?: string;
+    } >;
+    uri: string;
+    dropOff: boolean;
+    uris: string[];
+    industries: string[];
+    embargo?: boolean;
+    firstName?: string;
+    lastName?: string;
+    birthDate?: number;
+    finalWorth: number;
+    finalWorthDate: number;
+    stateProvince?: string;
+    city?: string;
+    numberOfChildren?: number;
+    maritalStatus?: 'M' | 'F';
+    source: string;
+    title?: string;
+    organization?: string;
+    asianFormat?: 'N' | 'Y';
+    personLists: Array< {
+      listUri: string;
+      name: string;
+      listDescription?: string;
+      rank?: number;
+      finalWorth?: number;
+      timestamp: number | string;
+      financialAssets?: Array< {
+        exchange: string;
+        ticker: string;
+        companyName: string;
+        numberOfShares?: number;
+        sharePrice: number;
+        exchangeRate?: number;
+        currentPrice?: number;
+      } >;
+      date?: number | string;
+      estWorthPrev?: number;
+      privateAssetsWorth?: number;
+      familyList?: boolean;
+      archivedWorth?: number;
+      bios?: string[];
+      abouts?: string[];
+      philanthropyScore?: number;
+    } >;
+    educations?: Array< {
+      school: string;
+      degree: string;
+    } >;
+    selfMade?: boolean;
+    gender: string;
+    countryOfCitizenship: string;
+    countryOfResidence?: string;
+    birthCity?: string;
+    birthState?: string;
+    birthCountry?: string;
+    deceased: boolean;
+    selfMadeType?: string;
+    selfMadeRank?: number;
+    quote?: string;
+  };
+};
