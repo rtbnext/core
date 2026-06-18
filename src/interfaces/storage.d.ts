c9470f9c36bf827a090858224b13f025a5583b7c
komed3 <pk97@outlook.de>
2026-06-18 11:04:58 +0200

    implement storage cls interface

--- src/interfaces/storage.d.ts
+++ src/interfaces/storage.d.ts
@@ -1,1 +1,16 @@
-export interface IStorage {}
+export interface IStorage {
+  readonly root: string;
+  exists ( path: string ) : boolean;
+  assertPath ( path: string ) : void | never;
+  ensurePath ( path: string, isDir: boolean = false ) : void;
+  stat ( path: string ) : Stats | false;
+  scanDir ( path: string, ext: string[] = [ 'json', 'csv' ] ) : string[];
+  readJSON < T extends object > ( path: string ) : T | false;
+  writeJSON < T extends object > ( path: string, content: T ) : boolean;
+  readCSV< T extends any[] > ( path: string ) : T | false;
+  writeCSV< T extends any[] > ( path: string, content: T ) : boolean;
+  appendCSV< T extends any[] > ( path: string, content: T, nl: boolean = true ) : boolean;
+  datedCSV< T extends any[] > ( path: string, content: T, force: boolean = false ) : boolean;
+  remove ( path: string, force: boolean = true ) : boolean;
+  move ( from: string, to: string, force: boolean = false ) : boolean;
+}
