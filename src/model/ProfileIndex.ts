1d60b173c4d16f8f7a3425dbc20e52461754be18
komed3 <pk97@outlook.de>
2026-06-19 13:25:27 +0200

    find by alias

--- src/model/ProfileIndex.ts
+++ src/model/ProfileIndex.ts
@@ -1,7 +1,8 @@
 import type { TProfileIndex, TProfileIndexItem } from '@rtbnext/schema/src/model/profile';
 
 import { Index } from '@/abstract/Index';
-import type { IProfileIndex } from '../interfaces';
+import { Utils } from '@/core/Utils';
+import type { IProfileIndex } from '@/interface/index';
 
 
 export class ProfileIndex extends Index< TProfileIndexItem, TProfileIndex > implements IProfileIndex {
@@ -9,6 +10,13 @@ export class ProfileIndex extends Index< TProfileIndexItem, TProfileIndex > impl
 
   private constructor () {
     super( 'profile', 'profile/index.json' );
+  }
+
+  // --- special profile index operations ---
+
+  public find ( uriLike: string ) : TProfileIndex {
+    const uri = Utils.sanitize( uriLike );
+    return new Map( [ ...this.index ].filter( ( [ key, { aliases } ] ) => key === uri || aliases.includes( uri ) ) );
   }
 
   // --- instantitate ---
