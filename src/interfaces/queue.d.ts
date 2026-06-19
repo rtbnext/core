55ddd10343d023a121b2b63f43205469252501c1
komed3 <pk97@outlook.de>
2026-06-19 23:51:39 +0200

    declare queue class interface

--- src/interfaces/queue.d.ts
+++ src/interfaces/queue.d.ts
@@ -1,1 +1,18 @@
-export interface IQueue {}
+import type { TQueueItem, TQueueOptions } from '@/type/queue';
+
+
+export interface IQueue {
+  readonly size: number;
+  getQueue () : TQueueItem[];
+  getByKey ( key: string ) : TQueueItem | undefined;
+  hasKey ( key: string ) : boolean;
+  getByUri ( uriLike: string ) : TQueueItem[];
+  hasUri ( uriLike: string ) : boolean;
+  clear () : void;
+  add ( opt: TQueueOptions, save: boolean = true ) : boolean;
+  addMany ( items: TQueueOptions[] ) : number;
+  removeByKey ( key: string ) : boolean;
+  remove ( ...uriLike: string[] ) : number;
+  next ( n: number = 1 ) : TQueueItem[];
+  nextUri ( n: number = 1 ) : string[];
+}
