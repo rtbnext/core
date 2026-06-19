9b62e40c096e463534db5322c3d424b1b7034722
komed3 <pk97@outlook.de>
2026-06-19 14:44:23 +0200

    create profile class

---
+++ src/model/Profile.ts
@@ -0,0 +1,38 @@
+import type { TMetaData } from '@rtbnext/schema/src/base/generic';
+import type { TProfileData, TProfileHistory, TProfileIndexItem } from '@rtbnext/schema/src/model/profile';
+import { join } from 'node:path';
+
+import { Storage } from '@/core/Storage';
+import type { IProfile } from '@/interface/profile';
+import { ProfileIndex } from '@/model/ProfileIndex';
+import { Utils } from '@/core/Utils';
+
+
+export class Profile implements IProfile {
+  private static readonly storage = Storage.getInstance();
+  private static readonly index = ProfileIndex.getInstance();
+
+  private uri: string;
+  private path: string;
+  private item: TProfileIndexItem;
+  private data?: TProfileData;
+  private history?: TProfileHistory;
+  private meta: TMetaData;
+
+  private constructor ( item?: TProfileIndexItem ) {
+    if ( ! item ) throw new Error( `Profile index item not given` );
+
+    this.uri = item.uri;
+    this.path = join( 'profile', item.uri );
+    this.item = item;
+
+    Profile.storage.ensurePath( this.path, true );
+    this.meta = Profile.storage.readJSON< TMetaData >( this.resolvePath( 'meta.json' ) ) || Utils.metaData();
+  }
+
+  // --- helper ---
+
+  private resolvePath ( path: string ) : string {
+    return join( this.path, path );
+  }
+}
