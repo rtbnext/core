42342135f64d610ff84b58d565fd803764b9b490
komed3 <pk97@outlook.de>
2026-06-19 18:34:29 +0200

    check mergeable profiles

--- src/utils/ProfileMerger.ts
+++ src/utils/ProfileMerger.ts
@@ -1,3 +1,4 @@
+import type { TProfileData, TProfileInfo } from '@rtbnext/schema/src/model/profile';
 import { CmpStrAsync, type CmpStrResult } from 'cmpstr';
 
 import { REGEX_URI_CLEANUP } from '@/lib/regex';
@@ -20,5 +21,19 @@ export class ProfileMerger {
       ...ProfileMerger.cmp.match< CmpStrResult[] >( keys, uri, 0.9 ).map( i => i.source ),
       ...ProfileMerger.cmp.match< CmpStrResult[] >( keys, revUri, 0.8 ).map( i => i.source )
     ] ) ];
+  }
+
+  // --- check mergeable profiles ---
+
+  public static mergeableProfiles ( target: TProfileData, source: TProfileData ) : boolean {
+    if ( target.id === source.id ) return true;
+
+    for ( const test of [ 'gender', 'birthDate', 'birthPlace', 'citizenship', 'industry' ] ) if (
+      test in target.info && test in source.info &&
+      JSON.stringify( target.info[ test as keyof TProfileInfo ] ) !==
+      JSON.stringify( source.info[ test as keyof TProfileInfo ] )
+    ) return false;
+
+    return true;
   }
 }
