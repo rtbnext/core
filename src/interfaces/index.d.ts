4c5f4ac4619316636912bc5486a4d8bfeafa876b
komed3 <pk97@outlook.de>
2026-06-19 13:19:05 +0200

    declare index cls interface

--- src/interfaces/index.d.ts
+++ src/interfaces/index.d.ts
@@ -1,4 +1,14 @@
 import type { TIndex } from '@rtbnext/schema/src/base/generic';
 
 
-export interface IIndex< I extends TIndex, T extends Map< string, I > > {}
+export interface IIndex< I extends TIndex, T extends Map< string, I > > {
+  readonly size: number;
+  getIndex () : T;
+  has ( uriLike: string ) : boolean;
+  get ( uriLike: string ) : I | undefined;
+  update ( uriLike: string, data: Partial< I >, allowUpdate: boolean = true, save: boolean = true ) : I | false;
+  delta ( items: { uriLike: string, data: Partial< I > }[], allowUpdate: boolean = true ) : number;
+  add ( uriLike: string, data: I ) : I | false;
+  delete ( uriLike: string ) : void;
+  search ( query: string, looseMatch: boolean = false ) : T;
+}
