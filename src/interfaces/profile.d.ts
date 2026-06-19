3e29c4826a11386f26cd11d205c2120731a8b06c
komed3 <pk97@outlook.de>
2026-06-19 17:19:32 +0200

    describe profile interface

--- src/interfaces/profile.d.ts
+++ src/interfaces/profile.d.ts
@@ -1,1 +1,30 @@
-export interface IProfile {}
+import type { ArrayMode } from '@komed3/deepmerge';
+import type { TProfileData, TProfileHistory, TProfileHistoryItem, TProfileIndexItem, TProfileMetaData } from '@rtbnext/schema/src/model/profile';
+
+
+export interface IProfile {
+  getUri () : string;
+  getItem () : TProfileIndexItem;
+  getMeta () : TProfileMetaData[ '$metadata' ];
+  schemaVersion () : number;
+  lastModified () : string;
+  lastModifiedTime () : number;
+  lastLookup () : string;
+  lastLookupTime () : number;
+  verify ( id: string ) : boolean;
+  getData () : TProfileData;
+  setData (
+    data: TProfileData, aliases?: string[], lookup: boolean = false,
+    aliasMode: ArrayMode = ArrayMode.Unique
+  ) : void;
+  updateData (
+    data: Partial< TProfileData >, aliases?: string[], lookup: boolean = false,
+    mode: ArrayMode = ArrayMode.Replace, aliasMode: ArrayMode = ArrayMode.Unique
+  ) : void;
+  getHistory () : TProfileHistory;
+  setHistory ( history: TProfileHistory ) : void;
+  addHistory ( row: TProfileHistoryItem ) : void;
+  mergeHistory ( history: TProfileHistory ) : void;
+  save () : void;
+  move ( uriLike: string, makeAlias: boolean = true ) : boolean;
+}
