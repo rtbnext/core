f61bbb0bfd7ce473c4522cb184ebdafd275bf62f
komed3 <pk97@outlook.de>
2026-06-19 10:41:12 +0200

    cache data

--- src/abstract/Cache.ts
+++ src/abstract/Cache.ts
@@ -1,4 +1,11 @@
 import type { ICache } from '@/interface/cache';
 
 
-export abstract class Cache implements ICache {}
+export abstract class Cache< K extends string, T = unknown > implements ICache {
+  protected cachedData: Map< K, T > = new Map();
+
+  protected cache ( key: K, fn: () => T ) : T {
+    if ( ! this.cachedData.has( key ) ) this.cachedData.set( key, fn() );
+    return this.cachedData.get( key )!;
+  }
+}
