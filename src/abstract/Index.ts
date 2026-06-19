b902a5471bd509875b0a39941c7a8713a31c0da9
komed3 <pk97@outlook.de>
2026-06-19 13:05:41 +0200

    instantiate index

--- src/abstract/Index.ts
+++ src/abstract/Index.ts
@@ -6,4 +6,13 @@ import type { IIndex } from '../interfaces';
 
 export abstract class Index< I extends TIndex, T extends Map< string, I > > implements IIndex< I, T > {
   protected static readonly storage = Storage.getInstance();
+  protected readonly type: 'profile' | 'list';
+  protected readonly path: string;
+  protected index: T;
+
+  protected constructor ( type: 'profile' | 'list', path: string ) {
+    this.type = type;
+    this.path = path;
+    Index.storage.ensurePath( this.path );
+  }
 }
