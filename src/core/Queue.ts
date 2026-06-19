40841c41373283e9708e7054fb8aaa675bfb8d9e
komed3 <pk97@outlook.de>
2026-06-19 23:33:21 +0200

    instantiate queue

--- src/core/Queue.ts
+++ src/core/Queue.ts
@@ -1,4 +1,28 @@
+import { join } from 'node:path';
+
+import { Config } from '@/core/Config';
+import { Storage } from '@/core/Storage';
 import type { IQueue } from '@/interface/queue';
+import type { TQueueConfig } from '@/type/config';
+import type { TQueue, TQueueType } from '@/type/queue';
 
 
-abstract class Queue implements IQueue {}
+export abstract class Queue implements IQueue {
+  protected static readonly storage = Storage.getInstance();
+
+  protected readonly config: TQueueConfig;
+  protected readonly type: TQueueType;
+  protected readonly path: string;
+  protected queue: TQueue;
+
+  protected constructor ( type: TQueueType ) {
+    const { root, queue } = Config.getInstance();
+    this.config = queue;
+    this.type = type;
+
+    this.path = join( root, `queue/${ this.type }.json` );
+    Queue.storage.ensurePath( this.path );
+
+    this.queue = this.loadQueue();
+  }
+}
