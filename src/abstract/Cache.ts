e18c7b56cf01aaa3add049b99d0aaff9c02e5e9a
komed3 <pk97@outlook.de>
2026-06-19 10:41:54 +0200

    Update Cache.ts

--- src/abstract/Cache.ts
+++ src/abstract/Cache.ts
@@ -8,4 +8,20 @@ export abstract class Cache< K extends string, T = unknown > implements ICache {
     if ( ! this.cachedData.has( key ) ) this.cachedData.set( key, fn() );
     return this.cachedData.get( key )!;
   }
+
+  public get size () : number {
+    return this.cachedData.size;
+  }
+
+  public get ( key: K ) : T | undefined {
+    return this.cachedData.get( key );
+  }
+
+  public has ( key: K ) : boolean {
+    return this.cachedData.has( key );
+  }
+
+  public clear () : void {
+    this.cachedData.clear();
+  }
 }
