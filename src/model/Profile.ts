1684658bab52f01e0fe90c0790e379ab01026e25
komed3 <pk97@outlook.de>
2026-06-19 14:54:46 +0200

    load meta data

--- src/model/Profile.ts
+++ src/model/Profile.ts
@@ -1,11 +1,10 @@
-import type { TMetaData } from '@rtbnext/schema/src/base/generic';
-import type { TProfileData, TProfileHistory, TProfileIndexItem } from '@rtbnext/schema/src/model/profile';
+import type { TProfileData, TProfileHistory, TProfileIndexItem, TProfileMetaData } from '@rtbnext/schema/src/model/profile';
 import { join } from 'node:path';
 
 import { Storage } from '@/core/Storage';
+import { Utils } from '@/core/Utils';
 import type { IProfile } from '@/interface/profile';
 import { ProfileIndex } from '@/model/ProfileIndex';
-import { Utils } from '@/core/Utils';
 
 
 export class Profile implements IProfile {
@@ -15,9 +14,9 @@ export class Profile implements IProfile {
   private uri: string;
   private path: string;
   private item: TProfileIndexItem;
+  private meta: TProfileMetaData;
   private data?: TProfileData;
   private history?: TProfileHistory;
-  private meta: TMetaData;
 
   private constructor ( item?: TProfileIndexItem ) {
     if ( ! item ) throw new Error( `Profile index item not given` );
@@ -27,12 +26,24 @@ export class Profile implements IProfile {
     this.item = item;
 
     Profile.storage.ensurePath( this.path, true );
-    this.meta = Profile.storage.readJSON< TMetaData >( this.resolvePath( 'meta.json' ) ) || Utils.metaData();
+    this.meta = this.metaData();
   }
 
   // --- helper ---
 
   private resolvePath ( path: string ) : string {
     return join( this.path, path );
+  }
+
+  private metaData () : TProfileMetaData {
+    return Profile.storage.readJSON< TProfileMetaData >(
+      this.resolvePath( 'meta.json' ) ) ||
+      Utils.metaData( { lastLookup: Utils.date( 'iso' ) }
+    );
+  }
+
+  private touch ( lookup: boolean = false ) : void {
+    this.meta.$metadata.lastModified = Utils.date( 'iso' );
+    if ( lookup ) this.meta.$metadata.lastLookup = this.meta.$metadata.lastModified;
   }
 }
