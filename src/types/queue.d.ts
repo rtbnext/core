0149ff5e342748311aca21dd276d4f60c5c79110
komed3 <pk97@outlook.de>
2026-06-19 23:31:07 +0200

    Update queue.d.ts

--- src/types/queue.d.ts
+++ src/types/queue.d.ts
@@ -1,18 +1,18 @@
 export type TQueueType = 'profile' | 'list';
 
-export interface TQueueOptions {
+export type TQueueOptions = {
   uriLike: string;
   args?: Record< string, unknown >;
   prio?: number;
-}
+};
 
-export interface TQueueItem {
+export type TQueueItem = {
   readonly key: string;
   readonly uri: string;
   ts: string;
   args?: Record< string, unknown >;
   prio?: number;
-}
+};
 
 export type TQueue = Map< string, TQueueItem >;
 
