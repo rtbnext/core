8433a53836de8d336fc9dcb1fd50d842733a3036
komed3 <pk97@outlook.de>
2026-06-19 23:36:32 +0200

    add helper methods for queue

--- src/core/Queue.ts
+++ src/core/Queue.ts
@@ -1,10 +1,11 @@
+import { sha256 } from 'js-sha256';
 import { join } from 'node:path';
 
 import { Config } from '@/core/Config';
 import { Storage } from '@/core/Storage';
 import type { IQueue } from '@/interface/queue';
 import type { TQueueConfig } from '@/type/config';
-import type { TQueue, TQueueType } from '@/type/queue';
+import type { TQueue, TQueueItem, TQueueStorage, TQueueType } from '@/type/queue';
 
 
 export abstract class Queue implements IQueue {
@@ -24,5 +25,28 @@ export abstract class Queue implements IQueue {
     Queue.storage.ensurePath( this.path );
 
     this.queue = this.loadQueue();
+  }
+
+  // --- helper ---
+
+  protected loadQueue () : TQueue {
+    return new Map( ( Queue.storage.readJSON< TQueueStorage >( this.path ) || [] ).map(
+      ( i: TQueueItem ) => [ i.key, i ]
+    ) );
+  }
+
+  protected saveQueue () : void {
+    const { defaultPrio = 0 } = this.config;
+
+    Queue.storage.writeJSON< TQueueStorage >( this.path,
+      Array.from( this.queue.values() ).sort( ( a: TQueueItem, b: TQueueItem ) =>
+        ( b.prio ?? defaultPrio ) - ( a.prio ?? defaultPrio ) || 
+        ( new Date( a.ts ).getTime() - new Date( b.ts ).getTime() )
+      )
+    );
+  }
+
+  protected key ( uri: string, args?: unknown ) : string {
+    return sha256( uri + JSON.stringify( args ) );
   }
 }
