64e1c033f99710d5dc120ae5b5ed96fb6429e18f
komed3 <pk97@outlook.de>
2026-06-19 18:57:47 +0200

    lookup profile by URI and ID, or find a similar matching profile

--- src/utils/ProfileManager.ts
+++ src/utils/ProfileManager.ts
@@ -2,6 +2,7 @@ import type { TProfileData } from '@rtbnext/schema/src/model/profile';
 
 import { Profile } from '@/model/Profile';
 import type { TProfileLookupResult } from '@/type/utils';
+import { ProfileMerger } from '@/util/ProfileMerger';
 
 
 export class ProfileManager {
@@ -24,5 +25,15 @@ export class ProfileManager {
     }
 
     return Profile.create( uriLike, profileData as TProfileData, [], aliases );
+  }
+
+  // --- lookup profile by URI and ID, or find a similar matching profile ---
+
+  public static lookup ( uriLike: string, id?: string, profileData?: Partial< TProfileData > ) : TProfileLookupResult {
+    let profile = Profile.find( uriLike );
+    const isExisting = profile && profile.verify( id ?? '' );
+    const isSimilar = ! isExisting && !! ( profile = ProfileMerger.findMatching( profileData ?? {} )[ 0 ] );
+
+    return { profile, isExisting, isSimilar };
   }
 }
