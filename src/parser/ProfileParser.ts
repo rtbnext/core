d39029d6602710d664a59cbfc980ba303bf4a567
komed3 <pk97@outlook.de>
2026-06-19 11:18:45 +0200

    implement static method to parse profile name

--- src/parser/ProfileParser.ts
+++ src/parser/ProfileParser.ts
@@ -1,6 +1,10 @@
+import type { TProfileName } from '@rtbnext/schema/src/model/profile';
+
 import { Cache } from '@/abstract/Cache';
 import { Utils } from '@/core/Utils';
 import type { IProfileParser } from '@/interface/parser';
+import { REGEX_FAMILY, REGEX_SPACE_DELIMITER } from '@/lib/regex';
+import { Parser } from '@/parser/Parser';
 import type { TProfileResponse } from '@/type/response';
 
 
@@ -42,5 +46,30 @@ export class ProfileParser extends Cache implements IProfileParser {
       return this.raw.uris.filter( Boolean ).map( i => Utils.sanitize( i ) )
         .filter( i => i !== uri ).sort();
     } );
+  }
+
+  // --- static methods ---
+
+  public static name (
+    value: unknown, lastName: unknown = undefined, firstName: unknown = undefined,
+    asianFormat: boolean = false
+  ) : { name: TProfileName, family: boolean } {
+    const name = Parser.string( value );
+    const clean = name.replace( REGEX_FAMILY, '' ).trim();
+    const family = REGEX_FAMILY.test( name );
+    const parts = clean.split( REGEX_SPACE_DELIMITER ).filter( Boolean );
+
+    const fN = firstName ? Parser.string( firstName ) : (
+      asianFormat ? parts.slice( 1 ).join( ' ' ) : parts.slice( 0, -1 ).join( ' ' )
+    );
+    const lN = lastName ? Parser.string( lastName ).replace( REGEX_FAMILY, '' ) : (
+      asianFormat ? parts[ 0 ] || '' : parts.pop() || ''
+    );
+
+    return { family, name: {
+      fullName: clean + ( family ? ' & family' : '' ),
+      shortName: `${ fN.split( ' ' )[ 0 ] } ${ lN }`.trim(),
+      lastName: lN, firstName: fN
+    } };
   }
 }
