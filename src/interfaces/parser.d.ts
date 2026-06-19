3207c75d8a2281f148d43366ae9300592b6a8ba2
komed3 <pk97@outlook.de>
2026-06-19 10:44:26 +0200

    create profile parser class

---
+++ src/interfaces/parser.d.ts
@@ -0,0 +1,4 @@
+import type { ICache } from '@/type/cache';
+
+
+export interface IProfileParser extends ICache {}
---
+++ src/parser/ProfileParser.ts
@@ -0,0 +1,4 @@
+import type { IProfileParser } from '@/interface/parser';
+
+
+export class ProfileParser extends Cache implements IProfileParser {}
