82360452e2d2dbe9c37962b0223dff57e7526fe9
komed3 <pk97@outlook.de>
2026-06-20 00:51:16 +0200

    implement basic snapshot getter

--- src/abstract/Snapshot.ts
+++ src/abstract/Snapshot.ts
@@ -4,6 +4,7 @@ import { join } from 'node:path';
 import { Storage } from '@/core/Storage';
 import { Utils } from '@/core/Utils';
 import type { ISnapshot } from '@/interface/snapshot';
+import { Parser } from '@/parser/Parser';
 import type { TStorageRWType } from '@/type/storage';
 
 
@@ -27,5 +28,23 @@ export abstract class Snapshot< T extends TSnapshot > implements ISnapshot< T > 
 
   protected datedPath ( date: string ) : string {
     return join( this.path, `${ date }.${ this.ext }` );
+  }
+
+  // --- basic getter ---
+
+  public getDates () : string[] {
+    return this.dates;
+  }
+
+  public hasDate ( dateLike: string ) : boolean {
+    return this.dates.includes( Parser.date( dateLike, 'ymd' )! );
+  }
+
+  public firstDate () : string | undefined {
+    return this.dates[ 0 ];
+  }
+
+  public latestDate () : string | undefined {
+    return this.dates.at( -1 );
   }
 }
