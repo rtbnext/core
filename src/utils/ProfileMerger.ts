8ad99fc63f47ae307cf52cecac099b12f5b1836b
komed3 <pk97@outlook.de>
2026-06-19 18:30:28 +0200

    sind similar URIs for merging profiles

--- src/utils/ProfileMerger.ts
+++ src/utils/ProfileMerger.ts
@@ -1,8 +1,24 @@
-import { CmpStrAsync } from 'cmpstr';
+import { CmpStrAsync, type CmpStrResult } from 'cmpstr';
 
 import { REGEX_URI_CLEANUP } from '@/lib/regex';
+import { ProfileIndex } from '@/model/ProfileIndex';
 
 
 CmpStrAsync.filter.add( 'input', 'normalizeUri', ( uri: string ) => uri.replace( REGEX_URI_CLEANUP, '' ) );
 
-export class ProfileMerger {}
+export class ProfileMerger {
+  private static readonly cmp = CmpStrAsync.create( { metric: 'dice', safeEmpty: true } );
+  private static readonly index = ProfileIndex.getInstance();
+
+  // --- helper ---
+
+  private static similarURIs ( uri: string ) : string[] {
+    const revUri = uri.split( '-' ).reverse().join( '-' );
+    const keys = [ ...ProfileMerger.index.getIndex().keys() ];
+
+    return [ ...new Set( [
+      ...ProfileMerger.cmp.match< CmpStrResult[] >( keys, uri, 0.9 ).map( i => i.source ),
+      ...ProfileMerger.cmp.match< CmpStrResult[] >( keys, revUri, 0.8 ).map( i => i.source )
+    ] ) ];
+  }
+}
