7d92aa7ebdf87ea89beea3e3f71e6c01f9df0534
komed3 <pk97@outlook.de>
2026-06-19 13:06:40 +0200

    load & save index

--- src/abstract/Index.ts
+++ src/abstract/Index.ts
@@ -1,5 +1,6 @@
 import type { TIndex } from '@rtbnext/schema/src/base/generic';
 
+import { log } from '@/core/Logger';
 import { Storage } from '@/core/Storage';
 import type { IIndex } from '../interfaces';
 
@@ -14,5 +15,20 @@ export abstract class Index< I extends TIndex, T extends Map< string, I > > impl
     this.type = type;
     this.path = path;
     Index.storage.ensurePath( this.path );
+    this.index = this.loadIndex();
+  }
+
+  // --- load & save index ---
+
+  protected loadIndex () : T {
+    const raw = Index.storage.readJSON< Record< string, I > > ( this.path ) ?? {};
+    log.debug( `Index [${ this.type }] loaded: ${ Object.keys( raw ).length } items` );
+
+    return new Map( Object.entries( raw ) ) as T;
+  }
+
+  protected saveIndex () : void {
+    const content = Object.fromEntries( this.index );
+    Index.storage.writeJSON< Record< string, I > >( this.path, content );
   }
 }
