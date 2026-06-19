d9837aaab012f40a825d07997046f135a2dda0d2
komed3 <pk97@outlook.de>
2026-06-19 23:30:43 +0200

    declare queue types

---
+++ src/types/queue.d.ts
@@ -0,0 +1,19 @@
+export type TQueueType = 'profile' | 'list';
+
+export interface TQueueOptions {
+  uriLike: string;
+  args?: Record< string, unknown >;
+  prio?: number;
+}
+
+export interface TQueueItem {
+  readonly key: string;
+  readonly uri: string;
+  ts: string;
+  args?: Record< string, unknown >;
+  prio?: number;
+}
+
+export type TQueue = Map< string, TQueueItem >;
+
+export type TQueueStorage = TQueueItem[];
