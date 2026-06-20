ed6731f0a60b7239636c38ec1e2443d84ca64da0
komed3 <pk97@outlook.de>
2026-06-21 00:57:10 +0200

    create program

--- src/bin/rtbnext.ts
+++ src/bin/rtbnext.ts
@@ -1,1 +1,11 @@
 #!/usr/bin/env node
+
+import { JOBS } from '@/job/index';
+import { Command } from 'commander';
+
+
+const program = new Command();
+
+for ( const JobClass of JOBS ) {}
+
+await program.parseAsync();
