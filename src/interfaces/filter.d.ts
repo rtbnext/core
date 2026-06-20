f32b1ad7e185b924450d14da816f10bc16b14037
komed3 <pk97@outlook.de>
2026-06-20 13:30:12 +0200

    declare filter interface

---
+++ src/interfaces/filter.d.ts
@@ -0,0 +1,13 @@
+import type { TFilterGroup, TFilterSpecial } from '@rtbnext/schema/src/base/const';
+import type { TFilter, TFilterList } from '@rtbnext/schema/src/model/filter';
+
+
+export interface IFilter {
+  resolvePath ( path: string ) : string | false;
+  getFilter ( group: TFilterGroup, key: string ) : TFilter | undefined;
+  getFilterByPath ( path: string ) : TFilter | undefined;
+  getGroup ( group: TFilterGroup ) : Record< string, TFilter > | undefined;
+  getSpecial ( special: TFilterSpecial ) : TFilter | undefined;
+  has ( path: string, uriLike: string ) : boolean;
+  save ( collection: Partial< TFilterList > ) : void;
+}
