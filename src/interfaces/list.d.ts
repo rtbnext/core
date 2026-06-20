4259490276992bf89534ab3cf182ec52ab55bcb7
komed3 <pk97@outlook.de>
2026-06-20 09:43:37 +0200

    Update list.d.ts

--- src/interfaces/list.d.ts
+++ src/interfaces/list.d.ts
@@ -1,10 +1,11 @@
 import type { TListIndexItem, TListSnapshot } from '@rtbnext/schema/src/model/list';
 
 import type { ISnapshot } from '@/interfaces/snapshot';
+import type { TListSnapshotData } from '@/type/generic';
 
 
 export interface IList extends ISnapshot< TListSnapshot > {
   getUri () : string;
   getItem () : TListIndexItem;
-  saveSnapshot ( snapshot: Omit< TListSnapshot, '$metadata' >, force: boolean = false ) : boolean;
+  saveSnapshot ( snapshot: TListSnapshotData, force: boolean = false ) : boolean;
 }
