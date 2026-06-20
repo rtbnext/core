4ac34d520bc5a156523bb52a7391fbdf701bc7cf
komed3 <pk97@outlook.de>
2026-06-20 14:56:26 +0200

    implement helper methods

--- src/model/Stats.ts
+++ src/model/Stats.ts
@@ -1,7 +1,9 @@
+import type { TMetaData } from '@rtbnext/schema/src/base/generic';
 import { join } from 'node:path';
 
 import { log } from '@/core/Logger';
 import { Storage } from '@/core/Storage';
+import { Utils } from '@/core/Utils';
 import type { IStats } from '@/interface/stats';
 import { StatsGroup } from '@/lib/const';
 
@@ -23,5 +25,21 @@ export class Stats implements IStats {
 
   private resolvePath ( path: string ) : string {
     return join( 'stats', path );
+  }
+
+  private prepStats < T > ( data: T ) : T & TMetaData {
+    return { ...Utils.metaData(), ...data };
+  }
+
+  private getStats < T > ( path: string, format: 'json' | 'csv' ) : T {
+    return ( ( Stats.storage[ format === 'csv' ? 'readCSV' : 'readJSON' ] as any )
+      ( this.resolvePath( path ) ) || ( format === 'csv' ? [] : {} ) ) as T;
+  }
+
+  private saveStats < T > ( path: string, format: 'json' | 'csv', data: T ) : boolean {
+    return log.catch( () =>
+      ( Stats.storage[ format === 'csv' ? 'writeCSV' : 'writeJSON' ] as any )
+        ( this.resolvePath( path ), data ), `Failed to save stats to ${ path }`
+    ) ?? false;
   }
 }
