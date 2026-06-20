28b1202a11f0cbcd94ea1474f6e748f5186555a7
komed3 <pk97@outlook.de>
2026-06-20 13:32:32 +0200

    initializing filter storage paths

--- src/model/Filter.ts
+++ src/model/Filter.ts
@@ -1,7 +1,10 @@
 import type { TFilterCollection } from '@rtbnext/schema/src/model/filter';
+import { join } from 'node:path';
 
+import { log } from '@/core/Logger';
 import { Storage } from '@/core/Storage';
 import type { IFilter } from '@/interface/filter';
+import { FilterGroup } from '@/lib/const';
 
 
 export class Filter implements IFilter {
@@ -9,4 +12,13 @@ export class Filter implements IFilter {
   private static instance: IFilter;
 
   private data: Partial< TFilterCollection > = {};
+
+  private constructor () {
+    this.initDB();
+  }
+
+  private initDB () : void {
+    log.debug( 'Initializing filter storage paths' );
+    FilterGroup.forEach( group => Filter.storage.ensurePath( join( 'filter', group ), true ) );
+  }
 }
