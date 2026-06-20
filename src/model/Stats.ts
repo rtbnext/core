2a47c3ef5932e22f8173ab99b8780b6ccef3a99a
komed3 <pk97@outlook.de>
2026-06-20 14:53:59 +0200

    init stats storage paths

--- src/model/Stats.ts
+++ src/model/Stats.ts
@@ -1,8 +1,27 @@
+import { join } from 'node:path';
+
+import { log } from '@/core/Logger';
 import { Storage } from '@/core/Storage';
 import type { IStats } from '@/interface/stats';
+import { StatsGroup } from '@/lib/const';
 
 
 export class Stats implements IStats {
   private static readonly storage = Storage.getInstance();
   private static instance: IStats;
+
+  private constructor () {
+    this.initDB();
+  }
+
+  private initDB () : void {
+    log.debug( 'Initializing stats storage paths' );
+    StatsGroup.forEach( group => Stats.storage.ensurePath( this.resolvePath( group ), true ) );
+  }
+
+  // --- helper ---
+
+  private resolvePath ( path: string ) : string {
+    return join( 'stats', path );
+  }
 }
