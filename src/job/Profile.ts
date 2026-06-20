0c3b6a8a285221e3ea3d19c42a7ae8425440c806
komed3 <pk97@outlook.de>
2026-06-20 23:20:36 +0200

    prep. profile job runner

---
+++ src/job/Profile.ts
@@ -0,0 +1,22 @@
+import { Job } from '@/abstract/Job';
+import { Fetch } from '@/core/Fetch';
+import { ProfileQueue } from '@/core/Queue';
+import type { IJob } from '@/interface/job';
+import { Parser } from '@/parser/Parser';
+
+
+export class ProfileJob extends Job implements IJob {
+  private static readonly fetch = Fetch.getInstance();
+  private static readonly queue = ProfileQueue.getInstance();
+
+  protected override readonly job = 'Profile';
+
+  public async run () : Promise< void > {
+    await this.protect( async () => {
+      const method = Parser.boolean( this.args.replace ) ? 'setData' : 'updateData';
+      const batch = 'profile' in this.args && typeof this.args.profile === 'string'
+        ? this.args.profile.split( ',' ).filter( Boolean )
+        : ProfileJob.queue.nextUri( Job.config.fetch.rateLimit.batchSize );
+    } );
+  }
+}
