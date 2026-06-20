aadf39ac162387a7292d354e7b20dc67cd8d8599
komed3 <pk97@outlook.de>
2026-06-20 18:26:29 +0200

    declare RETURNS map

--- src/utils/Performance.ts
+++ src/utils/Performance.ts
@@ -1,12 +1,16 @@
-import type { TExtrema } from '@rtbnext/schema/src/base/assets';
+import type { TExtrema, TReturns } from '@rtbnext/schema/src/base/assets';
 import type { TProfileHistory, TProfileHistoryItem } from '@rtbnext/schema/src/model/profile';
 
 
 export class Performance {
+  private static readonly RETURNS = {
+    week: 7, month: 30, quarter: 90, halfYear: 182, year: 365, twoYear: 730, fiveYear: 1825
+  } as const;
+
   public static getProfileExtrema ( history: TProfileHistory ) : TExtrema {
     const map = ( [ date, rank, networth ]: TProfileHistoryItem ) => ( { date, rank, networth } );
 
-    const [ low, high ] = history.reverse().reduce(
+    const [ low, high ] = history.toReversed().reduce(
       ( [ l, h ], row ) => [ ! l || row[ 2 ] < l[ 2 ] ? row : l, ! h || row[ 2 ] > h[ 2 ] ? row : h ],
       [] as [ TProfileHistoryItem?, TProfileHistoryItem? ]
     );
@@ -14,7 +18,7 @@ export class Performance {
     return { low: low && map( low ), high: high && map( high ) };
   }
 
-  public static generateProfileReturns () {}
+  public static generateProfileReturns ( history: TProfileHistory ) : TReturns {}
 
   public static generateProfilePerformance () {}
 }
