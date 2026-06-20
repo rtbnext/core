abb8483af451e631b1d8c7089a7943d0c1c5a11c
komed3 <pk97@outlook.de>
2026-06-20 03:47:31 +0200

    create list (static method)

--- src/model/List.ts
+++ src/model/List.ts
@@ -3,6 +3,7 @@ import type { TListIndexItem, TListSnapshot } from '@rtbnext/schema/src/model/li
 import { Snapshot } from '@/abstract/Snapshot';
 import type { IList } from '@/interface/list';
 import { ListIndex } from '@/model/ListIndex';
+import { log } from '@/core/Logger';
 
 
 export class List extends Snapshot< TListSnapshot > implements IList {
@@ -34,5 +35,22 @@ export class List extends Snapshot< TListSnapshot > implements IList {
   public static get ( uriLike: string ) : IList | false {
     try { return new List( List.index.get( uriLike ) ) }
     catch { return false }
+  }
+
+  // --- create list ---
+
+  public static create ( uriLike: string, data: TListIndexItem, snapshot?: Omit< TListSnapshot, '$metadata' > ) : List | false {
+    log.debug( `Creating List ${ uriLike }` );
+
+    return log.catch( () => {
+      const item = List.index.add( uriLike, data );
+      if ( ! item ) throw new Error( `List index item for ${ uriLike } could not be created` );
+
+      const list = new List( item );
+      if ( ! list ) throw new Error( `List ${ item.uri } could not be created` );
+
+      if ( snapshot ) list.saveSnapshot( snapshot );
+      return list;
+    }, `Failed to create List ${ uriLike }` ) ?? false;
   }
 }
