e6603d3fe46809164e6a0274691e3468483126da
komed3 <pk97@outlook.de>
2026-06-20 03:42:44 +0200

    Create List.ts

---
+++ src/model/List.ts
@@ -0,0 +1,21 @@
+import type { TListIndexItem, TListSnapshot } from '@rtbnext/schema/src/model/list';
+
+import { Snapshot } from '@/abstract/Snapshot';
+import type { IList } from '@/interface/list';
+import { ListIndex } from '@/model/ListIndex';
+
+
+export class List extends Snapshot< TListSnapshot > implements IList {
+  private static readonly index = ListIndex.getInstance();
+
+  private readonly uri: string;
+  private item: TListIndexItem;
+
+  private constructor ( item?: TListIndexItem ) {
+    if ( ! item ) throw new Error( `List index item not given` );
+
+    super( 'list', 'json' );
+    this.uri = item.uri;
+    this.item = item;
+  }
+}
