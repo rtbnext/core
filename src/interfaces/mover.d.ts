7666fde76417519e2f00668cf10c9f866b373c77
komed3 <pk97@outlook.de>
2026-06-20 13:13:32 +0200

    Update mover.d.ts

--- src/interfaces/mover.d.ts
+++ src/interfaces/mover.d.ts
@@ -3,4 +3,6 @@ import type { TMover } from '@rtbnext/schema/src/model/mover';
 import type { ISnapshot } from '@/interfaces/snapshot';
 
 
-export interface IMover extends ISnapshot< TMover > {}
+export interface IMover extends ISnapshot< TMover > {
+  saveSnapshot ( snapshot: TMoverData, force?: boolean ) : boolean;
+}
