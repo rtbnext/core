5205294aeb6f2e5c663e263b99a10c17570d45a1
komed3 <pk97@outlook.de>
2026-06-20 17:46:39 +0200

    prep new entries from sorted lists

--- src/utils/Ranking.ts
+++ src/utils/Ranking.ts
@@ -1,7 +1,9 @@
-import type { TRanking } from '@rtbnext/schema/src/base/assets';
+import type { TRanking, TRankingItem } from '@rtbnext/schema/src/base/assets';
 
 import { ListQueue } from '@/core/Queue';
 import { ListIndex } from '@/model/ListIndex';
+import { Parser } from '@/parser/Parser';
+import type { TQueueOptions } from '@/type/queue';
 import type { TProfileResponse } from '@/type/response';
 
 
@@ -13,6 +15,27 @@ export class Ranking {
     sortedLists: TProfileResponse[ 'person' ][ 'personLists' ], rankingData: TRanking[] = [],
     history: boolean = true, addQueue: boolean = true
   ) : TRanking[] {
+    const lists = new Map( rankingData.map( r => [ r.list, r ] ) );
+    const entries = new Map< string, TRankingItem[] >();
+    const names = new Map< string, { name: string, desc?: string } >();
+    const queue: TQueueOptions[] = [];
+
+    // --- prep new entries from sorted lists ---
+
+    for ( const { listUri, name, listDescription, date, timestamp, rank, finalWorth } of sortedLists ) {
+      if ( [ 'rtb', 'rtrl' ].includes( listUri ) ) continue;
+
+      const item = Parser.container< TRankingItem >( {
+        date: { value: date ?? timestamp, type: 'date', args: [ 'ymd' ], strict: false },
+        rank: { value: rank, type: 'number' },
+        networth: { value: finalWorth, type: 'money' }
+      } );
+
+      if ( ! entries.has( listUri ) ) entries.set( listUri, [] );
+      entries.get( listUri )!.push( item );
+      names.set( listUri, { name, desc: listDescription } );
+    }
+
     const result: TRanking[] = [];
 
     return result;
