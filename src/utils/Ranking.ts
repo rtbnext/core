0b4eacff8229ca2cf246a731c99ab11573da24bf
komed3 <pk97@outlook.de>
2026-06-20 17:08:14 +0200

    declare generateProfileRanking() method header

--- src/utils/Ranking.ts
+++ src/utils/Ranking.ts
@@ -1,8 +1,20 @@
+import type { TRanking } from '@rtbnext/schema/src/base/assets';
+
 import { ListQueue } from '@/core/Queue';
 import { ListIndex } from '@/model/ListIndex';
+import type { TProfileResponse } from '@/type/response';
 
 
 export class Ranking {
   private static readonly index = ListIndex.getInstance();
   private static readonly queue = ListQueue.getInstance();
+
+  public static generateProfileRanking (
+    sortedLists: TProfileResponse[ 'person' ][ 'personLists' ], rankingData: TRanking[] = [],
+    history: boolean = true, addQueue: boolean = true
+  ) : TRanking[] {
+    const result: TRanking[] = [];
+
+    return result;
+  }
 }
