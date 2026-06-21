5cc2ac9cd1672955d0911fa0e5cc28b891732a35
komed3 <pk97@outlook.de>
2026-06-21 19:52:14 +0200

    declare RTB job definition

--- src/job/RTB.ts
+++ src/job/RTB.ts
@@ -1,7 +1,21 @@
 import { Job } from '@/abstract/Job';
-import type { TJobClsOptions } from '@/type/job';
+import type { TJobClsOptions, TJobDefinition } from '@/type/job';
 
 
 export class RTBJob extends Job {
   constructor ( options: TJobClsOptions ) { super( options, 'rtb' ) }
+
+  // --- job runner ---
+
+  public async run () : Promise< void > {
+    await this.protect( async () => {} );
+  }
+
+  // --- command definition ---
+
+  public static readonly definition: TJobDefinition = {
+    id: 'rtb',
+    desc: 'Proceed daily real-time billionaires list',
+    options: []
+  } as const;
 }
