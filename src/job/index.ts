215566ae73fe9fc7112f9d91473072edbf7f78f5
komed3 <pk97@outlook.de>
2026-06-21 19:52:41 +0200

    connect RTB job

--- src/job/index.ts
+++ src/job/index.ts
@@ -2,10 +2,11 @@ import { MergeJob } from '@/job/Merge';
 import { MoveJob } from '@/job/Move';
 import { ProfileJob } from '@/job/Profile';
 import { QueueJob } from '@/job/Queue';
+import { RTBJob } from '@/job/RTB';
 import { WikiJob } from '@/job/Wiki';
 import type { TJobRegistry } from '@/type/job';
 
 
 export const JOBS = [
-  MergeJob, MoveJob, ProfileJob, QueueJob, WikiJob
+  MergeJob, MoveJob, ProfileJob, QueueJob, RTBJob, WikiJob
 ] as const satisfies TJobRegistry;
