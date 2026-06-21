60b80aed8ff1c54610ad71e3857efb31021603b4
komed3 <pk97@outlook.de>
2026-06-21 20:01:25 +0200

    check if RTB list is already up to date

--- src/job/RTB.ts
+++ src/job/RTB.ts
@@ -2,8 +2,9 @@ import { Job } from '@/abstract/Job';
 import { Fetch } from '@/core/Fetch';
 import { ProfileQueue } from '@/core/Queue';
 import { Stats } from '@/model/Stats';
+import { Parser } from '@/parser/Parser';
 import type { TJobClsOptions, TJobDefinition } from '@/type/job';
-import type { TListResponse, TPersonList } from '@/type/response';
+import type { TPersonList } from '@/type/response';
 
 
 export class RTBJob extends Job {
@@ -17,8 +18,16 @@ export class RTBJob extends Job {
 
   public async run () : Promise< void > {
     await this.protect( async () => {
-      const res = await RTBJob.fetch.list< TListResponse< TPersonList > >( 'rtb', '0' );
+      const res = await RTBJob.fetch.list< TPersonList >( 'rtb', '0' );
       if ( ! res?.success || ! res.data ) throw new Error( 'Request failed' );
+
+      const rawList = res.data.personList.personsLists;
+      const date = Parser.date( rawList[ 0 ].timestamp, 'ymd' )!;
+
+      if ( RTBJob.stats.getGlobalStats().date === date ) {
+        this.log( 'RTB list is already up to date' );
+        return;
+      }
     } );
   }
 
