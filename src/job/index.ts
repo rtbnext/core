cf4383ee1a514b2abdacc922d7f3f352abfe51d8
komed3 <pk97@outlook.de>
2026-06-21 14:59:41 +0200

    mount queue job

--- src/job/index.ts
+++ src/job/index.ts
@@ -1,9 +1,10 @@
 import { MoveJob } from '@/job/Move';
 import { ProfileJob } from '@/job/Profile';
+import { QueueJob } from '@/job/Queue';
 import { WikiJob } from '@/job/Wiki';
 import type { TJobRegistry } from '@/type/job';
 
 
 export const JOBS = [
-  MoveJob, ProfileJob, WikiJob
+  MoveJob, ProfileJob, QueueJob, WikiJob
 ] as const satisfies TJobRegistry;
