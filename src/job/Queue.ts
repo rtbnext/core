d81e52f7efa5987992a0291c9ba006e751de13c5
komed3 <pk97@outlook.de>
2026-06-21 14:49:02 +0200

    Update Queue.ts

--- src/job/Queue.ts
+++ src/job/Queue.ts
@@ -1,5 +1,21 @@
 import { Job } from '@/abstract/Job';
-import type { TQueueJobOptions } from '@/type/job';
+import type { TJobDefinition, TQueueJobOptions } from '@/type/job';
 
 
-export class QueueJob extends Job< TQueueJobOptions > {}
+export class QueueJob extends Job< TQueueJobOptions > {
+  constructor ( options: TQueueJobOptions ) { super( options, 'Queue' ) }
+
+  // --- job runner ---
+
+  public override async run () : Promise< void > {
+    await this.protect( async () => {} );
+  }
+
+  // --- command definition ---
+
+  public static readonly definition: TJobDefinition = {
+    id: 'queue',
+    desc: 'Managing list and profile queues',
+    options: []
+  } as const;
+}
