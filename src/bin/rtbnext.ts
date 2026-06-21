8d7e2ea89d2bcfc4737e8deefb6f8f8919843b3b
komed3 <pk97@outlook.de>
2026-06-21 10:49:53 +0200

    add global options

--- src/bin/rtbnext.ts
+++ src/bin/rtbnext.ts
@@ -5,11 +5,16 @@ import { Command } from 'commander';
 import { JOBS } from '@/job/index';
 
 
+const addGlobalOptions = ( command: Command ) : void => void command
+  .optionsGroup( 'Global Options:' )
+  .option( '-s, --silent', 'Disable logging' )
+  .option( '--safe-mode', 'Enable safe mode' );
+
 const program = new Command();
 
 program
   .name( 'ntbnext' )
-  .description( 'CLI to manage rtbnext database' )
+  .description( 'CLI to manage the @rtbnext database' )
   .version( '2.0.0' );
 
 for ( const JobClass of JOBS ) {
@@ -25,6 +30,7 @@ for ( const JobClass of JOBS ) {
     else fn( name, desc );
   }
 
+  addGlobalOptions( command );
   command.action( async ( options ) => await new JobClass( options ).run() );
 }
 
