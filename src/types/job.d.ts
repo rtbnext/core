ba16b4103948f16b18b67a4794b1c93c1764ad10
komed3 <pk97@outlook.de>
2026-06-21 14:59:52 +0200

    Update job.d.ts

--- src/types/job.d.ts
+++ src/types/job.d.ts
@@ -38,7 +38,7 @@ export type TProfileJobOptions = TJobClsOptions< {
 } >;
 
 export type TQueueJobOptions = TJobClsOptions< {
-  type: TQueueType;
+  queue: TQueueType;
   add?: string[];
   remove?: string[];
   prio?: number;
