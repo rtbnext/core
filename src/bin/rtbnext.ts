29bf1dbe2e7704cc976a7814b0a202988d6e60e6
komed3 <pk97@outlook.de>
2026-06-21 10:23:33 +0200

    commander will pass parsed options to job cls

--- src/bin/rtbnext.ts
+++ src/bin/rtbnext.ts
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 
-import { JOBS } from '@/job/index';
 import { Command } from 'commander';
+
+import { JOBS } from '@/job/index';
 
 
 const program = new Command();
@@ -10,12 +11,16 @@ for ( const JobClass of JOBS ) {
   const { id, desc, options } = JobClass.definition;
   const command = program.command( id ).description( desc );
 
-  for ( const [ option, desc, required ] of options )
-    command[ required ? 'requiredOption' : 'option' ]( option, desc );
+  for ( const { name, desc, parser, required } of options ) {
+    const fn = required
+      ? command.requiredOption.bind( command )
+      : command.option.bind( command );
 
-  command.action( async () => {
-    await new JobClass( process.argv.slice( 3 ) ).run();
-  } );
+    if ( parser ) fn( name, desc, parser );
+    else fn( name, desc );
+  }
+
+  command.action( async ( options ) => await new JobClass( options ).run() );
 }
 
 await program.parseAsync();
