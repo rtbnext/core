29d38c287b5f7c00aae1012491cd85d7cd6ba3db
komed3 <pk97@outlook.de>
2026-06-22 23:53:36 +0200

    instantiate fetch cls

--- src/core/Fetch.ts
+++ src/core/Fetch.ts
@@ -1,4 +1,18 @@
+import { Config } from '@/core/Config';
 import type { IFetch } from '@/interface/fetch';
+import type { TFetchConfig } from '@/type/config';
+import { type AxiosInstance } from 'axios';
 
 
-export class Fetch implements IFetch {}
+export class Fetch implements IFetch {
+  private static instance: IFetch;
+
+  private readonly config: TFetchConfig;
+  private lastRequest: number = 0;
+  private httpClient: AxiosInstance;
+
+  private constructor () {
+    this.config = Config.getInstance().fetch;
+    this.httpClient = this.setupHttpClient();
+  }
+}
