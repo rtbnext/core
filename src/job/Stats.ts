9827fa0cca66658a7568bd305299b182566999aa
komed3 <pk97@outlook.de>
2026-06-22 15:52:45 +0200

    aggregate stats over profiles

--- src/job/Stats.ts
+++ src/job/Stats.ts
@@ -1,5 +1,8 @@
+import type { TFilterList } from '@rtbnext/schema/src/model/filter';
+
 import { Job } from '@/abstract/Job';
 import { Filter } from '@/model/Filter';
+import { Profile } from '@/model/Profile';
 import { ProfileIndex } from '@/model/ProfileIndex';
 import { Stats } from '@/model/Stats';
 import type { TJobClsOptions, TJobDefinition } from '@/type/job';
@@ -18,6 +21,18 @@ export class StatsJob extends Job {
       const date = this.stats.getGlobalStats().date;
       const index = ProfileIndex.getInstance().getIndex();
       if ( ! date || ! index.size ) throw new Error( `No data available` );
+
+      this.log( `Generating stats for ${ date } with ${ index.size } profiles` );
+      const filter: Partial< TFilterList > = {}, stats: any = {};
+
+      for ( const item of index.values() ) {
+        const profile = Profile.getByItem( item );
+        if ( ! profile ) continue;
+
+        const data = profile.getData();
+        Stats.aggregate( data, date, stats );
+        Filter.aggregate( data, filter );
+      }
     } );
   }
 
