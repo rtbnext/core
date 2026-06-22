7f65a6596d20274f73c2b6e25471f031d8400e5b
komed3 <pk97@outlook.de>
2026-06-22 10:03:45 +0200

    parse uri / id

--- src/parser/ListParser.ts
+++ src/parser/ListParser.ts
@@ -1,13 +1,20 @@
 import { Cache } from '@/abstract/Cache';
+import { Utils } from '@/core/Utils';
 import type { IListParser } from '@/interface/parser';
 import type { TPersonListEntry } from '@/type/response';
 
 
 export class ListParser< T extends object > extends Cache implements IListParser< T > {
-  constructor ( private readonly raw: T ) { super() }
+  constructor ( protected readonly raw: T ) { super() }
   public rawData () : T { return this.raw }
 }
 
 export class PersonListParser extends ListParser< TPersonListEntry > {
+  public uri () : string {
+    return this.cache( 'uri', () => Utils.sanitize( this.raw.uri ) );
+  }
 
+  public id () : string {
+    return this.cache( 'id', () => Utils.hash( this.raw.naturalId ) );
+  }
 }
