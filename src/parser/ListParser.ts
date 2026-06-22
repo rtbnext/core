fb7e9f93555d81ff4d92a0b8b0b853936588f2d9
komed3 <pk97@outlook.de>
2026-06-22 10:02:38 +0200

    declare cls for person list

--- src/parser/ListParser.ts
+++ src/parser/ListParser.ts
@@ -1,7 +1,13 @@
 import { Cache } from '@/abstract/Cache';
 import type { IListParser } from '@/interface/parser';
+import type { TPersonListEntry } from '@/type/response';
 
 
 export class ListParser< T extends object > extends Cache implements IListParser< T > {
   constructor ( private readonly raw: T ) { super() }
+  public rawData () : T { return this.raw }
+}
+
+export class PersonListParser extends ListParser< TPersonListEntry > {
+
 }
