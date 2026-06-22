71fea181b9ae7dabde4fa376a18300fc3568c86c
komed3 <pk97@outlook.de>
2026-06-22 10:07:30 +0200

    parse basic fields

--- src/parser/ListParser.ts
+++ src/parser/ListParser.ts
@@ -1,7 +1,10 @@
 import { Cache } from '@/abstract/Cache';
 import { Utils } from '@/core/Utils';
 import type { IListParser } from '@/interface/parser';
+import { Parser } from '@/parser/Parser';
+import { ProfileParser } from '@/parser/ProfileParser';
 import type { TPersonListEntry } from '@/type/response';
+import { TProfileName } from '@rtbnext/schema/src/model/profile';
 
 
 export class ListParser< T extends object > extends Cache implements IListParser< T > {
@@ -16,5 +19,29 @@ export class PersonListParser extends ListParser< TPersonListEntry > {
 
   public id () : string {
     return this.cache( 'id', () => Utils.hash( this.raw.naturalId ) );
+  }
+
+  // --- basic fields ---
+
+  public date () : string {
+    return this.cache( 'date', () => Parser.date( this.raw.date || this.raw.timestamp, 'ymd' )! );
+  }
+
+  public rank () : number | undefined {
+    return this.cache( 'rank', () => Parser.strict( this.raw.rank, 'number' ) );
+  }
+
+  public networth () : number | undefined {
+    return this.cache( 'networth', () => Parser.strict( this.raw.finalWorth, 'money' ) );
+  }
+
+  public dropOff () : boolean | undefined {
+    return this.cache( 'dropOff', () => this.raw.finalWorth ? this.raw.finalWorth < 1e3 : undefined );
+  }
+
+  public name () : { name: TProfileName, family: boolean } {
+    return this.cache( 'name', () => ProfileParser.name(
+      this.raw.person?.name ?? this.raw.personName, this.raw.lastName
+    ) );
   }
 }
