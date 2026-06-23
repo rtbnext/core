79c06fbd6f11f3b288061cdb22699081d17ad2c0
komed3 <pk97@outlook.de>
2026-06-23 18:26:53 +0200

    generate profile performance data

--- src/job/Performance.ts
+++ src/job/Performance.ts
@@ -1,21 +1,38 @@
 import { Job } from '@/abstract/Job';
+import { Profile } from '@/model/Profile';
+import { ProfileIndex } from '@/model/ProfileIndex';
 import type { TJobClsOptions, TJobDefinition } from '@/type/job';
+import { Performance } from '@/util/Performance';
 
 
 export class PerformanceJob extends Job {
+  private static readonly index = ProfileIndex.getInstance();
+
   constructor ( options: TJobClsOptions ) { super( options, 'performance' ) }
 
   // --- job runner ---
 
   public override async run () : Promise< void > {
-    await this.protect( async () => {} );
+    await this.protect( async () => {
+      for ( const item of PerformanceJob.index.values ) {
+        const profile = Profile.getByItem( item );
+        if ( ! profile ) {
+          this.log( `Profile not found for ${ item.uri }`, undefined, 'warn' );
+          continue;
+        }
+
+        const performance = Performance.generateProfilePerformance( profile.getHistory() );
+        profile.updateData( { performance } );
+        profile.save();
+      }
+    } );
   }
 
   // --- command definition ---
 
   public static readonly definition: TJobDefinition = {
     id: 'performance',
-    desc: '',
+    desc: 'Generate profile performance data',
     options: []
   } as const;
 }
