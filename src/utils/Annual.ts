4d458f596795f2f1310cd919667479fe78c9f281
komed3 <pk97@outlook.de>
2026-06-24 11:34:54 +0200

    loop over history and aggregate data

--- src/utils/Annual.ts
+++ src/utils/Annual.ts
@@ -6,7 +6,26 @@ import type { TAnnualRawData } from '@/type/annual';
 export class Annual {
   private static aggregate ( history: TProfileHistory, year: number ) : TAnnualRawData {
     const rank: number[] = [], networth: number[] = [];
-    let prevRank: number, prevNet: number, hadBefore = false;
+    let prevRank: number | undefined = undefined;
+    let prevNetworth: number | undefined = undefined;
+    let hadBefore = false;
+
+    for ( let i = history.length - 1; i >= 0; i-- ) {
+      const [ date, rnk, ntw ] = history[ i ];
+      const y = +date.slice( 0, 4 );
+
+      if ( y < year - 1 ) break;
+      if ( y === year - 1 ) {
+        prevRank ??= rnk;
+        prevNetworth ??= ntw;
+      }
+
+      if ( y < year && ntw >= 1000 ) hadBefore = true;
+      if ( y !== year ) continue;
+
+      rank.push( rnk );
+      networth.push( ntw );
+    }
 
     return { rank, networth, prevRank, prevNetworth, hadBefore };
   }
