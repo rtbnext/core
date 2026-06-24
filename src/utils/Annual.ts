f311fa73bf429d50d5c5e809b9365827ac1c5278
komed3 <pk97@outlook.de>
2026-06-24 11:29:53 +0200

    declare aggregate method

--- src/utils/Annual.ts
+++ src/utils/Annual.ts
@@ -5,6 +5,9 @@ import type { TAnnualRawData } from '@/type/annual';
 
 export class Annual {
   private static aggregate ( history: TProfileHistory, year: number ) : TAnnualRawData {
-    
+    const rank: number[] = [], networth: number[] = [];
+    let prevRank: number, prevNet: number, hadBefore = false;
+
+    return { rank, networth, prevRank, prevNetworth, hadBefore };
   }
 }
