61611ac8778d1307d17bea22a4d12aac865a8305
komed3 <webmaster@komed3.de>
2026-06-27 23:53:00 +0200

    save current time as last cron run time

--- src/bin/cron.ts
+++ src/bin/cron.ts
@@ -7,3 +7,6 @@ const storage = Storage.getInstance();
 
 const lastRun = storage.readJSON< { lastRun: string } >( 'cron.json' );
 const after = new Date( lastRun ? lastRun.lastRun : 0 );
+const now = new Date();
+
+storage.writeJSON( 'cron.json', { lastRun: now.toISOString() } );
