901f15a82e725a09c6110d34891ae7e8cd89bc26
komed3 <webmaster@komed3.de>
2026-06-27 23:55:03 +0200

    skip jobs if cron obj is not declared

--- src/bin/cron.ts
+++ src/bin/cron.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import { Storage } from '@/core/Storage';
+import { JOBS } from '@/job/index';
 
 
 const storage = Storage.getInstance();
@@ -8,5 +9,9 @@ const storage = Storage.getInstance();
 const lastRun = storage.readJSON< { lastRun: string } >( 'cron.json' );
 const after = new Date( lastRun ? lastRun.lastRun : 0 );
 const now = new Date();
+
+for ( const JobClass of JOBS ) {
+  if ( ! ( 'cron' in JobClass ) ) continue;
+}
 
 storage.writeJSON( 'cron.json', { lastRun: now.toISOString() } );
