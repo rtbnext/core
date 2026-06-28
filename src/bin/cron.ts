422d0778590f50a9d7dba9777d8644e391fe83a2
komed3 <webmaster@komed3.de>
2026-06-28 04:13:12 +0200

    setup cron script

--- src/bin/cron.ts
+++ src/bin/cron.ts
@@ -1,39 +1,6 @@
 #!/usr/bin/env node
 
-import { prev } from 'nxtcron';
+import { Cron } from '@/core/Cron';
 
-import { Storage } from '@/core/Storage';
-import { JOBS } from '@/job/index';
-
-
-const storage = Storage.getInstance();
-const lastRun = storage.readJSON< { lastRun: string } >( 'cron.json' );
-const now = new Date();
-
-if ( ! lastRun ) {
-  storage.writeJSON( 'cron.json', { lastRun: now.toISOString() } );
-  return;
-}
-
-console.log( new Date(), prev( '* * * * *', { before: new Date(), after: new Date( Date.now() - 3e5 ), count: 99 } ) );
-
-/*import { Storage } from '@/core/Storage';
-import { JOBS } from '@/job/index';
-
-
-const storage = Storage.getInstance();
-const lastRun = storage.readJSON< { lastRun: string } >( 'cron.json' );
-
-const before = new Date( Math.ceil( Date.now() / 3e5 ) * 3e5 );
-const after = lastRun ? new Date( lastRun.lastRun ) : new Date( before.getTime() - 3e5 );
-
-for ( const JobClass of JOBS ) {
-  if ( ! ( 'cron' in JobClass ) ) continue;
-
-  for ( const { cronexpr, options } of JobClass.cron ) {
-    const date = prev( cronexpr, { count: 1, timezone: 'UTC', before } );
-    if ( date && date[ 0 ] > after ) console.log( JobClass.command.id, date[ 0 ] );
-  }
-}*/
-
-//storage.writeJSON( 'cron.json', { lastRun: now.toISOString() } );
+const cron = Cron.getInstance();
+await cron.run();
