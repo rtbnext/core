5515c5d7cbe9b3b728303f465fb16cc51109510c
komed3 <webmaster@komed3.de>
2026-06-28 04:17:39 +0200

    Update config.d.ts

--- src/interfaces/config.d.ts
+++ src/interfaces/config.d.ts
@@ -1,4 +1,7 @@
-import type { TConfigObject, TFetchConfig, TJobConfig, TLoggingConfig, TQueueConfig, TStorageConfig } from '@/type/config';
+import type {
+  TConfigObject, TCronConfig, TFetchConfig, TJobConfig, TLoggingConfig,
+  TQueueConfig, TStorageConfig
+} from '@/type/config';
 
 
 export interface IConfig {
@@ -7,6 +10,7 @@ export interface IConfig {
   config: TConfigObject;
   logging: TLoggingConfig;
   job: TJobConfig;
+  cron: TCronConfig;
   storage: TStorageConfig;
   fetch: TFetchConfig;
   queue: TQueueConfig;
