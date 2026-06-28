0bdc8aafe4042bd11bc471b43eeef6055e219071
komed3 <webmaster@komed3.de>
2026-06-28 03:33:25 +0200

    init cron class

--- src/bin/cron.ts
+++ src/bin/cron.ts
@@ -7,18 +7,33 @@ import { JOBS } from '@/job/index';
 
 
 const storage = Storage.getInstance();
-
 const lastRun = storage.readJSON< { lastRun: string } >( 'cron.json' );
-const after = new Date( lastRun ? lastRun.lastRun : 0 );
-const now = new Date(), before = new Date( after.getTime() + 3e5 );
+const now = new Date();
+
+if ( ! lastRun ) {
+  storage.writeJSON( 'cron.json', { lastRun: now.toISOString() } );
+  return;
+}
+
+console.log( new Date(), prev( '* * * * *', { before: new Date(), after: new Date( Date.now() - 3e5 ), count: 99 } ) );
+
+/*import { Storage } from '@/core/Storage';
+import { JOBS } from '@/job/index';
+
+
+const storage = Storage.getInstance();
+const lastRun = storage.readJSON< { lastRun: string } >( 'cron.json' );
+
+const before = new Date( Math.ceil( Date.now() / 3e5 ) * 3e5 );
+const after = lastRun ? new Date( lastRun.lastRun ) : new Date( before.getTime() - 3e5 );
 
 for ( const JobClass of JOBS ) {
   if ( ! ( 'cron' in JobClass ) ) continue;
 
   for ( const { cronexpr, options } of JobClass.cron ) {
-    const date = prev( cronexpr, { count: 1, timezone: 'UTC', after, before } );
-    console.log( date );
+    const date = prev( cronexpr, { count: 1, timezone: 'UTC', before } );
+    if ( date && date[ 0 ] > after ) console.log( JobClass.command.id, date[ 0 ] );
   }
-}
+}*/
 
-storage.writeJSON( 'cron.json', { lastRun: now.toISOString() } );
+//storage.writeJSON( 'cron.json', { lastRun: now.toISOString() } );
--- src/core/Cron.ts
+++ src/core/Cron.ts
@@ -5,9 +5,15 @@ export class Cron {
   private static readonly storage = Storage.getInstance();
 
   private readonly now: Date;
-  private readonly lastRun: Date;
+  private readonly lastRun: Date | false;
 
   constructor () {
     this.now = new Date();
+    this.lastRun = this.getLastRun();
+  }
+
+  private getLastRun () : Date | false {
+    const lastRun = Cron.storage.readJSON< { lastRun: string } >( 'cron.json' );
+    return lastRun && lastRun.lastRun ? new Date( lastRun.lastRun ) : false;
   }
 }
