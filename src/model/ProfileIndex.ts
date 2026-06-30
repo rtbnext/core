28576a132ef0c62a5a58cb55dbfc1ce5771571aa
komed3 <webmaster@komed3.de>
2026-06-30 10:32:27 +0200

    move profile index

--- src/model/ProfileIndex.ts
+++ src/model/ProfileIndex.ts
@@ -1,6 +1,7 @@
 import type { TProfileIndex, TProfileIndexItem } from '@rtbnext/schema/src/model/profile';
 
 import { Index } from '@/abstract/Index';
+import { log } from '@/core/Logger';
 import { Utils } from '@/core/Utils';
 import type { IProfileIndex } from '@/interface/index';
 
@@ -14,5 +15,29 @@ export class ProfileIndex extends Index< TProfileIndexItem, TProfileIndex > impl
   public find ( uriLike: string ) : TProfileIndex {
     const uri = Utils.sanitize( uriLike );
     return new Map( [ ...this.index ].filter( ( [ key, { aliases } ] ) => key === uri || aliases.includes( uri ) ) );
+  }
+
+  public move ( from: string, to: string, makeAlias: boolean = true ) : TProfileIndexItem | false {
+    log.debug( `Moving profile index item from ${ from } to ${ to }` );
+
+    return log.catch( () => {
+      from = Utils.sanitize( from ), to = Utils.sanitize( to );
+      const data = this.index.get( from ), test = this.find( to );
+      if ( ! data || test.size > 1 ) throw new Error( 'Invalid move operation' );
+
+      const foundKey = test.keys().next().value;
+      if ( foundKey && foundKey !== from ) throw new Error( 'Destination already exists' );
+
+      const item = { ...data, uri: to, aliases: [
+        ...data.aliases.filter( alias => alias !== to ),
+        ...( makeAlias ? [ from ] : [] )
+      ] };
+
+      this.index.delete( from );
+      this.index.set( to, item );
+      this.saveIndex();
+
+      return item;
+    }, `Failed to move profile index item ${ from } to ${ to }` ) ?? false;
   }
 }
