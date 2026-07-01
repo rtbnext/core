b15a17e01da434c0866c9f020ad6bbe5c4d8fc14
komed3 <webmaster@komed3.de>
2026-07-01 12:35:21 +0200

    Create Alias.ts

---
+++ src/job/Alias.ts
@@ -0,0 +1,21 @@
+import { Job } from '@/abstract/Job';
+import { TCommandJob } from '@/type/job';
+
+
+export class AliasJob extends Job< TAliasJobOptions > {
+  constructor ( options: TAliasJobOptions ) { super( options, 'alias' ) }
+
+  // --- job runner ---
+
+  public async run () : Promise< void > {
+    await this.protect( async () => {} );
+  }
+
+  // --- command definition ---
+
+  public static readonly command: TCommandJob = {
+    id: 'alias',
+    desc: 'Manage profile aliases',
+    options: []
+  } as const;
+}
