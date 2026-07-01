b325f165049c3353510f09b8f8241fadb2be5bf2
komed3 <webmaster@komed3.de>
2026-07-01 23:52:38 +0200

    update list interface

--- src/interfaces/list.d.ts
+++ src/interfaces/list.d.ts
@@ -1,4 +1,4 @@
-import type { TListIndexItem, TListSnapshot } from '@rtbnext/schema/src/model/list';
+import type { TListSnapshot } from '@rtbnext/schema/src/model/list';
 
 import type { ISnapshot } from '@/interface/snapshot';
 import type { TListSnapshotData } from '@/type/generic';
@@ -6,6 +6,5 @@ import type { TListSnapshotData } from '@/type/generic';
 
 export interface IList extends ISnapshot< TListSnapshot > {
   getUri () : string;
-  getItem () : TListIndexItem;
   saveSnapshot ( snapshot: TListSnapshotData, force: boolean = false ) : boolean;
 }
