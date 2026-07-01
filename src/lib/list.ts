2787d1c1b3c5035981d52c59682f2b18ab42f8b3
komed3 <webmaster@komed3.de>
2026-07-01 23:57:10 +0200

    list index item will not have date / count info

--- src/lib/list.ts
+++ src/lib/list.ts
@@ -4,15 +4,14 @@ import type { TListRegistry } from '@/type/list';
 export const LISTS = {
   rtb: {
     uri: 'rtb',
-    entry: ( date: string, count: number ) =>( {
+    entry: {
       uri: 'rtb',
       name: 'The World’s Real-Time Billionaires',
       shortName: 'Real-Time Billionaires',
-      date, count,
       desc: 'Today’s richest people in the world',
       text: 'todays richest people world',
       columns: [ 'rank', 'diff', 'profile', 'networth', 'today', 'ytd', 'age', 'citizenship', 'source' ],
       filters: [ 'gender', 'industry', 'citizenship', 'diff', 'age' ]
-    } )
+    }
   }
 } as const satisfies TListRegistry;
--- src/types/list.d.ts
+++ src/types/list.d.ts
@@ -3,7 +3,7 @@ import type { TListIndexItem } from '@rtbnext/schema/src/model/list';
 
 export type TListConfig< K extends string > = {
   uri: K;
-  entry: ( date: string, count: number ) => TListIndexItem & { uri: K };
+  entry: TListIndexItem & { uri: K };
 };
 
 export type TListRegistry = { readonly [ K in string ]?: TListConfig< K > };
