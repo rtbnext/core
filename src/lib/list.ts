15a87e667c50da718a980a1c1070a9a05739c9a6
komed3 <webmaster@komed3.de>
2026-07-02 10:36:33 +0200

    implement list config for RTB list

--- src/lib/list.ts
+++ src/lib/list.ts
@@ -1,9 +1,34 @@
 import { RTBListParser } from '@/parser/RTBListParser';
-import type { TListRegistry } from '@/type/list';
+import type { TListConfig, TRTBListItemCtx } from '@/type/list';
 
 
 export const LISTS = {
-  rtb: {},
-  billionaires: {},
-  person: {}
-} as const satisfies TListRegistry;
+  rtb: {
+    parser: RTBListParser,
+    lists: [ 'rtb' ],
+    indexItem: () => ( {
+      uri: 'rtb',
+      name: 'The World’s Real-Time Billionaires',
+      shortName: 'Real-Time Billionaires',
+      desc: 'Today’s richest people in the world',
+      text: 'todays richest people world',
+      columns: [ 'rank', 'diff', 'profile', 'networth', 'today', 'ytd', 'age', 'citizenship', 'source' ],
+      filters: [ 'gender', 'industry', 'citizenship', 'diff', 'age' ]
+    } ),
+    listItem: ( ctx: TRTBListItemCtx ) => ( {
+      uri: ctx.parsed.uri(),
+      rank: ctx.parsed.rank()!,
+      networth: ctx.parsed.networth()!,
+      name: ctx.data.info!.name.shortName,
+      flag: ctx.flag,
+      rankDiff: ctx.rankDiff,
+      gender: ctx.data.info?.gender,
+      age: ctx.parsed.age(),
+      today: ctx.realtime?.today,
+      ytd: ctx.realtime?.ytd,
+      citizenship: ctx.data.info?.citizenship,
+      industry: ctx.data.info?.industry!,
+      source: ctx.data.info?.source!
+    } )
+  }
+} as const satisfies TListConfig;
