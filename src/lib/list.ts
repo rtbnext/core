fd7c1127290a851ff5ad84dbfdb07cdf68983d1c
komed3 <webmaster@komed3.de>
2026-07-02 10:17:20 +0200

    prepare list registry

--- src/lib/list.ts
+++ src/lib/list.ts
@@ -3,17 +3,7 @@ import type { TListRegistry } from '@/type/list';
 
 
 export const LISTS = {
-  rtb: {
-    uri: 'rtb',
-    parser: RTBListParser,
-    entry: {
-      uri: 'rtb',
-      name: 'The World’s Real-Time Billionaires',
-      shortName: 'Real-Time Billionaires',
-      desc: 'Today’s richest people in the world',
-      text: 'todays richest people world',
-      columns: [ 'rank', 'diff', 'profile', 'networth', 'today', 'ytd', 'age', 'citizenship', 'source' ],
-      filters: [ 'gender', 'industry', 'citizenship', 'diff', 'age' ]
-    }
-  }
+  rtb: {},
+  billionaires: {},
+  person: {}
 } as const satisfies TListRegistry;
