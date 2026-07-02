330a0f487a29d2c5101d84484a948735965a8b58
komed3 <webmaster@komed3.de>
2026-07-02 19:03:53 +0200

    prepare raw list data, sort entries

--- src/parser/ListParser.ts
+++ src/parser/ListParser.ts
@@ -1,8 +1,21 @@
 import { Cache } from '@/abstract/Cache';
 import type { IListParser } from '@/interface/parser';
+import type { TListResponse, TResponse } from '@/type/response';
 
 
 export class ListParser< T extends object > extends Cache implements IListParser< T > {
   constructor ( protected readonly raw: T ) { super() }
   public rawData () : T { return this.raw }
+
+  // --- prepare raw list data ---
+
+  public static prepareList < T extends object > ( res: TResponse< TListResponse< T > > ) {
+    if ( ! res?.success || ! res.data ) throw new Error( 'Response does not contain valid list data' );
+
+    const rawList = res.data.personList.personsLists;
+    const entries = rawList.filter( ( i: any ) => ( i.rank || i.position ) && i.finalWorth ).filter( Boolean )
+      .sort( ( a: any, b: any ) => ( a.rank || a.position ) - ( b.rank || b.position ) );
+
+    return { rawData: res.data, rawList, entries };
+  }
 }
