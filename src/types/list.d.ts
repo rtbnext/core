388473277cf2da3beb82236a0ae9197114c6dfd5
komed3 <webmaster@komed3.de>
2026-07-02 10:16:29 +0200

    rework list config

--- src/types/list.d.ts
+++ src/types/list.d.ts
@@ -1,15 +1,16 @@
 import type { IListParser } from '@/interface/parser';
-import type { TListIndexItem } from '@rtbnext/schema/src/model/list';
+import type { TListIndexItem, TListItem } from '@rtbnext/schema/src/model/list';
 
 
 export type TListTypes = 'rtb' | 'billionaires' | 'person';
 
 export type TListParserClass = new ( ...args: any[] ) => IListParser;
 
-export type TListConfig< K extends string > = {
-  uri: K;
+export type TListConfig = {
   parser: TListParserClass;
-  entry: TListIndexItem & { uri: K };
+  entry ( entry: Partial< TListIndexItem > ) : TListIndexItem;
+  item < T extends TListItem > ( ctx: object ) : T;
+  lists?: string[];
 };
 
-export type TListRegistry = { readonly [ K in string ]?: TListConfig< K > };
+export type TListRegistry = readonly Record< TListTypes, TListConfig >;
