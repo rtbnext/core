066dd01b7ad89b8b26cd2c6ee9fdbc5df9e0c68b
komed3 <webmaster@komed3.de>
2026-07-03 18:35:47 +0200

    Update fetch.d.ts

--- src/interfaces/fetch.d.ts
+++ src/interfaces/fetch.d.ts
@@ -3,12 +3,12 @@ import type { TListResponse, TProfileResponse, TResponse } from '@/type/response
 
 
 export interface IFetch {
-  single < T > ( url: string, method: TFetchMethod = 'get', header?: THeader ) : Promise< TResponse< T > >;
-  batch < T > ( urls: string[], method: TFetchMethod = 'get', header?: THeader ) : Promise< TResponse< T >[] >;
+  single < T > ( url: string, method?: TFetchMethod, header?: THeader ) : Promise< TResponse< T > >;
+  batch < T > ( urls: string[], method?: TFetchMethod, header?: THeader ) : Promise< TResponse< T >[] >;
   wayback < T > ( url: string, ts: unknown ) : Promise< TResponse< T > >;
   list < T extends object > ( uriLike: string, year: string ) : Promise< TResponse< TListResponse< T > > >;
   profile ( ...uriLike: string[] ) : Promise< TResponse< TProfileResponse >[] >;
   wikidata < T > ( sparql: string ) : Promise< TResponse< T > >;
-  wikipedia < T > ( query: Record< string, unknown >, lang: string = 'en' ) : Promise< TResponse< T > >;
+  wikipedia < T > ( query: Record< string, unknown >, lang?: string ) : Promise< TResponse< T > >;
   commons < T > ( query: Record< string, unknown > ) : Promise< TResponse< T > >;
 }
