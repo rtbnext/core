6afe3414ee0d02fdb32349c3d083b8b516c04519
komed3 <webmaster@komed3.de>
2026-07-05 17:33:42 +0200

    update index interface

--- src/interfaces/index.d.ts
+++ src/interfaces/index.d.ts
@@ -1,24 +1,28 @@
-import type { TIndex } from '@rtbnext/schema/src/base/generic';
-import type { TListIndex, TListIndexItem } from '@rtbnext/schema/src/model/list';
-import type { TProfileIndex, TProfileIndexItem } from '@rtbnext/schema/src/model/profile';
+import type { TIndex, TMetaData } from '@rtbnext/schema/src/base/generic';
+import type { TListIndex, TListIndexItem, TListIndexMap } from '@rtbnext/schema/src/model/list';
+import type { TProfileIndex, TProfileIndexItem, TProfileIndexMap } from '@rtbnext/schema/src/model/profile';
 
 
-export interface IIndex< I extends TIndex, T extends Map< string, I > > {
+export interface IIndex<
+  I extends TIndex,
+  D extends TMetaData & { count: number, items: I[] },
+  M extends Map< string, I >
+> {
   readonly size: number;
   readonly values: IterableIterator< I >;
   readonly keys: IterableIterator< string >;
-  getIndex () : T;
+  getIndex () : D;
   has ( uriLike: string ) : boolean;
   get ( uriLike: string ) : I | undefined;
   update ( uriLike: string, data: Partial< I >, allowUpdate?: boolean, save?: boolean ) : I | false;
   delta ( items: Array< { uriLike: string, data: Partial< I > } >, allowUpdate?: boolean ) : number;
   add ( uriLike: string, data: I ) : I | false;
   delete ( uriLike: string ) : void;
-  search ( query: string, looseMatch?: boolean ) : T;
+  search ( query: string, looseMatch?: boolean ) : D;
 }
 
-export interface IProfileIndex extends IIndex< TProfileIndexItem, TProfileIndex > {
-  find ( uriLike: string ) : TProfileIndex;
+export interface IProfileIndex extends IIndex< TProfileIndexItem, TProfileIndex, TProfileIndexMap > {
+  find ( uriLike: string ) : TProfileIndexMap;
   move ( from: string, to: string, makeAlias?: boolean ) : TProfileIndexItem | false;
   syncFromData ( data: TProfileData ) : TProfileIndexItem | false;
   hasAlias ( aliasLike: string, uriLike?: string ) : string | false;
@@ -28,4 +32,4 @@ export interface IProfileIndex extends IIndex< TProfileIndexItem, TProfileIndex 
   rmvAliases ( uriLike: string, ...aliases: string[] ) : TProfileIndexItem | false;
 }
 
-export interface IListIndex extends IIndex< TListIndexItem, TListIndex > {}
+export interface IListIndex extends IIndex< TListIndexItem, TListIndex, TListIndexMap > {}
