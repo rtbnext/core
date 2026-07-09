d5c88cb360238f8f84fb99332bb5a805ceb69383
komed3 <webmaster@komed3.de>
2026-07-09 14:59:01 +0200

    declare TStorageScanType type

--- src/types/storage.d.ts
+++ src/types/storage.d.ts
@@ -1,5 +1,7 @@
 export type TStorageRWType = 'raw' | 'json' | 'csv';
 
+export type TStorageScanType = 'files' | 'dirs' | 'all';
+
 export type TStorageWOptions = {
   append: boolean;
   nl: boolean;
