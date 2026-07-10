b7f0803ed7c9cc4cd7c8a1d794ba0f1874564502
komed3 <webmaster@komed3.de>
2026-07-10 12:44:13 +0200

    rework status cls interface

--- src/interfaces/status.d.ts
+++ src/interfaces/status.d.ts
@@ -1,9 +1,14 @@
-import type { TService } from '@rtbnext/schema/src/base/const';
+import type { TService, TStatusFlag } from '@rtbnext/schema/src/base/const';
 import type { TStatus } from '@rtbnext/schema/src/model/status';
+
+import type { TStatusLog } from '@/type/status';
 
 
 export interface IStatus {
   log ( services: TService[], job: string, success: boolean, duration: number, err?: unknown, save?: boolean ) : void;
-  getStatus () : TStatus;
+  calculateStatus () : TStatus;
+  getStatus () : TStatus | undefined;
+  getServiceStatus ( service: TService ) : TStatusFlag;
+  getOverallStatus () : TStatusFlag;
   flush ( entries?: TStatusLog ) : void;
 }
