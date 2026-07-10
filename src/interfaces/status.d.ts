8a5c75a59e39b6f469596f63aed5b82c294d80f8
komed3 <webmaster@komed3.de>
2026-07-10 12:04:48 +0200

    update status cls interface

--- src/interfaces/status.d.ts
+++ src/interfaces/status.d.ts
@@ -1,7 +1,9 @@
 import type { TService } from '@rtbnext/schema/src/base/const';
+import type { TStatus } from '@rtbnext/schema/src/model/status';
 
 
 export interface IStatus {
   log ( services: TService[], job: string, success: boolean, duration: number, err?: unknown, save?: boolean ) : void;
+  getStatus () : TStatus;
   save ( entries?: TStatusLog ) : void;
 }
