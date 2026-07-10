0331e3b73a7990dcd69c046d7b8f6bd97ec25db0
komed3 <webmaster@komed3.de>
2026-07-10 11:50:42 +0200

    declare types for status police

--- src/types/status.d.ts
+++ src/types/status.d.ts
@@ -11,3 +11,11 @@ export type TStatusLogItem = {
 };
 
 export type TStatusLog = TStatusLogItem[];
+
+export type TStatusPolicy = {
+  samples: number;
+  degradedRatio: number;
+  outageRatio: number;
+};
+
+export type TStatusConfig = Record< TService, TStatusPolicy >;
