f4372c86b2722ddc2a427352b4b2c4f5435d5dbf
komed3 <webmaster@komed3.de>
2026-07-10 11:12:22 +0200

    load the status log

--- src/core/Status.ts
+++ src/core/Status.ts
@@ -2,23 +2,26 @@ import type { TService } from '@rtbnext/schema/src/base/const';
 
 import { Storage } from '@/core/Storage';
 import type { IStatus } from '@/interface/status';
+import type { TStatusLog, TStatusLogItem } from '@/type/status';
 
 
 export class Status implements IStatus {
   private static readonly storage = Storage.getInstance();
   private static instance: IStatus;
 
-  private readonly jsonl;
+  private readonly entries: TStatusLog;
 
   private constructor () {
-    this.jsonl = this.loadJSONL();
+    this.entries = this.loadLog();
   }
 
   // --- helper ---
 
-  private loadJSONL () {
-    return Status.storage.readJSONL( 'jobs.jsonl' );
+  private loadLog () : TStatusLog {
+    return Status.storage.readJSONL< TStatusLogItem >( 'jobs.jsonl' ) || [];
   }
+
+  // --- log job status ---
 
   public log ( services: TService[], job: string, success: boolean, duration: number, err?: unknown ) : void {
     //
