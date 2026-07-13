1197b3d7437d6f8406ec04569cf03ea6e04082f8
komed3 <webmaster@komed3.de>
2026-07-14 00:12:17 +0200

    create integrity job

---
+++ src/job/IntegrityJob.ts
@@ -0,0 +1,27 @@
+import { Job } from '@/abstract/Job';
+import type { TCommandJob, TCronJob, TJobClsOptions } from '@/type/job';
+import { Integrity } from '@/util/Integrity';
+
+
+export class IntegrityJob extends Job {
+  constructor ( options: TJobClsOptions = {} ) { super( options, 'integrity', [ 'profile' ] ) }
+
+  // --- job runner ---
+
+  public async run () : Promise< void > {
+    await this.protect( async () => Integrity.run() );
+  }
+
+  // --- command definition ---
+
+  public static readonly command: TCommandJob = {
+    id: 'integrity',
+    desc: 'Check profile integrity'
+  } as const;
+
+  // --- cron job definition ---
+
+  public static readonly cron: TCronJob = [ {
+    cronexpr: '0 0 * * *', // run every day at midnight
+  } ] as const;
+}
---
+++ src/utils/Integrity.ts
@@ -0,0 +1,3 @@
+export class Integrity {
+  public static run () : void {}
+}
