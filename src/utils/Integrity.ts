ba6bc7fe3264568cfd73bf3d7dd6c4ee43d343cd
komed3 <webmaster@komed3.de>
2026-07-14 00:18:15 +0200

    prepare integrity job

--- src/utils/Integrity.ts
+++ src/utils/Integrity.ts
@@ -1,3 +1,10 @@
+import { ProfileQueue } from '@/core/Queue';
+import { ProfileIndex } from '@/model/ProfileIndex';
+
 export class Integrity {
+  private static readonly files = [ 'meta.json', 'profile.json', 'history.csv' ] as const;
+  private static readonly index = ProfileIndex.getInstance();
+  private static readonly queue = ProfileQueue.getInstance();
+
   public static run () : void {}
 }
