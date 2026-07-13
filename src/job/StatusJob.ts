b1c25fbb45962d96354fc92d532fa1e738d54c32
komed3 <webmaster@komed3.de>
2026-07-13 15:32:52 +0200

    add `cleanup` flag to status job and run it every month

--- src/job/StatusJob.ts
+++ src/job/StatusJob.ts
@@ -1,21 +1,37 @@
 import { Job } from '@/abstract/Job';
 import { Status } from '@/core/Status';
-import type { TCommandJob, TJobClsOptions } from '@/type/job';
+import type { TCommandJob, TCronJob, TStatusJobOptions } from '@/type/job';
 
 
-export class StatusJob extends Job {
-  constructor ( options: TJobClsOptions = {} ) { super( options, 'status', [ 'system' ] ) }
+export class StatusJob extends Job< TStatusJobOptions > {
+  constructor ( options: TStatusJobOptions = {} ) { super( options, 'status', [ 'system' ] ) }
 
   // --- job runner ---
 
   public override async run () : Promise< void > {
-    await this.protect( async () => Status.getInstance().flush() );
+    await this.protect( async () => {
+      const status = Status.getInstance();
+
+      if ( this.options.cleanup ) status.cleanup();
+      else status.flush();
+    } );
   }
 
   // --- command definition ---
 
   public static readonly command: TCommandJob = {
     id: 'status',
-    desc: 'Calculates services status from job logs'
+    desc: 'Calculates services status from job logs',
+    options: [ {
+      name: '-c, --cleanup',
+      desc: 'Cleanup status log, archive entries older than 3 months'
+    } ]
   } as const;
+
+  // --- cron job definition ---
+
+  public static readonly cron: TCronJob< TStatusJobOptions > = [ {
+    cronexpr: '5 0 1 * *', // run at 0:05 AM on the first day of every month
+    options: () => ( { cleanup: true } )
+  } ] as const;
 }
