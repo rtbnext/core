c73d47e808942839dc90309d65bbbced989ea1ea
komed3 <webmaster@komed3.de>
2026-07-14 00:57:19 +0200

    check basic profile data integrity

--- src/utils/Integrity.ts
+++ src/utils/Integrity.ts
@@ -1,10 +1,32 @@
+import type { TProfileData } from '@rtbnext/schema/src/model/profile';
+
 import { ProfileQueue } from '@/core/Queue';
 import { Storage } from '@/core/Storage';
 import { ProfileIndex } from '@/model/ProfileIndex';
+import { Gender, Industry, MaritalStatus } from '@/lib/const';
 
 export class Integrity {
   private static readonly files = [ 'meta.json', 'profile.json', 'history.csv' ] as const;
   private static readonly storage = Storage.getInstance();
   private static readonly index = ProfileIndex.getInstance();
   private static readonly queue = ProfileQueue.getInstance();
+
+  private static validateData ( data: TProfileData, flags: string[] ) : void {
+    if ( ! data.id ) flags.push( 'missing-id' );
+    if ( ! data.uri ) flags.push( 'missing-uri' );
+
+    if ( ! data.info?.name?.fullName ) flags.push( 'missing-name' );
+    if ( ! Gender.includes( data.info?.gender ) ) flags.push( 'invalid-gender' );
+    if ( data.info?.birthDate && Number.isNaN( new Date( data.info?.birthDate ) ) ) flags.push( 'invalid-birthDate' );
+    if ( data.info?.maritalStatus && ! MaritalStatus.includes( data.info?.maritalStatus ) ) flags.push( 'invalid-maritalStatus' );
+    if ( data.info?.children && Number.isNaN( data.info?.children ) ) flags.push( 'invalid-children' );
+
+    if ( ! Industry.includes( data.info?.industry ) ) flags.push( 'invalid-industry' );
+    if ( ! Array.isArray( data.info?.source ) ) flags.push( 'invalid-source' );
+
+    if ( ! Array.isArray( data.related ) ) flags.push( 'invalid-related' );
+    if ( ! Array.isArray( data.media ) ) flags.push( 'invalid-media' );
+    if ( ! Array.isArray( data.assets ) ) flags.push( 'invalid-assets' );
+    if ( ! Array.isArray( data.annual ) ) flags.push( 'invalid-annual' );
+  }
 }
