5797a3a317edd7b51dc6f3e89caa152fcda72cfd
komed3 <webmaster@komed3.de>
2026-07-14 12:00:53 +0200

    declare TIntegrityCheck type

--- src/types/integrity.d.ts
+++ src/types/integrity.d.ts
@@ -1,4 +1,12 @@
 export type TValidateState = {
   flags: string[];
+  invalid: boolean;
   penalty: number;
 };
+
+export type TIntegrityCheck = readonly ( readonly [
+  check: unknown,
+  flag: string,
+  penalty: number,
+  invalid: boolean
+] )[];
