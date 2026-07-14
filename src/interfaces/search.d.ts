a7b1b107dd420f36ffd6106d2febe47f108e8d3c
komed3 <webmaster@komed3.de>
2026-07-14 13:03:18 +0200

    declare interface for search class

--- src/interfaces/search.d.ts
+++ src/interfaces/search.d.ts
@@ -1,1 +1,9 @@
-export interface ISearch {}
+import type { TSearchIndexItem } from '@rtbnext/schema/src/model/search';
+
+
+export interface ISearch {
+  getIndex () : TSearchIndexItem[];
+  readonly size: number;
+  filter ( predicate: ( item: TSearchIndexItem ) => boolean ) : TSearchIndexItem[];
+  save ( items?: TSearchIndexItem[] ) : void;
+}
