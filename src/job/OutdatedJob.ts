513a9f5a774fe120abe29ead8ea800bfd31c8fd4
komed3 <webmaster@komed3.de>
2026-07-20 00:50:32 +0200

    find outdated profiles and add them to the queue

--- src/job/OutdatedJob.ts
+++ src/job/OutdatedJob.ts
@@ -1,16 +1,33 @@
 import { Job } from '@/abstract/Job';
+import { ProfileQueue } from '@/core/Queue';
+import { Profile } from '@/model/Profile';
+import { ProfileIndex } from '@/model/ProfileIndex';
 import { Parser } from '@/parser/Parser';
 import type { TCommandJob, TCronJob, TOutdatedJobOptions } from '@/type/job';
+import type { TQueueOptions } from '@/type/queue';
 
 
 export class OutdatedJob extends Job< TOutdatedJobOptions > {
+  private static readonly index = ProfileIndex.getInstance();
+  private static readonly queue = ProfileQueue.getInstance();
+
   constructor ( options: TOutdatedJobOptions = {} ) { super( options, 'outdated', [ 'system' ] ) }
 
   // --- job runner ---
 
   public async run () : Promise< void > {
     await this.protect( async () => {
-      //
+      const prio = this.options.prio ?? 0;
+      const th = this.options.date ? Date.parse( this.options.date ) : Date.now() - 7.776e9;
+      const queue: TQueueOptions[] = [];
+
+      for ( const item of OutdatedJob.index.values ) {
+        const profile = Profile.getByItem( item );
+        if ( profile && ( profile.lastLookup() ?? 0 ) < th ) queue.push( { uriLike: profile.getUri(), prio } );
+      }
+
+      this.log( `Found ${ queue.length } outdated profiles (older than ${ Parser.date( new Date( th ), 'ymd' ) })` );
+      OutdatedJob.queue.addMany( queue );
     } );
   }
 
