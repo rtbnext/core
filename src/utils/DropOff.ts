b94de4bc68add65df22bee0f12e3156f1a06b540
komed3 <webmaster@komed3.de>
2026-07-31 10:56:10 +0200

    check drop-off based on realtime data

---
+++ src/utils/DropOff.ts
@@ -0,0 +1,23 @@
+import { Stats } from '@/model/Stats';
+import type { TProfileData } from '@rtbnext/schema/src/model/profile';
+
+
+export class DropOff {
+  private static readonly stats = Stats.getInstance();
+
+  public static check ( data: Partial< TProfileData > ) : boolean {
+    const { realtime } = data;
+    const today = DropOff.stats.getGlobalStats()?.date;
+
+    return ( ! realtime || realtime.date !== today || ! realtime.rank || ! realtime.networth || (
+      realtime.networth < 1000 && realtime.networth - ( realtime.today?.value ?? 0 ) < 1000
+    ) );
+  }
+
+  public static update ( data: Partial< TProfileData > ) : Partial< TProfileData > {
+    if ( ! data.info ) return data;
+
+    data.info.flags.dropOff = DropOff.check( data );
+    return data;
+  }
+}
