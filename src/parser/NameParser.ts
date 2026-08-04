4c7bcbb6d31c5eb68a8d5874cb373dff6f680828
komed3 <webmaster@komed3.de>
2026-08-04 22:35:49 +0200

    implement clean up name helper method

--- src/parser/NameParser.ts
+++ src/parser/NameParser.ts
@@ -1,4 +1,4 @@
-import { REGEX_NAME_TRIM, REGEX_SPACES } from '@/lib/regex';
+import { REGEX_FAMILY, REGEX_NAME_TRIM, REGEX_SPACES } from '@/lib/regex';
 import { Parser } from '@/parser/Parser';
 import type { TNameResult } from '@/type/parser';
 
@@ -6,6 +6,10 @@ import type { TNameResult } from '@/type/parser';
 export class NameParser {
   private static normalize ( value: unknown ) : string {
     return Parser.string( value ).replace( REGEX_NAME_TRIM, '' ).replace( REGEX_SPACES, ' ' ).trim();
+  }
+
+  private static cleanName ( value: string ) : string {
+    return value.replace( REGEX_FAMILY, '' ).replace( /^and\s+/i, '' ).trim();
   }
 
   public static parse ( value: unknown, lastName: unknown = undefined, firstName: unknown = undefined, asianFormat: boolean = false ) : TNameResult {}
