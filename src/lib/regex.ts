bfda61af199fceb146ba663907e79947e52e8651
komed3 <webmaster@komed3.de>
2026-08-04 18:41:13 +0200

    fix "family" regex

--- src/lib/regex.ts
+++ src/lib/regex.ts
@@ -1,8 +1,8 @@
 export const REGEX_DIACRITICS = /[\u0300-\u036f]/g;
-export const REGEX_FAMILY = /&\s*family/i;
+export const REGEX_FAMILY = /\s*&?\s*famil(?:y|ies)\s*$/i;
 export const REGEX_NOALNUM = /[^a-z0-9]+/g;
 export const REGEX_NONUM = /[^\d]+/g;
 export const REGEX_SPACE_DELIMITER = /\s+/;
 export const REGEX_SPACES = /\s+/g;
-export const REGEX_URI_CLEANUP = /-(family|\d+)$/i;
+export const REGEX_URI_CLEANUP = /-(family|families|\d+)$/i;
 export const REGEX_NAME_CLEANUP = /[^a-z0-9\s-]/g;
