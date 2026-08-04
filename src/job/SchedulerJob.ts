4642ccaa3560b85bdad394c2f6b203c520c177e1
komed3 <webmaster@komed3.de>
2026-08-04 17:38:55 +0200

    mount queues

--- src/job/SchedulerJob.ts
+++ src/job/SchedulerJob.ts
@@ -1,7 +1,11 @@
 import { Job } from '@/abstract/Job';
+import { ListQueue, ProfileQueue } from '@/core/Queue';
 import type { TJobClsOptions } from '@/type/job';
 
 
 export class SchedulerJob extends Job {
+  private static readonly profileQueue = ProfileQueue.getInstance();
+  private static readonly listQueue = ListQueue.getInstance();
+
   constructor ( options: TJobClsOptions = {} ) { super( options, 'scheduler', [ 'system' ] ) }
 }
