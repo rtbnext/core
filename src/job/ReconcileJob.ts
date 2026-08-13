9609a6a911df3c1d90f6ccef3ef9109a5aec1300
komed3 <webmaster@komed3.de>
2026-08-13 20:14:00 +0200

    declare command definition

--- src/job/ReconcileJob.ts
+++ src/job/ReconcileJob.ts
@@ -1,6 +1,6 @@
 import { Job } from '@/abstract/Job';
 import { ProfileIndex } from '@/model/ProfileIndex';
-import { TJobClsOptions } from '@/type/job';
+import type { TCommandJob, TJobClsOptions } from '@/type/job';
 
 
 export class ReconcileJob extends Job {
@@ -12,4 +12,11 @@ export class ReconcileJob extends Job {
   public override async run () : Promise< void > {
     await this.protect( async () => {} );
   }
+
+  // --- command definition ---
+
+  public static readonly command: TCommandJob = {
+    id: 'reconcile',
+    desc: 'Detect and resolve URI-alias conflicts'
+  } as const;
 }
