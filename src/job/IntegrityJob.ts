9930a70eb6962faabd68c40e3b1376bbf6525c10
komed3 <webmaster@komed3.de>
2026-08-13 23:19:24 +0200

    Update IntegrityJob.ts

--- src/job/IntegrityJob.ts
+++ src/job/IntegrityJob.ts
@@ -22,6 +22,6 @@ export class IntegrityJob extends Job {
   // --- cron job definition ---
 
   public static readonly cron: TCronJob = [ {
-    cronexpr: '0 0 * * *', // run every day at 00:00
+    cronexpr: '5 0 * * *', // run every day at 00:05
   } ] as const;
 }
