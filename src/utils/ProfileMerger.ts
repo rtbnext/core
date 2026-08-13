6895ce4db071299049dd1e8aae51038fc9b18847
komed3 <webmaster@komed3.de>
2026-08-13 13:19:30 +0200

    declare static readonly cmp + index instances

--- src/utils/ProfileMerger.ts
+++ src/utils/ProfileMerger.ts
@@ -3,7 +3,11 @@ import type { TProfileData, TProfileInfo } from '@rtbnext/schema/src/model/profi
 import { CmpStrAsync, type CmpStrResult } from 'cmpstr';
 
 import { REGEX_URI_CLEANUP } from '@/lib/regex';
+import { ProfileIndex } from '@/model/ProfileIndex';
 
 CmpStrAsync.filter.add( 'input', 'normalizeUri', ( uri: string ) => uri.replace( REGEX_URI_CLEANUP, '' ) );
 
-export class ProfileMerger {}
+export class ProfileMerger {
+  private static readonly cmp = CmpStrAsync.create( { metric: 'dice', safeEmpty: true } );
+  private static readonly index = ProfileIndex.getInstance();
+}
