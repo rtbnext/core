3b01d090027aa6ac08a08a1b5072369eb580fc2d
komed3 <webmaster@komed3.de>
2026-08-13 19:41:30 +0200

    declare job constructor

--- src/job/ReconcileJob.ts
+++ src/job/ReconcileJob.ts
@@ -1,4 +1,9 @@
 import { Job } from '@/abstract/Job';
+import { ProfileIndex } from '@/model/ProfileIndex';
+import { TJobClsOptions } from '@/type/job';
 
 
-export class ReconcileJob extends Job {}
+export class ReconcileJob extends Job {
+  private static index = ProfileIndex.getInstance();
+  constructor ( options: TJobClsOptions = {} ) { super( options, 'reconcile', [ 'profile', 'system' ] ) }
+}
