0cafcfb71167c55a696534406cf60cc0f330d23c
komed3 <webmaster@komed3.de>
2026-08-13 13:18:27 +0200

    create profile merger class

--- src/utils/ProfileMerger.ts
+++ src/utils/ProfileMerger.ts
@@ -5,3 +5,5 @@ import { CmpStrAsync, type CmpStrResult } from 'cmpstr';
 import { REGEX_URI_CLEANUP } from '@/lib/regex';
 
 CmpStrAsync.filter.add( 'input', 'normalizeUri', ( uri: string ) => uri.replace( REGEX_URI_CLEANUP, '' ) );
+
+export class ProfileMerger {}
