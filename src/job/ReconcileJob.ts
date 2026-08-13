03ac440e12ef55903fc0a4ca3b498d5167b82b0c
komed3 <webmaster@komed3.de>
2026-08-13 19:42:06 +0200

    prepare job runner

--- src/job/ReconcileJob.ts
+++ src/job/ReconcileJob.ts
@@ -6,4 +6,10 @@ import { TJobClsOptions } from '@/type/job';
 export class ReconcileJob extends Job {
   private static index = ProfileIndex.getInstance();
   constructor ( options: TJobClsOptions = {} ) { super( options, 'reconcile', [ 'profile', 'system' ] ) }
+
+  // --- job runner ---
+
+  public override async run () : Promise< void > {
+    await this.protect( async () => {} );
+  }
 }
