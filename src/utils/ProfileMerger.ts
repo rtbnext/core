4492a697aa432a60f2f7ab651eba7a461cd1444e
komed3 <webmaster@komed3.de>
2026-08-13 13:21:05 +0200

    implement helper method to find similar URIs

--- src/utils/ProfileMerger.ts
+++ src/utils/ProfileMerger.ts
@@ -10,4 +10,25 @@ CmpStrAsync.filter.add( 'input', 'normalizeUri', ( uri: string ) => uri.replace(
 export class ProfileMerger {
   private static readonly cmp = CmpStrAsync.create( { metric: 'dice', safeEmpty: true } );
   private static readonly index = ProfileIndex.getInstance();
+
+  // --- helper ---
+
+  private static similarURIs ( uri: string ) : string[] {
+    const revUri = uri.split( '-' ).reverse().join( '-' );
+    const entries = [ ...ProfileMerger.index.values ];
+    const names: string[] = [], owners = new Map< string, string >();
+
+    for ( const { uri: key, aliases } of entries ) for ( const name of [ key, ...aliases ] ) {
+      names.push( name );
+      owners.set( name, key );
+    }
+
+    const res = new Set( [
+      ...ProfileMerger.cmp.match< CmpStrResult[] >( names, uri, 0.9 ).map( i => owners.get( i.source ) ),
+      ...ProfileMerger.cmp.match< CmpStrResult[] >( names, revUri, 0.8 ).map( i => owners.get( i.source ) )
+    ] );
+
+    res.delete( uri );
+    return [ ...res ].filter( Boolean ) as string[];
+  }
 }
