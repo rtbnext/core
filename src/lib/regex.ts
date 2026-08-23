883af27efb389fa5ea2219aa9c54677ccb5d68db
komed3 <webmaster@komed3.de>
2026-08-23 15:46:07 +0200

    add regex consts

--- src/lib/regex.ts
+++ src/lib/regex.ts
@@ -2,6 +2,7 @@ export const REGEX_DIACRITICS = /[\u0300-\u036f]/g;
 
 export const REGEX_SPACES = /\s+/g;
 export const REGEX_SPACE_DELIMITER = /\s+/;
+export const REGEX_NEWLINE = /[\n\r]+/g;
 
 export const REGEX_NAME_CLEANUP = /[^\p{L}\s\-().']/gu;
 export const REGEX_NAME_TRIM = /^[\s.'-]+|[\s'-]+$/g;
@@ -9,6 +10,8 @@ export const REGEX_SEARCH_CLEANUP = /[^a-z0-9\s-]/g;
 
 export const REGEX_NOALNUM = /[^a-z0-9]+/g;
 export const REGEX_NONUM = /[^\d]+/g;
+
+export const REGEX_QUOTE = /"([\"]+)"/g;
 
 export const REGEX_FAMILY = /\s*(?:(&|and)\s*)?famil(?:y|ies)\b\s*$/i;
 export const REGEX_GROUP = /,|\band\b(?!\s+famil(?:y|ies)\b)|&\s*(?!famil(?:y|ies)\b)/i;
