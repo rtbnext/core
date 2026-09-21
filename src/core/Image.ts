f7d1f9318e316c9a6e0c8f98308a13ea9d79185f
komed3 <webmaster@komed3.de>
2026-09-21 14:31:38 +0200

    init media storage

--- src/core/Image.ts
+++ src/core/Image.ts
@@ -1,15 +1,18 @@
+import { Storage } from '@/core/Storage';
 import type { IImage } from '@/interface/image';
 import type { TImageMedia } from '@/type/image';
 
 
 export class Image implements IImage {
+  private static readonly storage = Storage.getInstance();
   private static instance: IImage;
-
   private static readonly hashLength = 32;
-  private static readonly mediaFile = 'profile/media.json';
 
-  private readonly storage: Storage;
-  private media: TImageMedia;
+  private readonly media: TImageMedia;
+
+  private constructor () {
+    this.media = Image.storage.readJSON< TImageMedia >( 'profile/media.json' ) || {};
+  }
 
   // --- instantiate ---
 
