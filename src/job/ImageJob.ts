1142f0b3816a9baa20d23d7ad21b812a67d8d66a
komed3 <webmaster@komed3.de>
2026-09-21 15:55:22 +0200

    add job runner

--- src/job/ImageJob.ts
+++ src/job/ImageJob.ts
@@ -4,4 +4,10 @@ import type { TImageJobOptions } from '@/type/job';
 
 export class ImageJob extends Job< TImageJobOptions > {
   constructor ( options: TImageJobOptions ) { super( options, 'image', [ 'system' ] ) }
+
+  // --- job runner ---
+
+  public async run () : Promise< void > {
+    await this.protect( async () => {} );
+  };
 }
