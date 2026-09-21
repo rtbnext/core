0c0484b27e51e3d5bbd4707decc5d069971ccb47
komed3 <webmaster@komed3.de>
2026-09-21 14:26:15 +0200

    update package exports

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@
 export { Config } from '@/core/Config';
 export { Cron, run } from '@/core/Cron';
 export { Fetch } from '@/core/Fetch';
+export { Image } from '@/core/Image';
 export { log, Logger } from '@/core/Logger';
 export { ListQueue, ProfileQueue, Queue } from '@/core/Queue';
 export { Status } from '@/core/Status';
@@ -83,6 +84,7 @@ export type * from '@/type/annual';
 export type * from '@/type/config';
 export type * from '@/type/fetch';
 export type * from '@/type/generic';
+export type * from '@/type/image';
 export type * from '@/type/integrity';
 export type * from '@/type/job';
 export type * from '@/type/list';
@@ -93,3 +95,4 @@ export type * from '@/type/response';
 export type * from '@/type/status';
 export type * from '@/type/storage';
 export type * from '@/type/wiki';
+
