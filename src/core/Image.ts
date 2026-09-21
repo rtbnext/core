ea641887f092ce662e5600cbcdbe91036283ed01
komed3 <webmaster@komed3.de>
2026-09-21 14:27:12 +0200

    instantiate image cls

--- src/core/Image.ts
+++ src/core/Image.ts
@@ -1,4 +1,12 @@
 import type { IImage } from '@/interface/image';
 
 
-export class Image implements IImage {}
+export class Image implements IImage {
+  private static instance: IImage;
+
+  // --- instantiate ---
+
+  public static getInstance () : IImage {
+    return Image.instance ??= new Image();
+  }
+}
