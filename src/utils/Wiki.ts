a2bfbeb92f4f1498daacf5e0b2e1aa5c8bd10dcf
komed3 <webmaster@komed3.de>
2026-09-21 19:19:53 +0200

    fix scores

--- src/utils/Wiki.ts
+++ src/utils/Wiki.ts
@@ -31,11 +31,11 @@ export class Wiki {
     // --- birth date matching ---
     if ( birthDate && item.birthdate?.value.startsWith( birthDate ) ) score += 0.25;
     else if ( birthDate && item.birthdate?.value.startsWith( birthDate.substring( 0, 4 ) ) ) score += 0.1;
-    else if ( birthDate && item.birthdate?.value ) score -= 0.5;
+    else if ( birthDate && item.birthdate?.value ) score -= 0.2;
 
     // --- gender matching ---
     if ( gender && item.gender?.value.endsWith( gender === 'm' ? 'Q6581097' : gender === 'f' ? 'Q6581072' : '-' ) ) score += 0.15;
-    else if ( gender && item.gender?.value ) return 0;
+    else if ( gender && item.gender?.value ) score -= 0.5;
 
     // --- citizenship matching ---
     if ( citizenship && item.iso2?.value === citizenship.toUpperCase() ) score += 0.15;
@@ -45,12 +45,12 @@ export class Wiki {
     if ( item.image ) score += 0.05;
 
     // --- occupation ---
-    if ( [ 'Q131524', 'Q557880', 'Q911554', 'Q2462658' ].some( e => item.occupation?.value.endsWith( e ) ) ) score += 0.2;
+    if ( [ 'Q131524', 'Q557880', 'Q911554', 'Q2462658' ].some( e => item.occupation?.value.endsWith( e ) ) ) score += 0.25;
     else if ( item.occupation ) score += 0.05;
 
     // --- economic relation ---
-    if ( item.employer ?? item.ownerOf ) score += 0.1;
-    if ( item.netWorth ) score += 0.2;
+    if ( item.employer ?? item.ownerOf ) score += 0.2;
+    if ( item.netWorth ) score += 0.25;
 
     return Math.min( 1, Math.max( 0, score ) );
   }
