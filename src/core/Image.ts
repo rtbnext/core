06805964d1db929fce1550afa594d2b6f958f9ee
komed3 <webmaster@komed3.de>
2026-09-21 14:27:52 +0200

    update Image.ts

--- src/core/Image.ts
+++ src/core/Image.ts
@@ -1,8 +1,15 @@
 import type { IImage } from '@/interface/image';
+import type { TImageMedia } from '@/type/image';
 
 
 export class Image implements IImage {
   private static instance: IImage;
+
+  private static readonly hashLength = 32;
+  private static readonly mediaFile = 'profile/media.json';
+
+  private readonly storage: Storage;
+  private media: TImageMedia;
 
   // --- instantiate ---
 
