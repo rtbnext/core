29b3160eb238ac5d5a0404226b8322caa96d36da
komed3 <webmaster@komed3.de>
2026-09-21 13:13:08 +0200

    update Fetch.ts

--- src/core/Fetch.ts
+++ src/core/Fetch.ts
@@ -183,17 +183,11 @@ export class Fetch implements IFetch {
     } ), 'get', this.useApiUserAgent() );
   }
 
-  public async download ( uri: string ) : Promise< Buffer | undefined > {
-    return log.catchAsync( async () => {
-      log.debug( `Downloading URI: ${ uri }` );
+  public async download ( uri: string ) : Promise< TResponse< Buffer > > {
+    const res = await this.single< ArrayBuffer >( uri, 'get', { Accept: '*/*' }, 'arraybuffer' );
 
-      const res = await this.httpClient.get< ArrayBuffer >( uri, {
-        headers: { ...this.config.headers, Accept: '*/*' },
-        responseType: 'arraybuffer'
-      } );
-
-      return Buffer.from( res.data );
-    }, `Failed to download URI: ${ uri }` );
+    if ( ! res.success || ! res.data ) return this.retErr( res, `Failed to download: ${ uri }` );
+    return { ...res, data: Buffer.from( res.data ) };
   }
 
   // --- instantiate ---
