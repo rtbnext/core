46253ebeaa524745e1e1e3f1867ec44722cd0997
komed3 <webmaster@komed3.de>
2026-09-22 01:18:14 +0200

    assign wiki page to profile

--- src/job/WikiJob.ts
+++ src/job/WikiJob.ts
@@ -1,5 +1,4 @@
 import { Job } from '@/abstract/Job';
-import type { IProfile } from '@/interface/profile';
 import { Profile } from '@/model/Profile';
 import type { TCommandJob, TWikiJobOptions } from '@/type/job';
 import { Wiki } from '@/util/Wiki';
@@ -10,33 +9,12 @@ export class WikiJob extends Job< TWikiJobOptions > {
 
   // --- job runner ---
 
-  private async update ( profile: IProfile ) : Promise< void > {
-    this.log( `Updating wiki for profile: ${ profile.getUri() }` );
-
-    const wiki = await Wiki.fromProfileData( profile.getData() );
-    if ( ! wiki ) throw new Error( `No wiki data found for profile: ${ profile.getUri() }` );
-
-    profile.updateData( { wiki } );
-    profile.save();
-  }
-
-  private async assign ( profile: IProfile, title: string ) : Promise< void > {
-    this.log( `Assigning wiki page "${ title }" to profile: ${ profile.getUri() }` );
-
-    const wiki = await Wiki.queryWikiPage( title );
-    if ( ! wiki ) throw new Error( `Wiki page not found: ${ title }` );
-
-    profile.updateData( { wiki } );
-    profile.save();
-  }
-
   public override async run () : Promise< void > {
     await this.protect( async () => {
       const profile = Profile.find( this.options.profile );
       if ( ! profile ) throw new Error( `Profile not found: ${ this.options.profile }` );
 
-      if ( this.options.assign ) await this.assign( profile, this.options.assign );
-      else await this.update( profile );
+      if ( this.options.assign ) Wiki.assign( profile.getData(), this.options.assign );
     } );
   }
 
