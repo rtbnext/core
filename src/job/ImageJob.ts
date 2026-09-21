8aa62242871354a7966c413ed2ec252ea80cbf25
komed3 <webmaster@komed3.de>
2026-09-21 15:58:35 +0200

    declare command definition

--- src/job/ImageJob.ts
+++ src/job/ImageJob.ts
@@ -1,5 +1,6 @@
 import { Job } from '@/abstract/Job';
-import type { TImageJobOptions } from '@/type/job';
+import { Parser } from '@/parser/Parser';
+import type { TCommandJob, TImageJobOptions } from '@/type/job';
 
 
 export class ImageJob extends Job< TImageJobOptions > {
@@ -10,4 +11,19 @@ export class ImageJob extends Job< TImageJobOptions > {
   public async run () : Promise< void > {
     await this.protect( async () => {} );
   };
+
+  // --- command definition ---
+
+  public static readonly command: TCommandJob = {
+    id: 'image',
+    desc: 'Remove profile images and clean up orphaned images',
+    options: [ {
+      name: '-r, --remove <URIs>',
+      desc: 'Remove specific profile images by URI (comma-separated)',
+      parser: ( v: string ) => Parser.list( v, 'string', ',' )
+    }, {
+      name: '-c, --cleanup',
+      desc: 'Cleanup orphaned images'
+    } ]
+  } as const;
 }
