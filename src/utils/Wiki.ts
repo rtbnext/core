01943e15cce832b36e0adaef09dce66a5b6442b0
komed3 <webmaster@komed3.de>
2026-09-21 18:43:30 +0200

    update Wiki.ts

--- src/utils/Wiki.ts
+++ src/utils/Wiki.ts
@@ -1,8 +1,19 @@
+import type { TProfileData } from '@rtbnext/schema/src/model/profile';
+
 import { Fetch } from '@/core/Fetch';
 import { Image } from '@/core/Image';
+import type { TWikidataResponseItem } from '@/type/response';
 
 
 export class Wiki {
   private static readonly fetch = Fetch.getInstance();
   private static readonly image = Image.getInstance();
+
+  private static scoreWDItem (
+    item: TWikidataResponseItem,
+    data: Partial< TProfileData >
+  ) : number {
+    const { name: { shortName } = {}, gender, birthDate, citizenship } = data.info ?? {};
+    let score = 0;
+  }
 }
