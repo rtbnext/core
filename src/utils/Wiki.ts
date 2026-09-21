e9386c698262568e32055185efc0460304ac7201
komed3 <webmaster@komed3.de>
2026-09-21 18:58:14 +0200

    update Wiki.ts

--- src/utils/Wiki.ts
+++ src/utils/Wiki.ts
@@ -9,17 +9,12 @@ export class Wiki {
   private static readonly fetch = Fetch.getInstance();
   private static readonly image = Image.getInstance();
 
-  private static scoreWDItem (
-    item: TWikidataResponseItem,
-    data: Partial< TProfileData >
-  ) : number {
+  private static scoreWDItem ( item: TWikidataResponseItem, data: Partial< TProfileData > ) : number {
     const { name: { shortName } = {}, gender, birthDate, citizenship } = data.info ?? {};
     let score = 0;
 
     // --- name matching ---
-    if ( shortName && item.itemLabel.value.trim() === shortName ) score += 0.25;
-    else if ( shortName && item.itemLabel.value.trim().toLowerCase() === shortName.toLowerCase() ) score += 0.15;
-    else return 0;
+    //
 
     // --- birth date matching ---
     if ( birthDate && item.birthdate?.value.startsWith( birthDate ) ) score += 0.25;
@@ -29,5 +24,22 @@ export class Wiki {
     // --- gender matching ---
     if ( gender && item.gender?.value.endsWith( gender === 'm' ? 'Q6581097' : gender === 'f' ? 'Q6581072' : '-' ) ) score += 0.15;
     else if ( gender && item.gender?.value ) return 0;
+
+    // --- citizenship matching ---
+    if ( citizenship && item.iso2?.value === citizenship.toUpperCase() ) score += 0.15;
+
+    // --- article / image ---
+    if ( item.article ) score += 0.05;
+    if ( item.image ) score += 0.05;
+
+    // --- occupation ---
+    if ( [ 'Q131524', 'Q557880', 'Q911554', 'Q2462658' ].some( e => item.occupation?.value.endsWith( e ) ) ) score += 0.2;
+    else if ( item.occupation ) score += 0.05;
+
+    // --- economic relation ---
+    if ( item.employer ?? item.ownerOf ) score += 0.1;
+    if ( item.netWorth ) score += 0.2;
+
+    return Math.min( 1, Math.max( 0, score ) );
   }
 }
