9c577e3b1d5a2557ef4f5f71eb7e7a5faebd0ec1
komed3 <webmaster@komed3.de>
2026-09-22 23:21:07 +0200

    chore: delete BillionairesListParser.ts

--- src/parser/BillionairesListParser.ts
+++
@@ -1,5 +0,0 @@
-import type { IBillionairesListParser } from '@/interface/parser';
-import { PersonListParser } from '@/parser/PersonListParser';
-
-
-export class BillionairesListParser extends PersonListParser implements IBillionairesListParser {}
