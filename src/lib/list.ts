21c6605f005aad769901859ce0034001e2c34bca
komed3 <webmaster@komed3.de>
2026-09-22 15:03:06 +0200

    remove short name from lists

--- src/lib/list.ts
+++ src/lib/list.ts
@@ -43,8 +43,7 @@ export const LISTS = {
     lists: [ 'billionaires', 'forbes-400' ],
     parser: BillionairesListParser,
     indexItem: ( uri: string, ctx: TListIndexItemCtx ) => ( {
-      uri, name: ctx.name, shortName: ctx.name, desc: ctx.desc,
-      text: Utils.buildSearchText( ctx.desc ),
+      uri, name: ctx.name, desc: ctx.desc, text: Utils.buildSearchText( ctx.desc ),
       columns: [ 'rank', 'profile', 'networth', 'age', 'citizenship', 'selfMadeRank', 'philanthropyScore', 'source' ],
       filters: [ 'gender', 'industry', 'citizenship', 'age', 'selfMadeRank', 'philanthropyScore' ]
     } ),
@@ -57,8 +56,7 @@ export const LISTS = {
   person: {
     parser: PersonListParser,
     indexItem: ( uri: string, ctx: TListIndexItemCtx ) => ( {
-      uri, name: ctx.name, shortName: ctx.name, desc: ctx.desc,
-      text: Utils.buildSearchText( ctx.desc ),
+      uri, name: ctx.name, desc: ctx.desc, text: Utils.buildSearchText( ctx.desc ),
       columns: [ 'rank', 'profile', 'networth', 'age', 'citizenship', 'source' ],
       filters: [ 'gender', 'industry', 'citizenship', 'age' ]
     } ),
