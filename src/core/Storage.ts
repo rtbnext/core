afe8169f65583a18cb2bd08ceb48dadbf45fc5d7
Paul Köhler <webmaster@komed3.de>
2026-09-22 09:41:18 +0200

    Update Storage.ts

--- src/core/Storage.ts
+++ src/core/Storage.ts
@@ -31,7 +31,7 @@ export class Storage implements IStorage {
   private initDB () : void {
     log.debug( `Initializing storage at ${ this.path }` );
     this.ensurePath( this.path, true );
-    this.ensurePath( this.mediaPath, true );
+    this.ensureMediaPath( this.mediaPath, true );
 
     [ 'profile', 'list', 'filter', 'mover', 'stats', 'queue', 'system' ]
       .forEach( path => this.ensurePath( path, true ) );
