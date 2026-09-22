10acb09371e793cc56f184290fd7df4b553fb436
komed3 <webmaster@komed3.de>
2026-09-22 12:58:17 +0200

    update ImageJob.ts

--- src/job/ImageJob.ts
+++ src/job/ImageJob.ts
@@ -1,7 +1,7 @@
 import { Job } from '@/abstract/Job';
 import { Image } from '@/core/Image';
 import { Parser } from '@/parser/Parser';
-import type { TCommandJob, TImageJobOptions } from '@/type/job';
+import type { TCommandJob, TCronJob, TImageJobOptions } from '@/type/job';
 
 
 export class ImageJob extends Job< TImageJobOptions > {
@@ -32,4 +32,11 @@ export class ImageJob extends Job< TImageJobOptions > {
       desc: 'Cleanup orphaned images'
     } ]
   } as const;
+
+  // --- cron job definition ---
+
+  public static readonly cron: TCronJob< TImageJobOptions > = [ {
+    cronexpr: '10 0 1 * *', // run at 0:10 on the first day of every month,
+    options: () => ( { cleanup: true } )
+  } ] as const;
 }
