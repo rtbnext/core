b343e696cf202c609b4a8f2006fb23a7a9a95caa
komed3 <webmaster@komed3.de>
2026-09-22 23:26:15 +0200

    manage imports

--- src/types/list.d.ts
+++ src/types/list.d.ts
@@ -1,12 +1,12 @@
 import type { TRealtime } from '@rtbnext/schema/src/base/assets';
 import type { TChangeFlag } from '@rtbnext/schema/src/base/const';
-import type { TBillionairesListItem, TListIndexItem, TPersonListItem, TRTBListItem } from '@rtbnext/schema/src/model/list';
+import type { TListIndexItem, TPersonListItem, TRTBListItem } from '@rtbnext/schema/src/model/list';
 import type { TProfileData } from '@rtbnext/schema/src/model/profile';
 import type { Expand } from 'devtypes/types/util';
 
-import type { IBillionairesListParser, IListParser, IPersonListParser, IRTBListParser } from '@/interface/parser';
-import type { TListResponse } from '@/type/response';
+import type { IListParser, IPersonListParser, IRTBListParser } from '@/interface/parser';
 import type { IProfile } from '@/interface/profile';
+import type { TListResponse } from '@/type/response';
 
 
 export type TListTypes = 'rtb' | 'billionaires' | 'person';
