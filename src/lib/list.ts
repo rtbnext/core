d14eb12e0e4c2f72f0b34a88c5fb3da5a3621784
komed3 <webmaster@komed3.de>
2026-09-22 23:29:05 +0200

    declare RTB list

--- src/lib/list.ts
+++ src/lib/list.ts
@@ -1,4 +1,19 @@
+import { RTBListParser } from '@/parser/RTBListParser';
 import type { TListConfig } from '@/type/list';
 
 
-export const LISTS = {} as const satisfies TListConfig;
+export const LISTS = {
+  rtb: {
+    lists: [ 'rtb' ],
+    parser: RTBListParser,
+    indexItem: () => ( {
+      uri: 'rtb',
+      name: 'The World’s Real-Time Billionaires',
+      shortName: 'Real-Time Billionaires',
+      desc: 'Today’s richest people in the world',
+      text: 'todays richest people world',
+      columns: [ 'rank', 'diff', 'profile', 'networth', 'today', 'ytd', 'age', 'citizenship', 'source' ],
+      filters: [ 'gender', 'industry', 'citizenship', 'diff', 'age' ]
+    } )
+  }
+} as const satisfies TListConfig;
